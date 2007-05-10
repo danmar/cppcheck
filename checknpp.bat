ad75792512928ab6a582621fefa10151519fd04a
Daniel Marjamäki <hyd_danmar@users.sourceforge.net>
2007-05-10 17:24:52 +0000

    Updated script that checks notepad++

--- checknpp.bat
+++ checknpp.bat
@@ -12,14 +12,17 @@ FOR %%s IN (npp41\scintilla\src\*.cxx) DO (
 )
 
 
-date /t > powereditor.txt
-time /t >> powereditor.txt
+date /t > npp41.txt
+time /t >> npp41.txt
+
+ECHO Notepad++ 4.1 >> npp41.txt
+ECHO ====================================== >> npp41.txt
 
 FOR %%s IN (npp41\PowerEditor\src\*.cpp) DO (
     ECHO %%s
-    ECHO ---------------------------------- >> powereditor.txt
-    ECHO %%s >> powereditor.txt
-    cppcheck %%s 2>> powereditor.txt
+    ECHO ---------------------------------- >> npp41.txt
+    ECHO %%s >> npp41.txt
+    cppcheck %%s 2>> npp41.txt
 )
 
 
