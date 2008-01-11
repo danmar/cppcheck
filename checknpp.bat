f3a96109ebc713a31683fb1cd751ac431493cd22
Daniel Marjamäki <hyd_danmar@users.sourceforge.net>
2008-01-11 07:30:56 +0000

    check npp472 instead of npp41

--- checknpp.bat
+++ checknpp.bat
@@ -4,25 +4,25 @@
 date /t > scintilla.txt
 time /t >> scintilla.txt
 
-FOR %%s IN (npp41\scintilla\src\*.cxx) DO (
+FOR %%s IN (npp472\scintilla\src\*.cxx) DO (
     ECHO %%s
     ECHO ---------------------------------- >> scintilla.txt
     ECHO %%s >> scintilla.txt
-    cppcheck --all %%s 2>> scintilla.txt
+    cppcheck %%s 2>> scintilla.txt
 )
 
 
-date /t > npp41.txt
-time /t >> npp41.txt
+date /t > npp472.txt
+time /t >> npp472.txt
 
-ECHO Notepad++ 4.1 >> npp41.txt
-ECHO ====================================== >> npp41.txt
+ECHO Notepad++ 4.7.2 >> npp472.txt
+ECHO ====================================== >> npp472.txt
 
-FOR %%s IN (npp41\PowerEditor\src\*.cpp) DO (
+FOR %%s IN (npp472\PowerEditor\src\*.cpp) DO (
     ECHO %%s
-    ECHO ---------------------------------- >> npp41.txt
-    ECHO %%s >> npp41.txt
-    cppcheck --all %%s 2>> npp41.txt
+    ECHO ---------------------------------- >> npp472.txt
+    ECHO %%s >> npp472.txt
+    cppcheck %%s 2>> npp472.txt
 )
 
 
