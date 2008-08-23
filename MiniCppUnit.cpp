7268c40560d71236774f6c50766584b9ae2705fd
Daniel Marjamäki <hyd_danmar@users.sourceforge.net>
2008-08-23 13:17:10 +0000

    minicppunit: removed compiler warnings about characters

--- MiniCppUnit.cpp
+++ MiniCppUnit.cpp
@@ -1,5 +1,5 @@
-﻿/*
- * Copyright (c) 2003-2004  Pau Arum� & David Garc�a
+/*
+ * Copyright (c) 2003-2004  Pau Arum. & David Garc.a
  *
  *
  * This program is free software; you can redistribute it and/or modify
