534445951ce4f3c73dd905b25435fd42109e1d8d
Daniel Marjamäki <hyd_danmar@users.sourceforge.net>
2008-09-24 11:28:00 +0000

    testunusedvar: Added simple testing for unused variables

---
+++ testunusedvar.cpp
@@ -0,0 +1,53 @@
+
+// Check for unused variables..
+
+#include "MiniCppUnit.h"
+#include "tokenize.h"
+#include "CheckOther.h"
+
+#include <sstream>
+
+extern std::ostringstream errout;
+
+class TestUnusedVar : public TestFixture<TestUnusedVar>
+{
+private:
+    void check( const char code[] )
+    {
+        // Tokenize..
+        tokens = tokens_back = NULL;
+        std::istringstream istr(code);
+        TokenizeCode( istr );
+        SimplifyTokenList();
+
+        // Clear the error buffer..
+        errout.str("");
+
+        // Check for unused variables..
+        CheckStructMemberUsage();
+    }
+
+public:
+    TEST_FIXTURE( TestUnusedVar )
+    {
+        TEST_CASE( structmember1 );
+    }
+
+    void structmember1()
+    {
+        check( "struct abc\n"
+               "{\n"
+               "    int a;\n"
+               "    int b;\n"
+               "    int c;\n"
+               "};\n" );
+        ASSERT_EQUALS( std::string("[test.cpp:2]: struct member 'abc::a' is never read\n"
+                                   "[test.cpp:3]: struct member 'abc::b' is never read\n"
+                                   "[test.cpp:4]: struct member 'abc::c' is never read\n"), errout.str() );
+    }
+
+};
+
+REGISTER_FIXTURE( TestUnusedVar )
+
+
