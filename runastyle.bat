0f111259b94500338e963330a53ed2078721ff93
Daniel Marjamäki <hyd_danmar@users.sourceforge.net>
2009-01-07 15:08:24 +0000

    windows: Added runastyle.bat

---
+++ runastyle.bat
@@ -0,0 +1,7 @@
+
+astyle --style=ansi --min-conditional-indent=0 --pad=oper --unpad=paren src/*.cpp
+astyle --style=ansi --min-conditional-indent=0 --pad=oper --unpad=paren src/*.h
+astyle --style=ansi --min-conditional-indent=0 --pad=oper --unpad=paren test/*.cpp
+astyle --style=ansi --min-conditional-indent=0 --pad=oper --unpad=paren test/*.h
+astyle --style=ansi --min-conditional-indent=0 --pad=oper --unpad=paren tools/*.cpp
+
