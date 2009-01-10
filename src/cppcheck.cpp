327d09502aa74ade9193394b592795f89eea317f
Daniel Marjamäki <hyd_danmar@users.sourceforge.net>
2009-01-10 19:24:23 +0000

    cppcheck: Minor bug fix

--- src/cppcheck.cpp
+++ src/cppcheck.cpp
@@ -272,7 +272,7 @@ void CppCheck::checkFile(const std::string &code, const char FileName[])
         checkMemoryLeak.CheckMemoryLeak();
 
     // Check that all class constructors are ok.
-    if (ErrorMessage::noConstructor(_settings) || ErrorMessage::uninitVar(_settings))
+    if (ErrorMessage::noConstructor(_settings) && ErrorMessage::uninitVar(_settings))
         checkClass.constructors();
 
     // Check that all base classes have virtual destructors
