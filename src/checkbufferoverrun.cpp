f33675288ed629610d749bdf89a705bfb97ccab6
Daniel Marjamäki <hyd_danmar@users.sourceforge.net>
2009-01-23 21:08:51 +0000

    borland: the strtol is found in <stdlib.h>

--- src/checkbufferoverrun.cpp
+++ src/checkbufferoverrun.cpp
@@ -30,7 +30,7 @@
 #include <cstring>
 
 
-#include <cstdlib>     // <- strtoul
+#include <stdlib.h>     // <- strtoul
 
 //---------------------------------------------------------------------------
 
