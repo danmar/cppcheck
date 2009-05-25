1173854c6fce97b12c4345d502199aad4d005ccf
Vesa Pikki <spyree@gmail.com>
2009-05-25 16:00:42 +0300

    GUI now uses Cppcheck's logo.

--- gui/main.cpp
+++ gui/main.cpp
@@ -24,6 +24,8 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    app.setWindowIcon(QIcon("../logo.svg"));
+
     MainWindow window;
     window.show();
     return app.exec();
