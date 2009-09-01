d8f2ec934b713d006059582720667678b7095254
Slava Semushin <php-coder@altlinux.ru>
2009-09-01 23:58:36 +0700

    Tokenizer::syntaxError: show details about unlogged syntax error.

--- src/tokenize.cpp
+++ src/tokenize.cpp
@@ -3503,7 +3503,9 @@ void Tokenizer::syntaxError(const Token *tok, char c)
 
     if (!_errorLogger)
     {
-        std::cout << "### Unlogged error at Tokenizer::syntaxError" << std::endl;
+        std::cout << "### Unlogged error at Tokenizer::syntaxError: Invalid number of character ("
+                  << c << ")"
+                  << std::endl;
         return;
     }
 
