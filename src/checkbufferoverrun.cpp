danmar/cppcheck

commit e8c83613e4bcc95b7c3d91d5c71f4e2363a5cdf3

Fixed #740 (False positive, buffer overrun with --all)
Regression since 07f41f4563fc138b527af6ce153433a661250020 commit.

http://sourceforge.net/apps/trac/cppcheck/ticket/740

src/checkbufferoverrun.cpp
@@ -264,7 +264,7 @@ void CheckBufferOverrun::checkScope(const Token *tok, const char *varname[], con
             // Get index variable and stopsize.
             const char *strindex = tok2->str().c_str();
             bool condition_out_of_bounds = true;
-            if (value < size)
+            if (value <= size)
                 condition_out_of_bounds = false;
 
             const Token *tok3 = tok2->tokAt(4);

test/testbufferoverrun.cpp
@@ -97,6 +97,7 @@ private:
         TEST_CASE(buffer_overrun_7);
         TEST_CASE(buffer_overrun_8);
         TEST_CASE(buffer_overrun_9);
+        TEST_CASE(buffer_overrun_10);
 
         TEST_CASE(sprintf1);
         TEST_CASE(sprintf2);
@@ -640,6 +641,20 @@ private:
               "    {\n"
               "        a[i] = 0;\n"
               "        i += 100;\n"
+              "    }\n"
+              "}\n");
+        ASSERT_EQUALS("", errout.str());
+    }
+
+    void buffer_overrun_10()
+    {
+        // ticket #740
+        check("void f()\n"
+              "{\n"
+              "    char a[4];\n"
+              "    for (int i = 0; i < 4; i++)\n"
+              "    {\n"
+              "        char b = a[i];\n"
               "    }\n"
               "}\n");
         ASSERT_EQUALS("", errout.str());

commit 342e71803f6a663c546c47662c46db15618b386c

src/checkbufferoverrun.cpp: fixed regression.
test suite was broken after my
e8c83613e4bcc95b7c3d91d5c71f4e2363a5cdf3 commit.

src/checkbufferoverrun.cpp
@@ -256,7 +256,7 @@ void CheckBufferOverrun::checkScope(const Token *tok, const char *varname[], con
                 }
                 else if (Token::Match(tok2, "%varid% <= %num% ;", counter_varid))
                 {
-                    value = std::atoi(tok2->strAt(2));
+                    value = std::atoi(tok2->strAt(2)) + 1;
                     max_counter_value = tok2->strAt(2);
                 }
             }