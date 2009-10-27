fad29ca2051754a9f0fe64fc97e0d62da744e805
Kimmo Varis <kimmov@gmail.com>
2009-10-27 23:59:37 +0200

    Make switching QT version easier in build batch files.

--- win_installer/build1.bat
+++ win_installer/build1.bat
@@ -1,7 +1,8 @@
 
-set QTDIR=c:\Qt\2009.01\qt
-set PATH=c:\Qt\2009.01\qt\bin
-set PATH=%PATH%;c:\Qt\2009.01\bin;c:\Qt\2009.01\mingw\bin
+set QTBASE=c:\Qt\2009.01
+set QTDIR=%QTBASE%\qt
+set PATH=%QTDIR%\bin
+set PATH=%PATH%;%QTBASE%\bin;%QTBASE%\mingw\bin
 set PATH=%PATH%;%SystemRoot%\System32
 set QMAKESPEC=win32-g++
 
