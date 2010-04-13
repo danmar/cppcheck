35d2a27b9cd7fe72eeefb51258607457fd26d6a4
Reijo Tomperi <aggro80@users.sourceforge.net>
2010-04-13 22:23:17 +0300

    Update copyright year in all source files

--- gui/translationhandler.h
+++ gui/translationhandler.h
@@ -1,6 +1,6 @@
 /*
  * Cppcheck - A tool for static C/C++ code analysis
- * Copyright (C) 2007-2009 Daniel Marjamäki and Cppcheck team.
+ * Copyright (C) 2007-2010 Daniel Marjamäki and Cppcheck team.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
