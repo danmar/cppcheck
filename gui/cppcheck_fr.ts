3c2a10d0a0f199d313f79a11fa00e8a38469a38d
Guillaume Miossec <guillaume.miossec@gmail.com>
2010-06-04 06:57:21 +0200

    Added french translation

---
+++ gui/cppcheck_fr.ts
@@ -0,0 +1,539 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR" sourcelanguage="en_GB">
+<context>
+    <name>About</name>
+    <message>
+        <source>About Cppcheck</source>
+        <translation>A propos</translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation>Version %1</translation>
+    </message>
+    <message>
+        <source>Cppcheck - A tool for static C/C++ code analysis.</source>
+        <translation>Cppcheck - Un outil d&apos;analyse statique de code C/C++.</translation>
+    </message>
+    <message utf8="true">
+        <source>Copyright (C) 2007-2010 Daniel Marjamäki and cppcheck team.</source>
+        <translation>Copyright (C) 2007-2010 Daniel Marjamäki et cppcheck team.</translation>
+    </message>
+    <message>
+        <source>This program is licensed under the terms
+of the GNU General Public License version 3</source>
+        <translation>Ce programme est sous license GNU
+General Public License version 3</translation>
+    </message>
+    <message>
+        <source>Visit Cppcheck homepage at %1</source>
+        <translation>Visitez le site Cppcheck : %1</translation>
+    </message>
+</context>
+<context>
+    <name>ApplicationDialog</name>
+    <message>
+        <source>Add an application</source>
+        <translation>Ajouter une application</translation>
+    </message>
+    <message>
+        <source>Here you can add applications that can open error files.
+Specify a name for the application and the application to execute.
+
+The following texts are replaced with appropriate values when application is executed:
+(file) - Filename containing the error
+(line) - Line number containing the error
+(message) - Error message
+(severity) - Error severity
+
+Example opening a file with Kate and make Kate scroll to the correct line:
+kate -l(line) (file)</source>
+        <translation type="unfinished">Ici vous pouvez ajouter des applications pour ouvir les rapports d&apos;erreur
+Spécifiez le nom de l&apos;application et l&apos;application à exécuter.
+
+Les textes suivants sont remplacés avec les valeurs appropriées lorsque l&apos;application est lancée :
+(file) - Nom du fichier contenant l&apos;erreur
+(line) - Numéro de ligne où est l&apos;erreur
+(message) - Message d&apos;erreur
+(severity) - Séverité de l&apos;erreur
+
+
+Example : ouvrir un fichier avec kate et position l&apos;affichage sur la bonne ligne:
+kate -l(ligne) (fichier)</translation>
+    </message>
+    <message>
+        <source>Application&apos;s name</source>
+        <translation>Nom de l&apos;application</translation>
+    </message>
+    <message>
+        <source>Application to execute</source>
+        <translation>Application à exécuter</translation>
+    </message>
+    <message>
+        <source>Browse</source>
+        <translation>Parcourir</translation>
+    </message>
+    <message>
+        <source>Executable files (*.exe);;All files(*.*)</source>
+        <translation>Fichier exécutable (*.exe);;Tous les fichiers(*.*)</translation>
+    </message>
+    <message>
+        <source>Select viewer application</source>
+        <translation>Sélection de l&apos;application</translation>
+    </message>
+    <message>
+        <source>Cppcheck</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>You must specify a name and a path for the application!</source>
+        <translation>Vous devez spécifier un nom et un chemin d&apos;accès pour l&apos;application !</translation>
+    </message>
+</context>
+<context>
+    <name>FileViewDialog</name>
+    <message>
+        <source>Could not find the file: %1</source>
+        <translation type="unfinished">Ne trouve pas le fichier : %1</translation>
+    </message>
+    <message>
+        <source>Cppcheck</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Could not read the file: %1</source>
+        <translation type="unfinished">Ne peut pas lire le fichier : %1</translation>
+    </message>
+</context>
+<context>
+    <name>Fileview</name>
+    <message>
+        <source>Fileview</source>
+        <translation type="unfinished">Visualisateur de fichier</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Cppcheck</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation type="unfinished">&amp;Fichier</translation>
+    </message>
+    <message>
+        <source>&amp;View</source>
+        <translation type="unfinished">&amp;Affichage</translation>
+    </message>
+    <message>
+        <source>&amp;Language</source>
+        <translation type="unfinished">&amp;Langue</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation type="unfinished">&amp;Aide</translation>
+    </message>
+    <message>
+        <source>&amp;Check</source>
+        <translation type="unfinished">&amp;Vérifier</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation type="unfinished">&amp;Édition</translation>
+    </message>
+    <message>
+        <source>Standard</source>
+        <translation type="unfinished">Standard</translation>
+    </message>
+    <message>
+        <source>&amp;License...</source>
+        <translation type="unfinished">&amp;License...</translation>
+    </message>
+    <message>
+        <source>A&amp;uthors...</source>
+        <translation type="unfinished">A&amp;uteur...</translation>
+    </message>
+    <message>
+        <source>&amp;About...</source>
+        <translation type="unfinished">À &amp;Propos...</translation>
+    </message>
+    <message>
+        <source>&amp;Files...</source>
+        <translation type="unfinished">&amp;Fichiers...</translation>
+    </message>
+    <message>
+        <source>Ctrl+F</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Directory...</source>
+        <translation type="unfinished">&amp;Répertoires...</translation>
+    </message>
+    <message>
+        <source>Ctrl+D</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Recheck files</source>
+        <translation type="unfinished">&amp;Revérifier les fichiers</translation>
+    </message>
+    <message>
+        <source>Ctrl+R</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Stop</source>
+        <translation type="unfinished">&amp;Arrêter</translation>
+    </message>
+    <message>
+        <source>Esc</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Save results to file...</source>
+        <translation type="unfinished">&amp;Sauvegarder les résultats dans un fichier...</translation>
+    </message>
+    <message>
+        <source>Ctrl+S</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation type="unfinished">&amp;Quitter</translation>
+    </message>
+    <message>
+        <source>&amp;Clear results</source>
+        <translation type="unfinished">&amp;Effacer les résultats</translation>
+    </message>
+    <message>
+        <source>&amp;Preferences</source>
+        <translation type="unfinished">&amp;Préférences</translation>
+    </message>
+    <message>
+        <source>Show possible false positives</source>
+        <translation type="unfinished">Afficher les possibles faux positifs</translation>
+    </message>
+    <message>
+        <source>Show security errors</source>
+        <translation type="unfinished">Afficher les erreurs de sécurité</translation>
+    </message>
+    <message>
+        <source>Show style errors</source>
+        <translation type="unfinished">Afficher les erreurs de style</translation>
+    </message>
+    <message>
+        <source>Show possible style errors</source>
+        <translation type="unfinished">Afficher les erreurs possibles de style</translation>
+    </message>
+    <message>
+        <source>Show common errors</source>
+        <translation type="unfinished">Afficher les erreurs</translation>
+    </message>
+    <message>
+        <source>&amp;Check all</source>
+        <translation type="unfinished">Tout &amp;cocher</translation>
+    </message>
+    <message>
+        <source>&amp;Uncheck all</source>
+        <translation type="unfinished">Tout &amp;décocher</translation>
+    </message>
+    <message>
+        <source>Collapse &amp;all</source>
+        <translation type="unfinished">Tout &amp;réduire</translation>
+    </message>
+    <message>
+        <source>&amp;Expand all</source>
+        <translation type="unfinished">Tout &amp;afficher</translation>
+    </message>
+    <message>
+        <source>&amp;Toolbar</source>
+        <translation type="unfinished">&amp;Barre d&apos;outil</translation>
+    </message>
+    <message>
+        <source>&amp;Contents</source>
+        <translation type="unfinished">&amp;Contenu</translation>
+    </message>
+    <message>
+        <source>Open the help contents</source>
+        <translation type="unfinished">Ouvir l&apos;aide</translation>
+    </message>
+    <message>
+        <source>F1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No suitable files found to check!</source>
+        <translation type="unfinished">Pas de fichiers trouvés !</translation>
+    </message>
+    <message>
+        <source>Select files to check</source>
+        <translation type="unfinished">Sélectionner les fichiers à vérifier</translation>
+    </message>
+    <message>
+        <source>Select directory to check</source>
+        <translation type="unfinished">Sélectionner le répertoire à vérifier</translation>
+    </message>
+    <message>
+        <source>Cannot exit while checking.
+
+Stop the checking before exiting.</source>
+        <translation type="unfinished">Ne peut pas quitter pendant une vérification.
+
+Arrêter la vérification avant de quitter.</translation>
+    </message>
+    <message>
+        <source>License</source>
+        <translation type="unfinished">Licence</translation>
+    </message>
+    <message>
+        <source>Authors</source>
+        <translation type="unfinished">Auteur</translation>
+    </message>
+    <message>
+        <source>XML files (*.xml);;Text files (*.txt);;CSV files (*.csv)</source>
+        <translation type="unfinished">Fichier XML (*.xml);;Fichier Texte (*.txt);;Fichier CSV (*.csv)</translation>
+    </message>
+    <message>
+        <source>Save the report file</source>
+        <translation type="unfinished">Sauvegarder le rapport</translation>
+    </message>
+    <message>
+        <source>XML files (*.xml)</source>
+        <translation type="unfinished">Fichier XML (*.xml)</translation>
+    </message>
+    <message>
+        <source>Text files (*.txt)</source>
+        <translation type="unfinished">Fichier Texte  (*.txt)</translation>
+    </message>
+    <message>
+        <source>CSV files (*.csv)</source>
+        <translation type="unfinished">Fichier CSV (*.csv)</translation>
+    </message>
+    <message>
+        <source>Cppcheck - %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to change the language:
+
+%1
+
+</source>
+        <translation type="unfinished">Erreur lors du chargement de la langue :
+
+%1
+
+</translation>
+    </message>
+    <message>
+        <source>English</source>
+        <translation type="unfinished">Anglais</translation>
+    </message>
+    <message>
+        <source>Dutch</source>
+        <translation type="unfinished">Hollandais</translation>
+    </message>
+    <message>
+        <source>Finnish</source>
+        <translation type="unfinished">Finlandais</translation>
+    </message>
+    <message>
+        <source>Swedish</source>
+        <translation type="unfinished">Suédois</translation>
+    </message>
+    <message>
+        <source>German</source>
+        <translation type="unfinished">Allemand</translation>
+    </message>
+    <message>
+        <source>Russian</source>
+        <translation type="unfinished">Russe</translation>
+    </message>
+    <message>
+        <source>Polish</source>
+        <translation type="unfinished">Polonais</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Incorrect language specified!</source>
+        <translation type="unfinished">Langue incorrecte !</translation>
+    </message>
+    <message>
+        <source>Language file %1 not found!</source>
+        <translation type="unfinished">Fichier de langue %1 non trouvé !</translation>
+    </message>
+    <message>
+        <source>Failed to load translation for language %1 from file %2</source>
+        <translation type="unfinished">Erreur lors du chargement de la langue %1 depuis le fichier %2</translation>
+    </message>
+</context>
+<context>
+    <name>ResultsTree</name>
+    <message>
+        <source>File</source>
+        <translation type="unfinished">Fichier</translation>
+    </message>
+    <message>
+        <source>Severity</source>
+        <translation type="unfinished">Sévérité</translation>
+    </message>
+    <message>
+        <source>Line</source>
+        <translation type="unfinished">Ligne</translation>
+    </message>
+    <message>
+        <source>Message</source>
+        <translation type="unfinished">Message</translation>
+    </message>
+    <message>
+        <source>Undefined file</source>
+        <translation type="unfinished">Fichier indéterminé</translation>
+    </message>
+    <message>
+        <source>Copy filename</source>
+        <translation type="unfinished">Copier nom du fichier</translation>
+    </message>
+    <message>
+        <source>Copy full path</source>
+        <translation type="unfinished">Copier le chemin complet</translation>
+    </message>
+    <message>
+        <source>Copy message</source>
+        <translation type="unfinished">Copier le message</translation>
+    </message>
+    <message>
+        <source>Cppcheck</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Configure the text file viewer program in Cppcheck preferences/Applications.</source>
+        <translation type="unfinished">Configurez l&apos;application d&apos;affichage des fichers dans préférences/Applications.</translation>
+    </message>
+    <message>
+        <source>Could not start %1
+
+Please check the application path and parameters are correct.</source>
+        <translation type="unfinished">Ne peut pas démarrer %1
+
+Merci de vérifier que le chemin de l&apos;application et que les paramètres sont corrects.</translation>
+    </message>
+    <message>
+        <source>possible error</source>
+        <translation type="unfinished">erreur possible</translation>
+    </message>
+    <message>
+        <source>style</source>
+        <translation type="unfinished">erreur de style</translation>
+    </message>
+    <message>
+        <source>possible style</source>
+        <translation type="unfinished">erreur de style possible</translation>
+    </message>
+    <message>
+        <source>error</source>
+        <translation type="unfinished">erreur</translation>
+    </message>
+</context>
+<context>
+    <name>ResultsView</name>
+    <message>
+        <source>Results</source>
+        <translation type="unfinished">Résultats</translation>
+    </message>
+    <message>
+        <source>Cppcheck</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No errors found.</source>
+        <translation type="unfinished">Pas d&apos;erreurs trouvées.</translation>
+    </message>
+    <message>
+        <source>Errors were found, but they are configured to be hidden.
+To toggle what kind of errors are shown, open view menu.</source>
+        <translation type="unfinished">Des erreurs ont été trouvées mais sont configurées pour rester cachées
+Pour configurer les erreurs affichées, ouvrez le menu d&apos;affichage.</translation>
+    </message>
+    <message>
+        <source>No errors found, nothing to save.</source>
+        <translation type="unfinished">Pas d&apos;erreurs trouvées, rien à sauvegarder.</translation>
+    </message>
+    <message>
+        <source>Failed to save the report.</source>
+        <translation type="unfinished">Erreur lors de la sauvegarde du rapport.</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Preferences</source>
+        <translation type="unfinished">Préférences</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation type="unfinished">Général</translation>
+    </message>
+    <message>
+        <source>Number of threads: </source>
+        <translation type="unfinished">Nombre de processus : </translation>
+    </message>
+    <message>
+        <source>Check all #ifdef configurations</source>
+        <translation type="unfinished">Vérifier toutes les configurations #ifdef</translation>
+    </message>
+    <message>
+        <source>Show full path of files</source>
+        <translation type="unfinished">Montrer le chemin complet des fichiers</translation>
+    </message>
+    <message>
+        <source>Show &quot;No errors found&quot; message when no errors found</source>
+        <translation type="unfinished">Afficher un message &quot;Pas d&apos;erreur trouvée&quot; lorsque aucune erreur est trouvée</translation>
+    </message>
+    <message>
+        <source>Applications</source>
+        <translation type="unfinished">Applications</translation>
+    </message>
+    <message>
+        <source>Add application</source>
+        <translation type="unfinished">Ajouter une application</translation>
+    </message>
+    <message>
+        <source>Delete application</source>
+        <translation type="unfinished">Supprimer l&apos;application</translation>
+    </message>
+    <message>
+        <source>Modify application</source>
+        <translation type="unfinished">Modifier l&apos;application</translation>
+    </message>
+    <message>
+        <source>Set as default application</source>
+        <translation type="unfinished">Définir comme application par défaut</translation>
+    </message>
+    <message>
+        <source>Reports</source>
+        <translation type="unfinished">Rapports</translation>
+    </message>
+    <message>
+        <source>Save all errors when creating report</source>
+        <translation type="unfinished">Sauvegarder toutes les erreurs lorsqu&apos;un rapport est créé</translation>
+    </message>
+    <message>
+        <source>Save full path to files in reports</source>
+        <translation type="unfinished">Sauvegarder le chemin complet des fichiers dans les rapports</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <source>Add a new application</source>
+        <translation type="unfinished">Ajouter une nouvelle application</translation>
+    </message>
+    <message>
+        <source>Modify an application</source>
+        <translation type="unfinished">Modifier une application</translation>
+    </message>
+</context>
+</TS>
