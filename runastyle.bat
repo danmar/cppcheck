66098b1c33e4b54c4c08e2286152ff6bb9f1f313
Kimmo Varis <kimmov@gmail.com>
2010-07-24 15:21:57 +0300

    Ticket #1899 Update Astyle flags to match older Astyle behavior.

--- runastyle.bat
+++ runastyle.bat
@@ -1,12 +1,12 @@
 
-astyle --style=ansi --min-conditional-indent=0 cli/*.cpp
-astyle --style=ansi --min-conditional-indent=0 cli/*.h
-astyle --style=ansi --min-conditional-indent=0 gui/*.cpp
-astyle --style=ansi --min-conditional-indent=0 gui/*.h
-astyle --style=ansi --min-conditional-indent=0 lib/*.cpp
-astyle --style=ansi --min-conditional-indent=0 lib/*.h
-astyle --style=ansi --min-conditional-indent=0 test/*.cpp
-astyle --style=ansi --min-conditional-indent=0 test/*.h
+astyle --style=ansi --min-conditional-indent=0 --pad-header cli/*.cpp
+astyle --style=ansi --min-conditional-indent=0 --pad-header cli/*.h
+astyle --style=ansi --min-conditional-indent=0 --pad-header gui/*.cpp
+astyle --style=ansi --min-conditional-indent=0 --pad-header gui/*.h
+astyle --style=ansi --min-conditional-indent=0 --pad-header lib/*.cpp
+astyle --style=ansi --min-conditional-indent=0 --pad-header lib/*.h
+astyle --style=ansi --min-conditional-indent=0 --pad-header test/*.cpp
+astyle --style=ansi --min-conditional-indent=0 --pad-header test/*.h
 
-astyle --style=ansi --min-conditional-indent=0 tools/*.cpp
+astyle --style=ansi --min-conditional-indent=0 --pad-header tools/*.cpp
 
