6e676fdd98868d7e853d8532d55d923732c9f791
Daniel Marjamäki <danielm77@spray.se>
2010-08-29 09:35:21 +0200

    astyle: prevent creating backup files

--- runastyle.bat
+++ runastyle.bat
@@ -1,7 +1,7 @@
 REM A script to run Astyle for the sources
 
 SET STYLE=--style=ansi --min-conditional-indent=0
-SET OPTIONS=--pad-header --unpad-paren
+SET OPTIONS=--pad-header --unpad-paren --suffix=none
 
 astyle %STYLE% %OPTIONS% cli/*.cpp
 astyle %STYLE% %OPTIONS% cli/*.h
