a7ee5a048896b48cf98a242a8408156d2a01e0cd
Kimmo Varis <kimmov@gmail.com>
2010-09-06 20:50:48 +0300

    Add some comments for the CmdLineParser class.
    Also remove one unneeded forward declaration.

--- cli/cmdlineparser.h
+++ cli/cmdlineparser.h
@@ -22,28 +22,65 @@
 #include <vector>
 #include <string>
 
-class CppCheck;
 class Settings;
 
+/// @addtogroup CLI
+/// @{
+
+/**
+ * @brief The command line parser.
+ * The command line parser parses options and parameters user gives to
+ * cppcheck command line.
+ *
+ * The parser takes a pointer to Settings instance which it will update
+ * based on options user has given. Couple of options are handled as
+ * class internal options.
+ */
 class CmdLineParser
 {
 public:
+    /**
+     * The constructor.
+     * @param settings Settings instance that will be modified accoding to
+     * options user has given.
+     */
     CmdLineParser(Settings *settings);
+
+    /**
+     * Parse given command line.
+     * @return true if command line was ok, false if there was an error.
+     */
     bool ParseFromArgs(int argc, const char* const argv[]);
+
+    /**
+     * Return if user wanted to see program version.
+     */
     bool GetShowVersion() const
     {
         return _showVersion;
     }
+
+    /**
+     * Return if user wanted to see list of error messages.
+     */
     bool GetShowErrorMessages() const
     {
         return _showErrorMessages;
     }
+
+    /**
+     * Return the path names user gave to command line.
+     */
     std::vector<std::string> GetPathNames() const
     {
         return _pathnames;
     }
 
 protected:
+
+    /**
+     * Print help text to the console.
+     */
     void PrintHelp();
 
 private:
@@ -53,5 +90,7 @@ private:
     bool _showErrorMessages;
     std::vector<std::string> _pathnames;
 };
+
+/// @}
 
 #endif // CMDLINE_PARSER_H
