b705a9cbddbd5d45f165a3fa3f81f7742e84ab42
Daniel Marjamäki <danielm77@spray.se>
2010-12-06 20:39:38 +0100

    Writing rules: converting the docbook to pdf with buildman.sh

--- man/buildman.sh
+++ man/buildman.sh
@@ -3,6 +3,8 @@
 xsltproc -o manual.html /usr/share/xml/docbook/stylesheet/nwalsh/xhtml/docbook.xsl manual.docbook
 
 xsltproc -o intermediate-fo-file.fo /usr/share/xml/docbook/stylesheet/nwalsh/fo/docbook.xsl manual.docbook
-
 fop -pdf manual.pdf -fo intermediate-fo-file.fo
+
+xsltproc --stringparam generate.toc "article nop" -o intermediate-fo-file.fo /usr/share/xml/docbook/stylesheet/nwalsh/fo/docbook.xsl writing-rules.docbook
+fop -pdf writing-rules.pdf -fo intermediate-fo-file.fo
 
