a5cf06f616f587b9cfd12e1a5db48c0ebead57a7
Daniel Marjamäki <danielm77@spray.se>
2011-01-10 20:16:10 +0100

    removed unused htdocs/build.bat

--- htdocs/build.bat
+++
@@ -1,7 +0,0 @@
-
-cd..
-
-msbuild cppcheck.cbproj /target:clean
-msbuild cppcheck.cbproj /target:build /property:"config=release" > htdocs\bcb.txt
-
-cppcheck --all --style --unused-functions src 2> htdocs\cppcheck-results.txt
