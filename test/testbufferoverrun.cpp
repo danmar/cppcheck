c7821675dd712e864bba9d54ef6d46dbde0a274b
Erik Lax <erik@datahack.se>
2011-02-11 18:57:58 +0100

    Preprocessor: Test handling of strings with multiple spaces (Ticket: #2548)

--- test/testbufferoverrun.cpp
+++ test/testbufferoverrun.cpp
@@ -133,6 +133,7 @@ private:
         TEST_CASE(buffer_overrun_14);
         TEST_CASE(buffer_overrun_15); // ticket #1787
         TEST_CASE(buffer_overrun_16);
+        TEST_CASE(buffer_overrun_17); // ticket #2548
         TEST_CASE(buffer_overrun_bailoutIfSwitch);  // ticket #2378 : bailoutIfSwitch
 
         // It is undefined behaviour to point out of bounds of an array
@@ -1858,6 +1859,15 @@ private:
               "    std::memcpy(b, a, sizeof(a));\n"
               "}\n");
         ASSERT_EQUALS("", errout.str());
+    }
+
+    void buffer_overrun_17() // ticket #2548
+    {
+        check("void f() {\n"
+              "    char t[8];\n"
+              "    sprintf(t, \"%s\", \"foo     bar\");\n"
+              "}\n");
+        ASSERT_EQUALS("[test.cpp:3]: (error) Buffer access out-of-bounds\n", errout.str());
     }
 
     void buffer_overrun_bailoutIfSwitch()
