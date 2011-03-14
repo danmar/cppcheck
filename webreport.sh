2920dfc08b05b88ce06762c9625e1c93694fa0b0
Daniel Marjamäki <danielm77@spray.se>
2011-03-14 13:40:10 +0100

    webreport: refactoring

--- webreport.sh
+++ webreport.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-doxygen 2> htdocs/doxygen-errors.txt
 ./generate_coverage_report
-rm -R coverage_report/bits
-rm -R coverage_report/ext
-rm -R coverage_report/home/daniel/cppcheck/test
-rm -R coverage_report/i486-linux-gnu
-rm -R coverage_report/usr
 
-rm -R htdocs/doxyoutput
-rm -R htdocs/coverage_report
+rm -rf devinfo
+mkdir devinfo
 
-mv doxyoutput/html htdocs/doxyoutput
-mv coverage_report htdocs/
+mv coverage_report devinfo/
+
+doxygen 2> devinfo/doxygen-errors.txt
+mv doxyoutput/html devinfo/doxyoutput
 
 # Detect duplicate code..
-~/pmd-4.2.5/bin/cpd.sh lib/ > htdocs/cpd.txt
+~/pmd-4.2.5/bin/cpd.sh lib/ > devinfo/cpd.txt
 
-scp -r htdocs hyd_danmar,cppcheck@web.sourceforge.net:/home/groups/c/cp/cppcheck/
+#scp -r devinfo/ hyd_danmar,cppcheck@web.sourceforge.net:/home/groups/c/cp/cppcheck/htdocs
+
