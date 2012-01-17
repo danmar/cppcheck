3066bb3430c8ce7dab1dda27f04081d2492ecb5a
Reijo Tomperi <aggro80@users.sourceforge.net>
2012-01-17 23:03:21 +0200

    Start fixing #3515 (Add samples/id/good|bad.cpp) http://sourceforge.net/apps/trac/cppcheck/ticket/3515

---
+++ samples/AssignmentAddressToInteger/bad.c
@@ -0,0 +1,11 @@
+int foo(int *p)
+{
+    int a = p;
+    return a + 4;
+}
+int main()
+{
+    int i[10];
+    foo(i);
+}
+
---
+++ samples/AssignmentAddressToInteger/good.c
@@ -0,0 +1,11 @@
+int* foo(int *p)
+{
+    return p + 4;
+}
+int main()
+{
+    int i[10];
+    foo(i);
+}
+
+
