2de6ea4627a9d38ab904078d3b0e52addc64c415
PKEuS <philipp.kloke@web.de>
2012-07-02 11:10:07 -0700

    Incremented version to "1.56 dev"

--- lib/cppcheck.cpp
+++ lib/cppcheck.cpp
@@ -34,7 +34,7 @@
 #include <pcre.h>
 #endif
 
-static const char Version[] = "1.55";
+static const char Version[] = "1.56 dev";
 static const char ExtraVersion[] = "";
 
 static TimerResults S_timerResults;
