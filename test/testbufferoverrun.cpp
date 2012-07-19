4312d31b87eb5d4ad54795349c314dab86ca01bc
Ettl Martin <ettl.martin78@googlemail.com>
2012-07-19 09:44:06 +0200

    #3979 added todo testcases

--- test/testbufferoverrun.cpp
+++ test/testbufferoverrun.cpp
@@ -111,6 +111,7 @@ private:
         TEST_CASE(array_index_41); // structs with the same name
         TEST_CASE(array_index_42);
         TEST_CASE(array_index_43); // struct with array
+        TEST_CASE(array_index_44); // #3979
         TEST_CASE(array_index_multidim);
         TEST_CASE(array_index_switch_in_for);
         TEST_CASE(array_index_for_in_for);   // FP: #2634
@@ -1422,6 +1423,30 @@ private:
               "   return y;\n"
               "}\n");
         ASSERT_EQUALS("", errout.str());
+    }
+
+    void array_index_44() { // #3979 (false positive)
+
+        check("void f()\n"
+              "{\n"
+              "    char buf[2];\n"
+              "    int i;\n"
+              "    for (i = 2; --i >= 0; )\n"
+              "    {\n"
+              " 	   buf[i] = 1;\n"
+              "    }\n"
+              "}\n");
+        TODO_ASSERT_EQUALS("","[test.cpp:7]: (error) Buffer is accessed out of bounds: buf\n", errout.str());
+
+        check("void f()\n"
+              "{\n"
+              "    double	 buf[2];\n"
+              "    for (int i = 2; i--; )\n"
+              "    {\n"
+              " 	   buf[i] = 2.;\n"
+              "    }\n"
+              "}\n");
+        TODO_ASSERT_EQUALS("","[test.cpp:6]: (error) Buffer is accessed out of bounds: buf\n", errout.str());
     }
 
     void array_index_multidim() {
