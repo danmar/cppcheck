263e13e9246b447256d9c588f4e50449cb7f90ab
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2012-08-27 06:33:56 +0200

    astyle formatting

--- lib/token.h
+++ lib/token.h
@@ -374,7 +374,7 @@ public:
      */
     void link(Token *linkToToken) {
         _link = linkToToken;
-        if(_str == "<" || _str == ">")
+        if (_str == "<" || _str == ">")
             update_property_info();
     }
 
