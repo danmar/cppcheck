e93679b3b25c5f7cb66b798f382fa93efdd333f6
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2012-09-08 18:40:47 +0200

    democlient: updated build script to match the script I have on the shell. Removed the old 'info.txt'.

--- democlient/build.sh
+++ democlient/build.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
-g++ -O2 -o democlient.cgi -Icppcheck-1.55/lib democlient.cpp cppcheck-1.55/lib/*.cpp
-mv democlient.cgi /home/project-web/cppcheck/cgi-bin/
+
+# this script downloads and builds the democlient
+# syntax:
+# ./build 1.56
+
+# cppcheck lib folder
+cppchecklib=cppcheck-$1/lib
+
+echo Download..
+wget https://downloads.sourceforge.net/project/cppcheck/cppcheck/$1/cppcheck-$1.tar.bz2
+
+echo Unpack..
+tar xjvf cppcheck-$1.tar.bz2
+rm cppcheck-$1.tar.bz2
+rm cppcheck-$1/Changelog
+
+echo Building..
+g++ -O2 -o democlient-$1.cgi -I$cppchecklib cppcheck-$1/democlient/democlient.cpp $cppchecklib/*.cpp
+
+echo Copy cgi to webspace..
+cp democlient-$1.cgi /home/project-web/cppcheck/cgi-bin/democlient.cgi
 chmod +rx /home/project-web/cppcheck/cgi-bin/democlient.cgi
+
+echo Done!
+
