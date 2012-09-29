4df326051d2e1713a6e870292a863ddf56cd2b1f
Tim Gerundt <tim@gerundt.de>
2012-09-29 14:41:18 +0200

    htdocs: Move some CSS rules to geshi(.min).css file

--- htdocs/demo/index.php
+++ htdocs/demo/index.php
@@ -5,6 +5,7 @@
   <title>Online Demo - Cppcheck</title>
   <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Orbitron&amp;text=Cppcheck" />
   <link rel="stylesheet" type="text/css" href="/site/css/all.min.css" />
+  <link rel="stylesheet" type="text/css" href="/site/css/geshi.min.css" />
   <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
   <script type="text/javascript">
     function checkCodeLength() {
