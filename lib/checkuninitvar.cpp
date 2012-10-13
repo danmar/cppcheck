8324de1dc000a7f7fd375b300a17edf4b1021645
Edoardo Prezioso <edo88@email.it>
2012-10-13 14:59:34 +0200

    Fixed 'tokAt(-2)->link() -> linkAt(-2)' thanks to cppcheck internal check.

--- lib/checkuninitvar.cpp
+++ lib/checkuninitvar.cpp
@@ -474,7 +474,7 @@ private:
                 // variable is assigned like: "( %var% ) .. ="
                 if (Token::Match(tok.previous(), "( %var% )") && tok2 && tok2->str() == "=")
                     ExecutionPath::bailOutVar(checks, tok.varId());
-                else if (!(tok.strAt(-2) == ">" && tok.tokAt(-2)->link()))
+                else if (tok.strAt(-2) != ">" || !tok.linkAt(-2))
                     use(checks, &tok);
                 //use_array(checks, &tok);
                 return &tok;
