d7c7f8c9afb00345a4f6ac7d591fa0e8bd7b9e71
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2012-10-13 08:18:11 +0200

    astyle: Added --convert-tabs option

--- runastyle.bat
+++ runastyle.bat
@@ -1,7 +1,7 @@
 REM A script to run Astyle for the sources
 
 SET STYLE=--style=stroustrup --indent=spaces=4 --indent-namespaces --lineend=linux --min-conditional-indent=0
-SET OPTIONS=--pad-header --unpad-paren --suffix=none
+SET OPTIONS=--pad-header --unpad-paren --suffix=none --convert-tabs
 
 astyle %STYLE% %OPTIONS% cli/*.cpp
 astyle %STYLE% %OPTIONS% cli/*.h
