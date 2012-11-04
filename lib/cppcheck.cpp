3a44a5ca3c322c07053998e692070614a2e5c72a
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2012-11-04 13:50:23 +0100

    1.57: Setting version

--- lib/cppcheck.cpp
+++ lib/cppcheck.cpp
@@ -34,7 +34,7 @@
 #include <pcre.h>
 #endif
 
-static const char Version[] = "1.57 dev";
+static const char Version[] = "1.57";
 static const char ExtraVersion[] = "";
 
 static TimerResults S_timerResults;
--- lib/version.h
+++ lib/version.h
@@ -1,3 +1,3 @@
-#define CPPCHECK_VERSION_STRING "1.57 dev"
-#define CPPCHECK_VERSION 0,0,0,0
+#define CPPCHECK_VERSION_STRING "1.57"
+#define CPPCHECK_VERSION 1,57,0,0
 #define LEGALCOPYRIGHT L"Copyright (C) 2007-2012 Daniel Marjam\x00E4ki and Cppcheck team."
