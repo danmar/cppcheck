7fb5c0d73bcb4de6fdbc4774aaebc81f49a38336
XhmikosR <xhmikosr@users.sourceforge.net>
2012-11-15 14:05:16 +0200

    minify.bat: update workaround for node.js exiting

--- htdocs/site/minify.bat
+++ htdocs/site/minify.bat
@@ -1,7 +1,7 @@
 @echo off
 pushd %~dp0
 
-cleancss -o css/all.min.css css/all.css && ^
-cleancss -o css/demo.min.css css/demo.css && ^
-uglifyjs -o js/github.min.js js/github.js && ^
-uglifyjs -o js/picnet.table.filter.min.pack.js js/picnet.table.filter.min.js
+cmd /c cleancss -o css/all.min.css css/all.css
+cmd /c cleancss -o css/demo.min.css css/demo.css
+cmd /c uglifyjs -o js/github.min.js js/github.js
+cmd /c uglifyjs -o js/picnet.table.filter.min.pack.js js/picnet.table.filter.min.js
