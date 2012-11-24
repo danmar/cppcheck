25b76894f7e06d7a098a7a10f5f602ffd882264b
XhmikosR <xhmikosr@users.sourceforge.net>
2012-11-24 09:20:03 +0200

    htdocs: switch to uglify-js 2.x

--- htdocs/site/minify.bat
+++ htdocs/site/minify.bat
@@ -3,5 +3,5 @@ pushd %~dp0
 
 cmd /c cleancss -o css/all.min.css css/all.css
 cmd /c cleancss -o css/demo.min.css css/demo.css
-cmd /c uglifyjs -o js/github.min.js js/github.js
-cmd /c uglifyjs -o js/picnet.table.filter.min.pack.js js/picnet.table.filter.min.js
+cmd /c uglifyjs js/github.js -o js/github.min.js -c -m
+cmd /c uglifyjs js/picnet.table.filter.min.js -o js/picnet.table.filter.min.pack.js -c -m
