b7501a1db02375a19afd312dedb687a493f17f94
XhmikosR <xhmikosr@users.sourceforge.net>
2012-12-22 14:18:47 +0200

    buildhelp.bat: Add missing popd

--- gui/help/buildhelp.bat
+++ gui/help/buildhelp.bat
@@ -1,7 +1,10 @@
 @echo off
+
 pushd %~dp0
 
 if exist online-help.qhc del online-help.qhc
 if exist online-help.qch del online-help.qch
 
 qcollectiongenerator online-help.qhcp -o online-help.qhc
+
+popd
