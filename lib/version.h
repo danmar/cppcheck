049c995c99c9a290e0f0c19448c1e4ff73eec651
XhmikosR <xhmikosr@users.sourceforge.net>
2013-01-07 13:23:56 +0200

    version.h: update copyright year

--- lib/version.h
+++ lib/version.h
@@ -1,3 +1,3 @@
 #define CPPCHECK_VERSION_STRING "1.58 dev"
 #define CPPCHECK_VERSION 1,57,99,0
-#define LEGALCOPYRIGHT L"Copyright (C) 2007-2012 Daniel Marjam\x00E4ki and Cppcheck team."
+#define LEGALCOPYRIGHT L"Copyright (C) 2007-2013 Daniel Marjam\x00E4ki and Cppcheck team."
