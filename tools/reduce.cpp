fdcfbd5ff94c752e8525b63b3ec04dcba0222333
Thomas Jarosch <thomas.jarosch@intra2net.com>
2013-01-10 11:10:45 +0100

    Add missing copyright header

--- tools/reduce.cpp
+++ tools/reduce.cpp
@@ -1,4 +1,20 @@
-
+/*
+ * Cppcheck - A tool for static C/C++ code analysis
+ * Copyright (C) 2007-2013 Daniel Marjamäki and Cppcheck team.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -657,4 +673,3 @@ int main(int argc, char *argv[])
 
     return EXIT_SUCCESS;
 }
-
