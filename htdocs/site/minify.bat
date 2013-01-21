55b8ac00b080f55e71a1f5ce73016ed54b03323f
XhmikosR <xhmikosr@users.sourceforge.net>
2013-01-21 12:35:54 +0200

    htdocs: add normalize.css to ensure that the visual styles are the same across all browsers

--- htdocs/site/minify.bat
+++ htdocs/site/minify.bat
@@ -5,7 +5,7 @@ rem npm install -g uglify-js
 
 pushd %~dp0
 
-type css\all.css css\demo.css | cleancss -o css\pack.css
+type css\all.css css\demo.css css\normalize.css | cleancss -o css\pack.css
 cmd /c uglifyjs js/github.js js/picnet.table.filter.min.js -o js/pack.js -c -m
 
 popd
