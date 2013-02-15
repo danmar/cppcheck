c64174ab3638e43f9bb444e85ac122fe3a2dfc39
XhmikosR <xhmikosr@users.sourceforge.net>
2013-02-15 09:41:01 +0200

    htdocs: update minify scripts

--- htdocs/site/minify.bat
+++ htdocs/site/minify.bat
@@ -5,7 +5,7 @@ rem npm install -g uglify-js
 
 pushd %~dp0
 
-type css\all.css css\demo.css css\normalize.css | cleancss -o css\pack.css
-cmd /c uglifyjs js/github.js js/picnet.table.filter.min.js -o js/pack.js -c -m
+type css\all.css css\demo.css css\normalize.css | cleancss --s0 -o css\pack.css
+cmd /c uglifyjs js/github.js js/picnet.table.filter.min.js -o js/pack.js --compress --mangle
 
 popd
