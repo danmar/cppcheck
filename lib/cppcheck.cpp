14c3b390a47563a0f23bd20996585a570bd7c7c6
PKEuS <philipp@kloke-witten.dyndns.org>
2013-07-04 13:32:45 +0200

    Fixed version number

--- lib/cppcheck.cpp
+++ lib/cppcheck.cpp
@@ -34,7 +34,7 @@
 #include <pcre.h>
 #endif
 
-static const char Version[] = "1.60.1";
+static const char Version[] = "1.61 dev";
 static const char ExtraVersion[] = "";
 
 static TimerResults S_timerResults;
