07b8607bf897233d6af9791050faaf620adfa088
XhmikosR <xhmikosr@users.sourceforge.net>
2013-07-06 21:43:11 +0200

    democlient: updated build script

--- democlient/build.sh
+++ democlient/build.sh
@@ -2,13 +2,13 @@
 
 # this script downloads and builds the democlient
 # syntax:
-# ./build 1.56
+# ./build 1.60.1
 
 # cppcheck lib folder
 cppchecklib=cppcheck-$1/lib
 
 echo Downloading...
-wget https://downloads.sourceforge.net/project/cppcheck/cppcheck/$1/cppcheck-$1.tar.bz2
+wget http://downloads.sourceforge.net/project/cppcheck/cppcheck/$1/cppcheck-$1.tar.bz2
 
 echo Unpacking...
 tar xjvf cppcheck-$1.tar.bz2
@@ -16,7 +16,7 @@ rm cppcheck-$1.tar.bz2
 rm cppcheck-$1/Changelog
 
 echo Building...
-g++ -O2 -o democlient-$1.cgi -I$cppchecklib cppcheck-$1/democlient/democlient.cpp $cppchecklib/*.cpp
+g++ -O2 -o democlient-$1.cgi -I$cppchecklib -Icppcheck-$1/externals/tinyxml cppcheck-$1/democlient/democlient.cpp $cppchecklib/*.cpp cppcheck-$1/externals/tinyxml/tinyxml2.cpp
 
 echo Copy cgi to webspace...
 cp democlient-$1.cgi /home/project-web/cppcheck/cgi-bin/democlient.cgi
