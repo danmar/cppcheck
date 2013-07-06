8d74d6649df48868f49058b0fd386f3b2d162518
XhmikosR <xhmikosr@users.sourceforge.net>
2013-07-06 18:47:13 +0300

    Convert submitfile.html to index.php.
    Use our style, add the navbar/logo and fix validation errors.

---
+++ htdocs/archive/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="description" content="Cppcheck is an analysis tool for C/C++ code.
+It detects the types of bugs that the compilers normally fail to detect. The
+goal is no false positives." />
+  <meta name="keywords" content="Cppcheck, open source, analysis tool, C/C++,
+code, errors, bugs, compilers, bounds checking, memory leaks, obsolete functions,
+uninitialized variables, unused functions" />
+  <title>Cppcheck - Archive</title>
+  <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Orbitron&amp;text=Cppcheck" />
+  <link rel="stylesheet" type="text/css" href="/site/css/pack.css" />
+  <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+  <link rel="alternate" type="application/rss+xml" title="Project News"
+        href="http://sourceforge.net/export/rss2_projnews.php?group_id=195752" />
+</head>
+<body>
+<div id="header">
+  <div class="wrap">
+    <h1>Cppcheck</h1>
+    <p>A tool for static C/C++ code analysis</p>
+  </div> <!-- .wrap -->
+</div> <!-- #header -->
+<div id="tabs">
+  <div class="wrap">
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="http://sourceforge.net/apps/mediawiki/cppcheck/">Wiki</a></li>
+      <li><a href="http://sourceforge.net/apps/phpbb/cppcheck/">Forum</a></li>
+      <li><a href="http://sourceforge.net/apps/trac/cppcheck/">Issues</a></li>
+      <li><a href="/devinfo/" title="Developer Information">Developer Info</a></li>
+      <li><a href="/demo/">Online Demo</a></li>
+      <li><a href="http://sourceforge.net/projects/cppcheck/">Project page</a></li>
+    </ul>
+  </div> <!-- .wrap -->
+</div> <!-- #tabs -->
+<div id="content">
+  <div class="wrap">
+    <h1>Submit file</h1>
+    <form action="http://cppcheck.sf.net/cgi-bin/submit.cgi" method="get">
+      <p>Name:</p>
+      <input name="name" size="30" />
+      <p>Copy/paste the file data:</p>
+      <textarea name="data" rows="10" cols="40"></textarea>
+      <br />
+      <br />
+      <input type="submit" value="Submit" />
+    </form>
+  </div> <!-- .wrap -->
+</div> <!-- #content -->
+</body>
+</html>
