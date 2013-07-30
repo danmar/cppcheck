9656e71c036ff80ce4e3de800f9ba63a55134009
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2013-07-30 20:36:32 +0200

    archive: replace &amp; since it doesn't work well

--- htdocs/archive/index.php
+++ htdocs/archive/index.php
@@ -22,13 +22,13 @@ uninitialized variables, unused functions" />
     }
 
     function editFile(name,version) {
-        window.location = "http://cppcheck.sourceforge.net/cgi-bin/edit.cgi?name=" + name + "&amp;version=" + version;
+        window.location = "http://cppcheck.sourceforge.net/cgi-bin/edit.cgi?name=" + name + "&version=" + version;
     }
 
     function renameFile(name1,version) {
         var name2 = prompt("Name", name1);
         if (name2 != null)
-            window.location = "http://cppcheck.sourceforge.net/cgi-bin/renamefile.cgi?name1=" + name1 + "&amp;name2=" + name2;
+            window.location = "http://cppcheck.sourceforge.net/cgi-bin/renamefile.cgi?name1=" + name1 + "&name2=" + name2;
     }
 
     function deleteFile(name,version) {
