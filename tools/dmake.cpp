e39dc546c16871f239db767315aeb7b2a4f7e224
Lauri Nurmi <lanurmi@iki.fi>
2013-10-27 23:09:18 +0200

    As a special case, add lib/cxx11emu.h as a dependency for each source file.

--- tools/dmake.cpp
+++ tools/dmake.cpp
@@ -79,6 +79,7 @@ static void compilefiles(std::ostream &fout, const std::vector<std::string> &fil
     for (unsigned int i = 0; i < files.size(); ++i) {
         fout << objfile(files[i]) << ": " << files[i];
         std::vector<std::string> depfiles;
+        depfiles.push_back("lib/cxx11emu.h");
         getDeps(files[i], depfiles);
         for (unsigned int dep = 0; dep < depfiles.size(); ++dep)
             fout << " " << depfiles[dep];
