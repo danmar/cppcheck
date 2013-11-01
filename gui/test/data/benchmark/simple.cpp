38471babf9233773b414335146a6a2059aa05f96
XhmikosR <xhmikosr@users.sourceforge.net>
2013-11-01 13:36:12 +0200

    Reduce variable scope.

--- gui/test/data/benchmark/simple.cpp
+++ gui/test/data/benchmark/simple.cpp
@@ -1356,7 +1356,6 @@ static int doAssignment(Variables &variables, const Token *tok, bool dereference
     Variables::VariableUsage *var1 = variables.find(varid1);
 
     if (var1) {
-        Variables::VariableUsage *var2 = 0;
         int start = 1;
 
         // search for '='
@@ -1437,7 +1436,7 @@ static int doAssignment(Variables &variables, const Token *tok, bool dereference
 
             // check if variable is local
             varid2 = tok->tokAt(next)->varId();
-            var2 = variables.find(varid2);
+            Variables::VariableUsage *var2 = variables.find(varid2);
 
             if (var2) { // local variable (alias or read it)
                 if (var1->_type == Variables::pointer) {
