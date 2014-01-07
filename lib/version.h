17818d43aa00635064e6d7f7ff0874ef65b55ea5
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2014-01-07 20:58:43 +0100

    1.63.1: set versions

--- lib/version.h
+++ lib/version.h
@@ -1,3 +1,3 @@
 #define CPPCHECK_VERSION_STRING "1.63"
-#define CPPCHECK_VERSION 1,63,0,0
+#define CPPCHECK_VERSION 1,63,1,0
 #define LEGALCOPYRIGHT L"Copyright (C) 2007-2013 Daniel Marjam\x00E4ki and Cppcheck team."
