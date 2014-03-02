4842115a567514dec40e28f749d6f39f18dc450f
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2014-03-02 09:55:39 +0100

    1.64 : Update versions

--- lib/version.h
+++ lib/version.h
@@ -1,3 +1,3 @@
-#define CPPCHECK_VERSION_STRING "1.64 dev"
-#define CPPCHECK_VERSION 1,63,99,0
+#define CPPCHECK_VERSION_STRING "1.64"
+#define CPPCHECK_VERSION 1,64,0,0
 #define LEGALCOPYRIGHT L"Copyright (C) 2007-2014 Daniel Marjam\x00E4ki and Cppcheck team."
