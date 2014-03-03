a9df4a3288e4fbca6a5acc8bc09adae3c00ac129
Matthias Krüger <matthias.krueger@famsik.de>
2014-03-03 11:03:17 +0100

    man, htdocs, gui, test, tools: bump a few more copyrights to 2014

--- tools/extracttests.py
+++ tools/extracttests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #
 # Cppcheck - A tool for static C/C++ code analysis
-# Copyright (C) 2007-2013 Daniel Marjamaeki and Cppcheck team.
+# Copyright (C) 2007-2014 Daniel Marjamaeki and Cppcheck team.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
