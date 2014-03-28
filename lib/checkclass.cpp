34d1f885a3f2487554f6f5f56fd369fb99ecabf3
Alexander Mai <amai@users.sf.net>
2014-03-28 20:29:33 +0100

    Ticket #4296: Improve message text for operatorEq

--- lib/checkclass.cpp
+++ lib/checkclass.cpp
@@ -1138,7 +1138,9 @@ void CheckClass::operatorEq()
 
 void CheckClass::operatorEqReturnError(const Token *tok, const std::string &className)
 {
-    reportError(tok, Severity::style, "operatorEq", "'" + className + "::operator=' should return '" + className + " &'.");
+    reportError(tok, Severity::style, "operatorEq", "'" + className + "::operator=' should return '" + className + " &'.\n"
+      "The "+className+"::operator= does not conform to standard C/C++ behaviour. To conform to standard C/C++ behaviour, return a reference to self (such as: '"+className+" &"+className+"::operator=(..) { .. return *this; }'. For safety reasons it might be better to not fix this message. If you think that safety is always more important than conformance then please ignore/suppress this message. For more details about this topic, see the book \"Effective C++\" by Scott Meyers."
+    );
 }
 
 //---------------------------------------------------------------------------
