fa18adea6f9d5432f482a170f05390438c356439
Toralf Förster <toralf.foerster@gmx.de>
2014-06-25 22:48:56 +0200

    tools/times.sh: prefer a loop over repeating the same command
    Signed-off-by: Toralf Förster <toralf.foerster@gmx.de>

--- tools/times.sh
+++ tools/times.sh
@@ -20,10 +20,9 @@ for i in `seq 1 50`; do
     # if build fails, make clean and try again
     make SRCDIR=build CXXFLAGS=-O2 -j4 || make clean ; make SRCDIR=build CXXFLAGS=-O2 -j4
     echo "Run number $i"
-    ./cppcheck -q --showtime=summary --enable=all --inconclusive src 2> /dev/null | tee -a times.log
-    ./cppcheck -q --showtime=summary --enable=all --inconclusive src 2> /dev/null | tee -a times.log
-    ./cppcheck -q --showtime=summary --enable=all --inconclusive src 2> /dev/null | tee -a times.log
-    ./cppcheck -q --showtime=summary --enable=all --inconclusive src 2> /dev/null | tee -a times.log
+    for j in `seq 1 4`; do
+        ./cppcheck -q --showtime=summary --enable=all --inconclusive src 2> /dev/null | tee -a times.log
+    done
     grep "Overall" times.log | tail -4 | sed s/s// | awk -v "i=$i" -v "git_head=$git_head"  '{ sum+=$3} END {print "Run " i",  "git_head  "  Average: " sum/4}' | tee -a times.log
     git reset --hard HEAD^1 | tee -a times.log
 done
