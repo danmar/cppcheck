6d5603af4523c067bc419011ab02d13b4dbc9610
PKEuS <philipp.kloke@web.de>
2014-08-02 18:07:01 +0200

    Increased version to 1.66.99/1.67 dev

--- lib/version.h
+++ lib/version.h
@@ -1,3 +1,3 @@
-#define CPPCHECK_VERSION_STRING "1.66"
-#define CPPCHECK_VERSION 1,66,0,0
+#define CPPCHECK_VERSION_STRING "1.67 dev"
+#define CPPCHECK_VERSION 1,66,99,0
 #define LEGALCOPYRIGHT L"Copyright (C) 2007-2014 Daniel Marjam\x00E4ki and Cppcheck team."
