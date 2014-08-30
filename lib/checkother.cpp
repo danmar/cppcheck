e9fdfc6cd0348b2b56afc5799ad29c2aa1a02ab0
PKEuS <philipp.kloke@web.de>
2014-08-30 11:18:31 +0200

    Fix on checking side for #6099

--- lib/checkother.cpp
+++ lib/checkother.cpp
@@ -1377,7 +1377,7 @@ void CheckOther::checkVariableScope()
         bool reduce = true;
         bool used = false; // Don't warn about unused variables
         for (; tok != var->scope()->classEnd; tok = tok->next()) {
-            if (tok->str() == "{" && tok->scope() != tok->previous()->scope()) {
+            if (tok->str() == "{" && tok->scope() != tok->previous()->scope() && !tok->isExpandedMacro()) {
                 if (used) {
                     bool used2 = false;
                     if (!checkInnerScope(tok, var, used2) || used2) {
