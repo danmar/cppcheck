f24e1b82cf4d71c1d76344d180afdc66f3d967ae
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2014-12-20 18:47:40 +0100

    astyle formatting

--- lib/checkuninitvar.cpp
+++ lib/checkuninitvar.cpp
@@ -1666,7 +1666,7 @@ bool CheckUninitVar::isVariableUsage(const Token *vartok, bool pointer, bool all
     if (!alloc && vartok->previous()->str() == "return")
         return true;
 
-   // Passing variable to typeof/__alignof__
+    // Passing variable to typeof/__alignof__
     if (Token::Match(vartok->tokAt(-3), "typeof|__alignof__ ( * %var%"))
         return false;
 
