39fde3c8c7ba238360750c8c4391cce48e96167c
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2015-01-05 15:10:43 +0100

    triage: updated linux-3.11 triage data

--- triage/linux-3.11/linux-3.11.h
+++ triage/linux-3.11/linux-3.11.h
@@ -2,6 +2,8 @@
 #define BUG_ON(C)      if (C) exit(1)
 #define ARRAY_SIZE(A)  (sizeof(A) / sizeof(A[0]))
 
+#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+
 #define container_of(ptr, type, member) ({                      \
         const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
         (type *)( (char *)__mptr - offsetof(type,member) );})
