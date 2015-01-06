70408e9a38290df0113d13ebf84d73cc2f9c512a
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2015-01-06 12:18:43 +0100

    triage: quick triage of last linux-3.11 errors

--- triage/linux-3.11/linux-3.11.h
+++ triage/linux-3.11/linux-3.11.h
@@ -1,12 +1,13 @@
 
-#define BUG_ON(C)      if (C) exit(1)
-#define ARRAY_SIZE(A)  (sizeof(A) / sizeof(A[0]))
-
-#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
-
+#define __alignof__(x)            1
+#define offsetof(TYPE, MEMBER)    ((size_t) &((TYPE *)0)->MEMBER)
+#define ARRAY_SIZE(A)             (sizeof(A) / sizeof(A[0]))
+#define BUG_ON(C)                 if (C) exit(1)
+#define NVERSION(version)         (version >> 16) & 0xFF, (version >> 8) & 0xFF, version & 0xFF
 #define container_of(ptr, type, member) ({                      \
         const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
         (type *)( (char *)__mptr - offsetof(type,member) );})
 
 void panic(const char *fmt, ...) __attribute__((noreturn));
+
 
