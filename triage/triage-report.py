2b018db25add54f15ca51449f5d031dc6c79e3a3
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2015-01-07 21:12:09 +0100

    triage-report.py: use classifications 'true positive', 'false positive', 'false negative' instead

--- triage/triage-report.py
+++ triage/triage-report.py
@@ -24,12 +24,13 @@ fin.close()
 
 out = {}
 out['untriaged'] = ''
+out['fn'] = ''
 out['fp'] = ''
 out['tp'] = ''
-out['notfound'] = ''
 
 numberOfFalsePositives = 0
 numberOfTruePositives  = 0
+numberOfFalseNegatives = 0
 
 for result in results.split('\n'):
     result = result.strip()
@@ -45,11 +46,11 @@ for result in results.split('\n'):
     classification = 'Untriaged'
     if result in truepositives:
         css = 'tp'
-        classification = 'Bug'
+        classification = 'True Positive'
         numberOfTruePositives += 1
     elif result in falsepositives:
         css = 'fp'
-        classification = 'Not bug'
+        classification = 'False Positive'
         numberOfFalsePositives += 1
     href = None
 
@@ -80,11 +81,13 @@ for line in f.readlines():
     if line in results:
         continue
 
+    numberOfFalseNegatives += 1
+
     filename       = res.group(1)
     linenr         = res.group(2)
     message        = res.group(3)
-    classification = 'Not Found'
-    css            = 'notfound'
+    classification = 'False Negative'
+    css            = 'fn'
 
     html =  '  <tr>'
     html += '<td class='+css+'>'+filename+'</td>'
@@ -106,14 +109,15 @@ else:
 fout = open('report.html','wt')
 fout.write('<html><head><title>Cppcheck results for ' + project + '</title><link rel="stylesheet" type="text/css" href="theme1.css"></head><body>\n')
 fout.write('<h1>Cppcheck results for ' + project + '</h1>\n')
+fout.write('<p>Number of false negatives: ' + str(numberOfFalseNegatives) + '</p>\n')
 fout.write('<p>Number of true positives: ' + str(numberOfTruePositives) + '</p>\n')
 fout.write('<p>Number of false positives: ' + str(numberOfFalsePositives) + '</p>\n')
 fout.write('<table border="0">\n')
 fout.write('<tr><th>Filename</th><th>Line</th><th>Message</th><th>Classification</th></tr>\n')
+fout.write(out['fn'])
 fout.write(out['tp'])
-fout.write(out['notfound'])
-fout.write(out['untriaged'])
 fout.write(out['fp'])
+fout.write(out['untriaged'])
 
 fout.write('</table></body></html>')
 fout.close()
