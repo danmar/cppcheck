53804a32fa5e6a191862f2c87248efa309583c07
Luis Díaz Más <luis@pix4d.com>
2015-01-25 13:35:35 +0100

    Compiling LIB, TinyXml and Client application

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,35 +1,36 @@
 #------------------------------------------------------
 # Build type
 #------------------------------------------------------
-SET(CMAKE_CONFIGURATION_TYPES "Debug;Release" CACHE STRING "Configs" FORCE)
-IF(DEFINED CMAKE_BUILD_TYPE)
+set(CMAKE_CONFIGURATION_TYPES "Debug;Release" CACHE STRING "Configs" FORCE)
+if(DEFINED CMAKE_BUILD_TYPE)
    SET_PROPERTY(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS ${CMAKE_CONFIGURATION_TYPES})
-ENDIF()
+endif()
 
-IF(NOT CMAKE_BUILD_TYPE)
-   SET(CMAKE_BUILD_TYPE "Debug")
-ENDIF()
+if(NOT CMAKE_BUILD_TYPE)
+   set(CMAKE_BUILD_TYPE "Debug")
+endif()
 
 # ----------------------------------------------------------------------------
 #   PROJECT CONFIGURATION
 # ----------------------------------------------------------------------------
-OPTION(USE_CLANG            "Use Clang compiler"                                            OFF)
-OPTION(USE_ANALYZE          "Use Clang compiler with analyze mode"                          OFF)
-OPTION(ANALYZE_MEMORY       "Clang dynamic analyzer: detector of uninitialized reads."      OFF)
-OPTION(ANALYZE_ADDRESS      "Clang dynamic analyzer: fast memory error detector. "          OFF)
-OPTION(ANALYZE_THREAD       "Clang dynamic analyzer: tool that detects data races. "        OFF)
-OPTION(ANALYZE_UNDEFINED    "Clang dynamic analyzer: undefined behavior checker. "          OFF)
-OPTION(ANALYZE_DATAFLOW     "Clang dynamic analyzer: general dynamic dataflow analysis."    OFF)
-OPTION(WARNINGS_ARE_ERRORS  "Treat warnings as errors"                                      OFF)
-OPTION(WARNINGS_ANSI_ISO    "Issue all the mandatory diagnostics Listed in C standard"      ON)
+option(USE_CLANG            "Use Clang compiler"                                            OFF)
+option(USE_ANALYZE          "Use Clang compiler with analyze mode"                          OFF)
+option(ANALYZE_MEMORY       "Clang dynamic analyzer: detector of uninitialized reads."      OFF)
+option(ANALYZE_ADDRESS      "Clang dynamic analyzer: fast memory error detector. "          OFF)
+option(ANALYZE_THREAD       "Clang dynamic analyzer: tool that detects data races. "        OFF)
+option(ANALYZE_UNDEFINED    "Clang dynamic analyzer: undefined behavior checker. "          OFF)
+option(ANALYZE_DATAFLOW     "Clang dynamic analyzer: general dynamic dataflow analysis."    OFF)
+option(WARNINGS_ARE_ERRORS  "Treat warnings as errors"                                      OFF)
+option(WARNINGS_ANSI_ISO    "Issue all the mandatory diagnostics Listed in C standard"      ON)
 
-OPTION(BUILD_SHARED_LIBS    "Build shared libraries"                                        ON)
-OPTION(BUILD_TESTS          "Build tests"                                                   OFF)
-OPTION(BUILD_QT_APP         "Build the qt application"                                      OFF)
-OPTION(BUILD_UTILS_TESTS    "Build applications tests using the different modules"          OFF)
+option(BUILD_SHARED_LIBS    "Build shared libraries"                                        ON)
+option(BUILD_TESTS          "Build tests"                                                   OFF)
+option(BUILD_QT_APP         "Build the qt application"                                      OFF)
+option(BUILD_UTILS_TESTS    "Build applications tests using the different modules"          OFF)
 
-SET(CMAKE_INCLUDE_DIRS_CONFIGCMAKE ${CMAKE_INSTALL_PREFIX}/include      CACHE PATH "Output directory for headers")
-SET(CMAKE_LIB_DIRS_CONFIGCMAKE     ${CMAKE_INSTALL_PREFIX}/lib          CACHE PATH "Output directory for libraries")
-SET(RUNTIME_OUTPUT_DIRECTORY       ${PROJECT_BINARY_DIR}/bin)
-SET(ARCHIVE_OUTPUT_DIRECTORY       ${PROJECT_BINARY_DIR}/lib)
-SET(LIBRARY_OUTPUT_DIRECTORY       ${PROJECT_BINARY_DIR}/lib)
+set(CMAKE_INCLUDE_DIRS_CONFIGCMAKE ${CMAKE_INSTALL_PREFIX}/include      CACHE PATH "Output directory for headers")
+set(CMAKE_LIB_DIRS_CONFIGCMAKE     ${CMAKE_INSTALL_PREFIX}/lib          CACHE PATH "Output directory for libraries")
+
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin)
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/lib)
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/lib)
