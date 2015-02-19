b2634788b03b7eb4e578b99985193c992be9daa5
Luis Díaz Más <piponazo@gmail.com>
2015-02-19 19:31:03 +0100

    PCRE now is handled properly in windows (place .h & .lib in externals folder)

--- cmake/findDependencies.cmake
+++ cmake/findDependencies.cmake
@@ -4,7 +4,7 @@ set(CMAKE_AUTOMOC OFF)
 find_package(Qt4 4.6.1 COMPONENTS QtCore QtGui)
 
 if (HAVE_RULES)
-    find_library(PCRE pcre)
+    find_library(PCRE pcre PATHS ${PROJECT_SOURCE_DIR}/externals)
     if (NOT PCRE)
         message(FATAL_ERROR "pcre dependency for RULES has not been found")
     endif()
