780a530e0ec0f0066766defe5767ddefd68e0d60
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2015-03-08 16:23:54 +0100

    test/cfg/windows.cpp: Added test for #6571, Library::isNotLibraryFunction() should return true for methods.

--- test/cfg/windows.cpp
+++ test/cfg/windows.cpp
@@ -7,12 +7,12 @@
 //
 
 class CSharedFilesCtrl {
-	void OpenFile(const CShareableFile* file);
+	void OpenFile(const CShareableFile* file, int, int);
 	afx_msg void OnNmDblClk(NMHDR *pNMHDR, LRESULT *pResult);
 
 };
 
 void CSharedFilesCtrl::OnNmDblClk(NMHDR* /*pNMHDR*/, LRESULT* pResult) {
 	if (file)
-	  OpenFile(file);
+	  OpenFile(file,0,0); // <- not the windows OpenFile function
 }
