faf8f21a3addf8d7ea86b62b50cd17b68fe79d09
Jakub Melka <Jakub.Melka@seznam.cz>
2015-03-16 20:26:32 +0100

    Added library support (malloc-like functions are handled by library)

--- lib/checkstl.cpp
+++ lib/checkstl.cpp
@@ -1234,6 +1234,7 @@ void CheckStl::checkAutoPointer()
     std::set<unsigned int> autoPtrVarId;
     std::map<unsigned int, const std::string> mallocVarId; // variables allocated by the malloc-like function
     static const char STL_CONTAINER_LIST[] = "array|bitset|deque|list|forward_list|map|multimap|multiset|priority_queue|queue|set|stack|vector|hash_map|hash_multimap|hash_set|unordered_map|unordered_multimap|unordered_set|unordered_multiset|basic_string";
+    const int malloc = _settings->library.alloc("malloc"); // allocation function, which are not compatible with auto_ptr
 
     for (const Token *tok = _tokenizer->tokens(); tok; tok = tok->next()) {
         if (Token::simpleMatch(tok, "auto_ptr <")) {
@@ -1248,7 +1249,7 @@ void CheckStl::checkAutoPointer()
                     if (Token::Match(tok3, "( new %type%") && hasArrayEndParen(tok3)) {
                         autoPointerArrayError(tok2->next());
                     }
-                    if (Token::Match(tok3, "( malloc|calloc|strdup|strndup|kmalloc|kzalloc|kcalloc (")) {
+                    if (Token::Match(tok3, "( %name% (") && malloc && _settings->library.alloc(tok3->next()) == malloc) {
                         // malloc-like function allocated memory passed to the auto_ptr constructor -> error
                         autoPointerMallocError(tok2->next(), tok3->next()->str());
                     }
@@ -1292,7 +1293,7 @@ void CheckStl::checkAutoPointer()
                 if (iter != autoPtrVarId.end()) {
                     autoPointerArrayError(tok);
                 }
-            } else if (Token::Match(tok, "%var% = malloc|calloc|strdup|strndup|kmalloc|kzalloc|kcalloc (")) {
+            } else if (Token::Match(tok, "%var% = %name% (") && malloc && _settings->library.alloc(tok->tokAt(2)) == malloc) {
                 // C library function like 'malloc' used together with auto pointer -> error
                 std::set<unsigned int>::const_iterator iter = autoPtrVarId.find(tok->varId());
                 if (iter != autoPtrVarId.end()) {
@@ -1301,7 +1302,7 @@ void CheckStl::checkAutoPointer()
                     // it is not an auto pointer variable and it is allocated by malloc like function.
                     mallocVarId.insert(std::make_pair(tok->varId(), tok->strAt(2)));
                 }
-            } else if ((Token::Match(tok, "%var% . reset ( malloc|calloc|strdup|strndup|kmalloc|kzalloc|kcalloc ("))) {
+            } else if (Token::Match(tok, "%var% . reset ( %name% (") && malloc && _settings->library.alloc(tok->tokAt(4)) == malloc) {
                 // C library function like 'malloc' used when resetting auto pointer -> error
                 std::set<unsigned int>::const_iterator iter = autoPtrVarId.find(tok->varId());
                 if (iter != autoPtrVarId.end()) {
