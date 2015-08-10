38cba6711547832ed686aaf2c6c7d26aea84676a
Martin Ettl <ettl.martin78@googlemail.com>
2015-08-10 22:58:46 +0200

    Suppressed compiler warnings during testing cfg tests.

--- test/cfg/runtests.sh
+++ test/cfg/runtests.sh
@@ -9,18 +9,23 @@ else # assume we are in repo root
 	DIR=./test/cfg/
 fi
 
+# Compiler settings
+CXX=g++
+CC=gcc
+CC_OPT='-Wno-nonnull -fsyntax-only'
+
 # posix.c
-gcc -fsyntax-only ${DIR}posix.c
+${CC} ${CC_OPT} ${DIR}posix.c
 ${CPPCHECK} --check-library --library=posix --enable=information --enable=style --error-exitcode=1 --suppress=missingIncludeSystem --inline-suppr ${DIR}posix.c
 
 # gnu.c
-gcc -fsyntax-only -D_GNU_SOURCE ${DIR}gnu.c
+${CC} ${CC_OPT} -D_GNU_SOURCE ${DIR}gnu.c
 ${CPPCHECK} --check-library --library=gnu --enable=information --enable=style --error-exitcode=1 --suppress=missingIncludeSystem --inline-suppr ${DIR}gnu.c
 
 # windows.cpp
-#g++ -fsyntax-only ${DIR}windows.cpp
+#${CXX} -fsyntax-only ${DIR}windows.cpp
 ${CPPCHECK} --check-library --library=windows --enable=information --enable=style --error-exitcode=1 --inline-suppr ${DIR}windows.cpp
 
 # std.c
-gcc -fsyntax-only ${DIR}std.c
+${CC} ${CC_OPT} ${DIR}std.c
 ${CPPCHECK} --check-library --enable=information --error-exitcode=1 --enable=style --suppress=missingIncludeSystem --inline-suppr ${DIR}std.c
