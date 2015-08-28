6a05eb87cb72004e7110421dfa48ba0e6f55bdfb
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2015-08-28 10:38:12 +0200

    devinfo: Add cppcheckdata doxygen output

--- webreport.sh
+++ webreport.sh
@@ -10,5 +10,8 @@ mv coverage_report devinfo/
 doxygen 2> devinfo/doxygen-errors.txt
 mv doxyoutput/html devinfo/doxyoutput
 
+doxygen addons/cppcheckdata.doxyfile
+mv html devinfo/cppcheckdata
+
 # Detect duplicate code..
 ~/pmd-4.2.6/bin/cpd.sh lib/ > devinfo/cpd.txt
