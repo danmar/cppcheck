b48cbde860363e54efe85a93c6be3fe6f853f720
Frank Zingsheim <f.zingsheim@gmx.de>
2015-11-26 21:16:59 +0100

    Cmake: Allow the RelWithDebInfo MinSizeRel as Configuration Type, additionally

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,7 +1,7 @@
 #------------------------------------------------------
 # Build type
 #------------------------------------------------------
-set(CMAKE_CONFIGURATION_TYPES "Debug;Release" CACHE STRING "Configs" FORCE)
+set(CMAKE_CONFIGURATION_TYPES "Debug;Release;RelWithDebInfo;MinSizeRel" CACHE STRING "Configs" FORCE)
 if(DEFINED CMAKE_BUILD_TYPE)
    SET_PROPERTY(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS ${CMAKE_CONFIGURATION_TYPES})
 endif()
@@ -26,7 +26,7 @@ option(WARNINGS_ANSI_ISO    "Issue all the mandatory diagnostics Listed in C sta
 set(USE_MATCHCOMPILER "Auto" CACHE STRING "Usage of match compliler")
 set_property(CACHE USE_MATCHCOMPILER PROPERTY STRINGS Auto Off On Verify) 
 if (USE_MATCHCOMPILER STREQUAL "Auto")
-    if (CMAKE_BUILD_TYPE STREQUAL "Release")
+    if (NOT CMAKE_BUILD_TYPE STREQUAL "Debug")
         set(USE_MATCHCOMPILER_OPT "On")
     else()
         set(USE_MATCHCOMPILER_OPT "Off")
