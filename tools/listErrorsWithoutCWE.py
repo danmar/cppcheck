c7c10d0b43ff74e570fee58ee34dc23b79a99501
Roberto Martelloni <rmartelloni@gmail.com>
2016-08-09 23:15:31 +0100

    added python script to list in CSV format all errors without a CWE

---
+++ tools/listErrorsWithoutCWE.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import argparse
+import xml.etree.ElementTree as ET
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="List all error without a CWE assigned in CSV format")
+    parser.add_argument("-F", metavar="filename", required=True, help="XML file containing output from: ./cppcheck --errorlist --xml-version=2")
+    parsed = parser.parse_args()
+
+    tree = ET.parse(vars(parsed)["F"])
+    root = tree.getroot()
+    for child in root.iter("error"):
+
+        if "cwe" not in child.attrib:
+            print child.attrib["id"], ",", child.attrib["severity"], ",", child.attrib["verbose"]
+
+if __name__ == "__main__":
+    main()
