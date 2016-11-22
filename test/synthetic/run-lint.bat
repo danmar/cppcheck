887019579496d13722e75f16331d6b047f6c5ee6
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2016-11-22 19:25:05 +0100

    Synthetic: Add run-lint.bat script

---
+++ test/synthetic/run-lint.bat
@@ -0,0 +1,4 @@
+\lint\lint-nt.exe -e526 -e529 -e550 -e552 -e714 -e744 -e765 -e830 -e831 -e843 -h1 controlflow.c
+\lint\lint-nt.exe -e526 -e529 -e550 -e552 -e714 -e744 -e765 -e830 -e831 -e843 -h1 data.c
+\lint\lint-nt.exe -e526 -e529 -e550 -e552 -e714 -e744 -e765 -e830 -e831 -e843 -h1 functions.c
+\lint\lint-nt.exe -e526 -e529 -e550 -e552 -e714 -e744 -e765 -e830 -e831 -e843 -h1 ub.c
