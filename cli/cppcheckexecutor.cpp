4bd6a75c48743dd064f054487bd324dfe6682c8d
Neszt Tibor <tibor@neszt.hu>
2016-11-29 09:44:50 +0100

    execinfo.h is part of glibc
    On systems where glibc is not available, execinfo.h should not be
    included.

--- cli/cppcheckexecutor.cpp
+++ cli/cppcheckexecutor.cpp
@@ -50,7 +50,7 @@
 #endif
 #endif
 
-#if !defined(NO_UNIX_BACKTRACE_SUPPORT) && defined(USE_UNIX_SIGNAL_HANDLING) && defined(__GNUC__) && !defined(__CYGWIN__) && !defined(__MINGW32__) && !defined(__NetBSD__) && !defined(__SVR4) && !defined(__QNX__)
+#if !defined(NO_UNIX_BACKTRACE_SUPPORT) && defined(USE_UNIX_SIGNAL_HANDLING) && defined(__GNUC__) && defined(__GLIBC__) && !defined(__CYGWIN__) && !defined(__MINGW32__) && !defined(__NetBSD__) && !defined(__SVR4) && !defined(__QNX__)
 #define USE_UNIX_BACKTRACE_SUPPORT
 #include <cxxabi.h>
 #include <execinfo.h>
