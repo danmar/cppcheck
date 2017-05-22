7af58cf31a5058ac67111b72cc062612b8dec9ec
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2017-05-22 07:42:54 +0200

    show full error path in xml files

--- lib/check.h
+++ lib/check.h
@@ -169,7 +169,7 @@ protected:
         ErrorPath errorPath;
         if (!value) {
             errorPath.push_back(ErrorPathItem(errtok,bug));
-        } else if (_settings->verbose || _settings->outputFormat == "clang") {
+        } else if (_settings->verbose || _settings->xml || _settings->outputFormat == "clang") {
             errorPath = value->errorPath;
             errorPath.push_back(ErrorPathItem(errtok,bug));
         } else {
