3099da9e50bfba38ae660202bdecd295a0fba364
rsBNT <rsBNT@users.noreply.github.com>
2017-11-21 10:30:10 +0100

    instead of checking the checksum over the tokenlist, use the filecontents as checksum. This avoids expensive tokenizing and computing the checksum over the content is really fast

--- lib/cppcheck.cpp
+++ lib/cppcheck.cpp
@@ -139,6 +139,39 @@ unsigned int CppCheck::processFile(const std::string& filename, const std::strin
     bool internalErrorFound(false);
     try {
         Preprocessor preprocessor(_settings, this);
+
+		// Check with checksum for unmodified file early to avoid expensive tokenizing
+		if (!_settings.buildDir.empty()) {
+			// Get toolinfo
+			std::string toolinfo;
+			toolinfo += CPPCHECK_VERSION_STRING;
+			toolinfo += _settings.isEnabled(Settings::WARNING) ? 'w' : ' ';
+			toolinfo += _settings.isEnabled(Settings::STYLE) ? 's' : ' ';
+			toolinfo += _settings.isEnabled(Settings::PERFORMANCE) ? 'p' : ' ';
+			toolinfo += _settings.isEnabled(Settings::PORTABILITY) ? 'p' : ' ';
+			toolinfo += _settings.isEnabled(Settings::INFORMATION) ? 'i' : ' ';
+			toolinfo += _settings.userDefines;
+
+			// Read complete file to generate checksum over it
+			const std::string filecontent(std::istreambuf_iterator<char>(fileStream), {});
+
+			// Calculate checksum so it can be compared with old checksum / future checksums
+			const unsigned int checksum = preprocessor.calculateChecksum(toolinfo+'\n'+filecontent);
+
+			std::list<ErrorLogger::ErrorMessage> errors;
+			if (!analyzerInformation.analyzeFile(_settings.buildDir, filename, cfgname, checksum, &errors)) {
+				while (!errors.empty()) {
+					reportErr(errors.front());
+					errors.pop_front();
+				}
+				return exitcode;  // known results => no need to reanalyze file
+			}
+
+			// reset stream state and position for analysis
+			fileStream.clear();
+			fileStream.seekg(0, std::ios::beg);
+		}
+
         std::set<std::string> configurations;
 
         simplecpp::OutputList outputList;
@@ -226,29 +259,6 @@ unsigned int CppCheck::processFile(const std::string& filename, const std::strin
         preprocessor.inlineSuppressions(tokens1);
         tokens1.removeComments();
         preprocessor.removeComments();
-
-        if (!_settings.buildDir.empty()) {
-            // Get toolinfo
-            std::string toolinfo;
-            toolinfo += CPPCHECK_VERSION_STRING;
-            toolinfo += _settings.isEnabled(Settings::WARNING) ? 'w' : ' ';
-            toolinfo += _settings.isEnabled(Settings::STYLE) ? 's' : ' ';
-            toolinfo += _settings.isEnabled(Settings::PERFORMANCE) ? 'p' : ' ';
-            toolinfo += _settings.isEnabled(Settings::PORTABILITY) ? 'p' : ' ';
-            toolinfo += _settings.isEnabled(Settings::INFORMATION) ? 'i' : ' ';
-            toolinfo += _settings.userDefines;
-
-            // Calculate checksum so it can be compared with old checksum / future checksums
-            const unsigned int checksum = preprocessor.calculateChecksum(tokens1, toolinfo);
-            std::list<ErrorLogger::ErrorMessage> errors;
-            if (!analyzerInformation.analyzeFile(_settings.buildDir, filename, cfgname, checksum, &errors)) {
-                while (!errors.empty()) {
-                    reportErr(errors.front());
-                    errors.pop_front();
-                }
-                return exitcode;  // known results => no need to reanalyze file
-            }
-        }
 
         // Get directives
         preprocessor.setDirectives(tokens1);
