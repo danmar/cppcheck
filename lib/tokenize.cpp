913fdf44b63dc2ca66fe5d433955d1178ed506c7
Alexey Eryomenko <32419783+aeryomenko@users.noreply.github.com>
2018-01-21 17:58:12 +0300

    member access operators are allowed inside the embedded SQL block when (#1043)
    passing arguments for a query

--- lib/tokenize.cpp
+++ lib/tokenize.cpp
@@ -10069,7 +10069,7 @@ const Token *Tokenizer::findSQLBlockEnd(const Token *tokSQLStart)
             if (Token::simpleMatch(tok->tokAt(-2), "END - __CPPCHECK_EMBEDDED_SQL_EXEC__ ;"))
                 return tok->next();
             return tokLastEnd;
-        } else if (Token::Match(tok, "{|}|==|&&|!|&|^|<<|>>|++|+=|-=|/=|*=|>>=|<<=|->|::|~"))
+        } else if (Token::Match(tok, "{|}|==|&&|!|^|<<|>>|++|+=|-=|/=|*=|>>=|<<=|~"))
             break; // We are obviously outside the SQL block
     }
 
