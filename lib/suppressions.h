0f6a5cb7210020f454ea6dae2b347f5018769d9b
rebnridgway <nick.ridgway@rebellion.co.uk>
2018-04-17 12:01:13 +0100

    Fixed indentation to be consistent

--- lib/suppressions.h
+++ lib/suppressions.h
@@ -135,10 +135,10 @@ public:
     bool isSuppressed(const ErrorMessage &errmsg);
 
     /**
-	 * @brief Create an xml dump of suppressions
-	 * @param out stream to write XML to
-	*/
-	void dump(std::ostream &out);
+     * @brief Create an xml dump of suppressions
+     * @param out stream to write XML to
+    */
+    void dump(std::ostream &out);
 
     /**
      * @brief Returns list of unmatched local (per-file) suppressions.
