4a333088f6b9e75d8efb7c5114114e90afbb9783
versat <versat82@gmail.com>
2018-04-18 14:17:32 +0200

    GUI: Remove connect call with nonexistent slot function
    When starting the application Qt prints this message:
    QObject::connect: No such slot CodeEditor::highlightCurrentLine() in
    ..\gui\codeeditor.cpp:141
    And this function really does not exist, i guess it is some relict. If
    it is planned to implement it this PR can just be closed.

--- gui/codeeditor.cpp
+++ gui/codeeditor.cpp
@@ -138,7 +138,6 @@ CodeEditor::CodeEditor(QWidget *parent) : QPlainTextEdit(parent)
 
     connect(this, SIGNAL(blockCountChanged(int)), this, SLOT(updateLineNumberAreaWidth(int)));
     connect(this, SIGNAL(updateRequest(QRect,int)), this, SLOT(updateLineNumberArea(QRect,int)));
-    connect(this, SIGNAL(cursorPositionChanged()), this, SLOT(highlightCurrentLine()));
 
     updateLineNumberAreaWidth(0);
 }
