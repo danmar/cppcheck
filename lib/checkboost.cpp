5518247b96dd7e258ed5c59ae19a6f33da0ec18c
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2018-04-24 21:18:36 +0200

    Refactoring; use range for

--- lib/checkboost.cpp
+++ lib/checkboost.cpp
@@ -34,9 +34,7 @@ static const CWE CWE664(664);
 void CheckBoost::checkBoostForeachModification()
 {
     const SymbolDatabase *symbolDatabase = _tokenizer->getSymbolDatabase();
-    const std::size_t functions = symbolDatabase->functionScopes.size();
-    for (std::size_t i = 0; i < functions; ++i) {
-        const Scope * scope = symbolDatabase->functionScopes[i];
+    for (const Scope * scope : symbolDatabase->functionScopes) {
         for (const Token *tok = scope->classStart->next(); tok && tok != scope->classEnd; tok = tok->next()) {
             if (!Token::simpleMatch(tok, "BOOST_FOREACH ("))
                 continue;
