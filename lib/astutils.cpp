a2cced791bf9a40b14efe16bc7c1b3456054cc30
Paul <pfultz2@yahoo.com>
2018-05-08 09:45:55 -0500

    Simplify conditional

--- lib/astutils.cpp
+++ lib/astutils.cpp
@@ -365,9 +365,9 @@ bool isOppositeExpression(bool cpp, const Token * const tok1, const Token * cons
         return false;
     if (isOppositeCond(true, cpp, tok1, tok2, library, pure))
         return true;
-    if (tok1->str() == "-" && !(tok1->astOperand2() && tok1->astOperand1()))
+    if (tok1->str() == "-" && !tok1->astOperand2())
         return isSameExpression(cpp, true, tok1->astOperand1(), tok2, library, pure);
-    if (tok2->str() == "-" && !(tok2->astOperand2() && tok2->astOperand1()))
+    if (tok2->str() == "-" && !tok2->astOperand2())
         return isSameExpression(cpp, true, tok2->astOperand1(), tok1, library, pure);
     return false;
 }
