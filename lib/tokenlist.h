960904832f074bb1a1916d0f66c3ed347ffb7d41
rebnridgway <nick.ridgway@rebellion.co.uk>
2018-05-11 14:38:50 +0100

    Removed unneeded deleteToken

--- lib/tokenlist.h
+++ lib/tokenlist.h
@@ -67,12 +67,6 @@ public:
      * @param tok token list to delete
      */
     static void deleteTokens(Token *tok);
-	
-    /**
-     * Delete a single token
-     * @param tok token to delete
-     */
-    void deleteToken(Token *tok);
 
     void addtoken(std::string str, const unsigned int lineno, const unsigned int fileno, bool split = false);
     void addtoken(const Token *tok, const unsigned int lineno, const unsigned int fileno);
