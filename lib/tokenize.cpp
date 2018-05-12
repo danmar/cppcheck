16e1e1d8f98f45ab42ee1dbdb2d68e0d172403cb
Simon Martin <simartin@users.noreply.github.com>
2018-05-12 10:20:33 +0200

    Ticket #8550: Properly simplify "typedef class A B;". (#1224)

--- lib/tokenize.cpp
+++ lib/tokenize.cpp
@@ -1254,7 +1254,7 @@ void Tokenizer::simplifyTypedef()
                             structRemoved = true;
                         typeStart = typeStart->next();
                     }
-                    if (typeStart->str() == "struct" && Token::Match(tok2, "%name% ::"))
+                    if (Token::Match(typeStart, "struct|class") && Token::Match(tok2, "%name% ::"))
                         typeStart = typeStart->next();
 
                     if (sameStartEnd)
