danmar/cppcheck

commit 0c6d60d202663196119d090b8a9fecb942aeade0

MISRA - support for per file/line error suppression (#1275)
* cppcheckdata.py: fixed Suppression.isMatch method

* cppcheckdata.py: fixed parsing <suppressions> tag

* misra.py: now uses cppcheckdata.reportError and supports suppressions

addons/misra.py
@@ -23,6 +23,7 @@ import argparse
 
 ruleTexts = {}
 suppressRules = {}
+suppressions = None
 typeBits = {
     'CHAR': None,
     'SHORT': None,
@@ -60,9 +61,14 @@ def reportError(location, num1, num2):
             errmsg = 'misra violation (use --rule-texts=<file> to get proper output) [' + id + ']'
         else:
             return
-        sys.stderr.write('[' + location.file + ':' + str(location.linenr) + '] (style): ' + errmsg + '\n')
-
-        VIOLATIONS.append(errmsg)
+        if not cppcheckdata.reportError('[{file}:{line}] {severity}: {message}',
+                                        callstack=[(location.file, location.linenr)],
+                                        severity='style',
+                                        message = errmsg,
+                                        errorId = id,
+                                        suppressions = suppressions,
+                                        outputFunc = sys.stderr.write) is None:
+            VIOLATIONS.append(errmsg)
 
 
 def simpleMatch(token, pattern):
@@ -1699,6 +1705,9 @@ def generateTable():
 def parseDump(dumpfile):
 
     data = cppcheckdata.parsedump(dumpfile)
+
+    global suppressions
+    suppressions = data.suppressions
 
     typeBits['CHAR'] = data.platform.char_bit
     typeBits['SHORT'] = data.platform.short_bit

addons/cppcheckdata.py
@@ -483,14 +483,14 @@ class Suppression:
         self.lineNumber = element.get('lineNumber')
         self.symbolName = element.get('symbolName')
 
-    def isMatch(file, line, message, errorId):
+    def isMatch(self, file, line, message, errorId):
         if (fnmatch(file, self.fileName)
             and (self.lineNumber is None or line == self.lineNumber)
-            and fnmatch(message, '*'+self.symbolName+'*')
+            and (self.symbolName is None or fnmatch(message, '*'+self.symbolName+'*'))
             and fnmatch(errorId, self.errorId)):
-            return true
+            return True
         else:
-            return false
+            return False
 
 class Configuration:
     """
@@ -506,7 +506,6 @@ class Configuration:
         functions     List of Function items
         variables     List of Variable items
         valueflow     List of ValueFlow values
-        suppressions  List of warning suppressions
     """
 
     name = ''
@@ -516,7 +515,6 @@ class Configuration:
     functions = []
     variables = []
     valueflow = []
-    suppressions = []
 
     def __init__(self, confignode):
         self.name = confignode.get('cfg')
@@ -526,7 +524,6 @@ class Configuration:
         self.functions = []
         self.variables = []
         self.valueflow = []
-        self.suppressions = []
         arguments = []
 
         for element in confignode:
@@ -562,9 +559,6 @@ class Configuration:
             if element.tag == 'valueflow':
                 for values in element:
                     self.valueflow.append(ValueFlow(values))
-            if element.tag == "suppressions":
-                for suppression in element:
-                    self.suppressions.append(Suppression(suppression))
 
         IdMap = {None: None, '0': None, '00000000': None, '0000000000000000': None}
         for token in self.tokenlist:
@@ -664,6 +658,7 @@ class CppcheckData:
     rawTokens = []
     platform = None
     configurations = []
+    suppressions = []
 
     def __init__(self, filename):
         self.configurations = []
@@ -688,6 +683,13 @@ class CppcheckData:
             for i in range(len(self.rawTokens) - 1):
                 self.rawTokens[i + 1].previous = self.rawTokens[i]
                 self.rawTokens[i].next = self.rawTokens[i + 1]
+
+
+        for suppressionsNode in data.getroot():
+            if suppressionsNode.tag == "suppressions":
+                for suppression in suppressionsNode:
+                    self.suppressions.append(Suppression(suppression))
+
 
         # root is 'dumps' node, each config has its own 'dump' subnode.
         for cfgnode in data.getroot():

commit 79f70cbb32d16fb7ec8e33169da40a39a0ad9b64

misra.py: restored previous output style. The output style was broken by https://github.com/danmar/cppcheck/commit/0c6d60d202663196119d090b8a9fecb942aeade0.

addons/misra.py
@@ -61,10 +61,10 @@ def reportError(location, num1, num2):
             errmsg = 'misra violation (use --rule-texts=<file> to get proper output) [' + id + ']'
         else:
             return
-        if not cppcheckdata.reportError('[{file}:{line}] {severity}: {message}',
+        if not cppcheckdata.reportError('[{file}:{line}] ({severity}): {message}',
                                         callstack=[(location.file, location.linenr)],
                                         severity='style',
-                                        message = errmsg,
+                                        message = errmsg + '\n',
                                         errorId = id,
                                         suppressions = suppressions,
                                         outputFunc = sys.stderr.write) is None: