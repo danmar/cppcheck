6812cb57babcda44be2e05d4c37892ff269399dd
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2018-06-29 08:47:00 +0200

    daca2-report: a bit better report

--- tools/daca2-report.py
+++ tools/daca2-report.py
@@ -121,8 +121,8 @@ mainpage.write('<h1>DACA2</h1>\n')
 mainpage.write('<p>Results when running latest (git head) Cppcheck on Debian.</p>\n')
 mainpage.write('<p>For performance reasons the analysis is limited. Files larger than 1mb are skipped. ' +
                'If analysis of a file takes more than 10 minutes it may be stopped.</p>\n')
-mainpage.write('<p>Negatives: <a href="negatives.txt">' + str(count_negatives) + '</a></p>\n')
-mainpage.write('<p>Positives: <a href="positives.txt">' + str(count_positives) + '</a></p>\n')
+mainpage.write('<p>Negatives (fixed/lost warnings): <a href="negatives.txt">' + str(count_negatives) + '</a></p>\n')
+mainpage.write('<p>Positives (new warnings): <a href="positives.txt">' + str(count_positives) + '</a></p>\n')
 mainpage.write('<table class="sortable">\n')
 mainpage.write(
     '<tr>' +
