5de8b797deb50dffe1092f74e9fb41046eabe2b2
lordylike <lord-of-magic@gmx.ch>
2018-07-18 12:00:28 +0200

    fix ticket 8570
    allow member initializer list variables that are moved to be non-const

--- lib/checkother.cpp
+++ lib/checkother.cpp
@@ -1354,6 +1354,27 @@ static std::size_t estimateSize(const Type* type, const Settings* settings, cons
 
 static bool canBeConst(const Variable *var)
 {
+	{  // Ticket #8570
+		const Function* func_scope = var->scope()->function;
+		if (func_scope->type == Function::Type::eConstructor) {
+			//could be initialized in initializer list
+			if (func_scope->arg->link()->next()->str() == ":") {
+				for (const Token* tok2 = func_scope->arg->link()->next()->next(); tok2 != var->scope()->bodyStart; tok2 = tok2->next()) {
+					if (tok2->varId() != var->declarationId())
+						continue;
+
+					const Token* parent = tok2->astParent();
+					if (parent && parent->str() == "{") //is initializer variable
+						return true;
+					else if (parent && parent->str() == "(") { //is initializer variable
+						if (parent->previous() && parent->previous()->str() == "move")
+							return false;
+						return true;
+					}
+				}
+			}
+		}
+	}
     for (const Token* tok2 = var->scope()->bodyStart; tok2 != var->scope()->bodyEnd; tok2 = tok2->next()) {
         if (tok2->varId() != var->declarationId())
             continue;
