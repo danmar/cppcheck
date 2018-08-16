df636c77ec6093a22f0ab3a42db5a6d292f28749
versat <versat@users.noreply.github.com>
2018-08-16 11:57:29 +0200

    bsd.cfg: Add arc4random* functions with tests.
    Reference: https://www.freebsd.org/cgi/man.cgi?query=arc4random

--- test/cfg/bsd.c
+++ test/cfg/bsd.c
@@ -7,6 +7,8 @@
 //
 
 #include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
 
 // size_t strlcat(char *dst, const char *src, size_t size);
 void uninitvar_strlcat(char *Ct, const char *S, size_t N)
@@ -25,4 +27,43 @@ void uninitvar_strlcat(char *Ct, const char *S, size_t N)
 
     // no warning is expected for
     (void)strlcat(Ct,S,N);
+}
+
+void bufferAccessOutOfBounds(void)
+{
+    uint16_t uint16Buf[4];
+    // cppcheck-suppress bufferAccessOutOfBounds
+    arc4random_buf(uint16Buf, 9);
+    // valid
+    arc4random_buf(uint16Buf, 8);
+}
+
+void ignoredReturnValue(void)
+{
+    // cppcheck-suppress ignoredReturnValue
+    arc4random();
+    // cppcheck-suppress ignoredReturnValue
+    arc4random_uniform(10);
+}
+
+void invalidFunctionArg()
+{
+    // cppcheck-suppress invalidFunctionArg
+    (void) arc4random_uniform(1);
+    // valid
+    (void) arc4random_uniform(2);
+}
+
+void nullPointer(void)
+{
+    // cppcheck-suppress nullPointer
+    arc4random_buf(NULL, 5);
+}
+
+void uninitvar(void)
+{
+    uint32_t uint32Uninit;
+
+    // cppcheck-suppress uninitvar
+    (void) arc4random_uniform(uint32Uninit);
 }
