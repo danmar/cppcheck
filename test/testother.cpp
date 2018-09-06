f5cf33d4e2dd048816cc0ee68361429b7bf3830c
Paul <pfultz2@yahoo.com>
2018-09-06 17:51:29 -0500

    Fix issue 8738: Dont warn about multiconditions when value is known

--- test/testother.cpp
+++ test/testother.cpp
@@ -4623,6 +4623,14 @@ private:
               "}\n");
         ASSERT_EQUALS("[test.cpp:2] -> [test.cpp:3]: (style) The expression 'val < 0' is always false.\n"
                       "[test.cpp:2] -> [test.cpp:4]: (style) The expression 'val < 0' is always false.\n", errout.str());
+
+        check("void f() {\n"
+              "  int activate = 0;\n"
+              "  int foo = 0;\n"
+              "  if (activate) {}\n"
+              "  else if (foo) {}\n"
+              "}\n");
+        ASSERT_EQUALS("", errout.str());
     }
 
     void checkSignOfUnsignedVariable() {
