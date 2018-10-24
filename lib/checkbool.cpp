5b7e3a5bf250360c8320ec42e2662e4d502004d6
Rikard Falkeborn <rikard.falkeborn@gmail.com>
2018-10-24 20:31:57 +0200

    Add check for return value of boolean function
    The rule for converting an integer to a boolean is that 0 is mapped to
    false and everything else is mapped to true. There is nothing wrong with
    the following code (according to the standards):
    
        bool f()
        {
            return -1;
        }
    
    and neither gcc nor clang will warn about it. However, it's a bit
    confusing. This commit adds a check that warns when a value other than 0
    or 1 is returned from a boolean function (similar to the existing check
    that functions with boolean arguments are only passed 0 or 1). Since the
    code is perfectly legal, set the severity to "Style".

--- lib/checkbool.cpp
+++ lib/checkbool.cpp
@@ -27,6 +27,7 @@
 #include "symboldatabase.h"
 #include "token.h"
 #include "tokenize.h"
+#include "valueflow.h"
 
 #include <cstddef>
 #include <list>
@@ -453,4 +454,34 @@ void CheckBool::assignBoolToFloatError(const Token *tok)
 {
     reportError(tok, Severity::style, "assignBoolToFloat",
                 "Boolean value assigned to floating point variable.", CWE704, false);
+}
+
+void CheckBool::returnValueOfFunctionReturningBool(void)
+{
+    if (!mSettings->isEnabled(Settings::STYLE))
+        return;
+
+    const SymbolDatabase * const symbolDatabase = mTokenizer->getSymbolDatabase();
+
+    for (const Scope * scope : symbolDatabase->functionScopes) {
+        if (scope->function && Token::Match(scope->function->retDef, "bool|_Bool")) {
+            for (const Token* tok = scope->bodyStart->next(); tok && (tok != scope->bodyEnd); tok = tok->next()) {
+                if (Token::simpleMatch(tok, "return")) {
+                    const ValueFlow::Value * maxValue = tok->astOperand1()->getValueGE(2, mSettings);
+                    if (maxValue) {
+                        returnValueBoolError(tok);
+                    }
+                    const ValueFlow::Value * minValue = tok->astOperand1()->getValueLE(-1, mSettings);
+                    if (minValue) {
+                        returnValueBoolError(tok);
+                    }
+                }
+            }
+        }
+    }
+}
+
+void CheckBool::returnValueBoolError(const Token *tok)
+{
+    reportError(tok, Severity::style, "returnNonBoolInBooleanFunction", "Non-boolean value returned from function returning bool");
 }
