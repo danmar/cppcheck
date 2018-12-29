3f318548e21646d522197e9545939cd56113d65e
ivangalkin <ivan.a.galkin@googlemail.com>
2018-12-29 21:34:22 +0100

    CheckNullPointer: Add missing id 'nullPointerArithmeticRedundantCheck' to errorlist (#1535)
    * split CheckNullPointer::arithmeticError() into
      * CheckNullPointer::pointerArithmeticError() and
      * CheckNullPointer::redundantConditionWarning()
    
    * Additional errorlist entry:
    
    ```XML
    <error id="nullPointerArithmeticRedundantCheck"
      severity="warning"
      msg="Either the condition is redundant or there is pointer arithmetic with NULL pointer."
      verbose="Either the condition is redundant or there is pointer arithmetic with NULL pointer." cwe="682"/>
    ```

--- lib/checknullpointer.h
+++ lib/checknullpointer.h
@@ -126,7 +126,8 @@ private:
     void getErrorMessages(ErrorLogger *errorLogger, const Settings *settings) const override {
         CheckNullPointer c(nullptr, settings, errorLogger);
         c.nullPointerError(nullptr, "pointer", nullptr, false);
-        c.arithmeticError(nullptr, nullptr);
+        c.pointerArithmeticError(nullptr, nullptr, false);
+        c.redundantConditionWarning(nullptr, nullptr, nullptr, false);
     }
 
     /** Name of check */
@@ -155,7 +156,8 @@ private:
 
     /** undefined null pointer arithmetic */
     void arithmetic();
-    void arithmeticError(const Token *tok, const ValueFlow::Value *value);
+    void pointerArithmeticError(const Token* tok, const ValueFlow::Value *value, bool inconclusive);
+    void redundantConditionWarning(const Token* tok, const ValueFlow::Value *value, const Token *condition, bool inconclusive);
 };
 /// @}
 //---------------------------------------------------------------------------
