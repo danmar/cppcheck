14a0031e884c765e8ef5705e5330dfc26f985e11
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2019-02-28 10:26:47 +0100

    Improved isVariableChangedByFunctionCall, better logic when parameter might be passed by reference

--- lib/astutils.cpp
+++ lib/astutils.cpp
@@ -811,6 +811,8 @@ bool isVariableChangedByFunctionCall(const Token *tok, const Settings *settings,
     if (!tok)
         return false;
 
+    const Token * const tok1 = tok;
+
     // address of variable
     const bool addressOf = tok->astParent() && tok->astParent()->isUnaryOp("&");
 
@@ -844,10 +846,13 @@ bool isVariableChangedByFunctionCall(const Token *tok, const Settings *settings,
             tok = tok->link();
         else if (Token::Match(tok->previous(), "%name% ("))
             break;
+        else if (Token::simpleMatch(tok->previous(), "> (") && tok->previous()->link())
+            break;
         tok = tok->previous();
     }
     if (!tok || tok->str() != "(")
         return false;
+    const bool possiblyPassedByReference = (tok->next() == tok1 || Token::simpleMatch(tok->previous(), ", %name% [,)]"));
     tok = tok->previous();
     if (tok && tok->link() && tok->str() == ">")
         tok = tok->link()->previous();
@@ -879,12 +884,13 @@ bool isVariableChangedByFunctionCall(const Token *tok, const Settings *settings,
         // => it is assumed that parameter is an in parameter (TODO: this is a bad heuristic)
         if (!addressOf && settings && settings->library.isnullargbad(tok, 1+argnr))
             return false;
-        // addressOf => inconclusive
-        if (!addressOf) {
+        // possible pass-by-reference => inconclusive
+        if (possiblyPassedByReference) {
             if (inconclusive != nullptr)
                 *inconclusive = true;
             return false;
         }
+        // Safe guess: Assume that parameter is changed by function call
         return true;
     }
 
