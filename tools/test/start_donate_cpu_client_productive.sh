e64c631deaa72b116a7a68fc86565041139b2683
versat <versat@users.noreply.github.com>
2019-03-06 08:41:16 +0100

    start_donate_cpu_client_productive.sh: Update client script regularly.
    Use the new option `--max-packages=` to update the client script (and
    the repository) regularly.

--- tools/test/start_donate_cpu_client_productive.sh
+++ tools/test/start_donate_cpu_client_productive.sh
@@ -2,9 +2,8 @@
 #
 # Runs the daca@home client against the real server and thus produces real results.
 #
-# The donate-cpu.py script is stopped once per day to pull updates and then starts again after a
-# short pause (until midnight is reached). Exception: If an analysis runs longer than the period
-# for detection lasts, the donate-cpu.py script is not stopped that day.
+# The donate-cpu.py script is stopped regularly to pull updates and then starts again after a
+# short pause with the latest "Donate CPU" client script.
 #
 # Should any error occur then the script also pulls updates and restarts donate-cpu.py.
 
@@ -13,6 +12,6 @@ cppcheck_tools_path=..
 while :
 do
     git pull
-    python "${cppcheck_tools_path}/donate-cpu.py" --bandwidth-limit=1m --stop-time=23:55
+    python "${cppcheck_tools_path}/donate-cpu.py" --bandwidth-limit=1m --max-packages=100
     sleep 10
 done
