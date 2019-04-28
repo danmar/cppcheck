fd4e3710911f4d440bc1946dd0a08cc4c21e300b
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2019-04-28 07:40:00 +0200

    Refactoring: Use stl algorithm

--- lib/astutils.cpp
+++ lib/astutils.cpp
@@ -739,30 +739,22 @@ bool isUniqueExpression(const Token* tok)
         // Iterate over the variables in scope and the parameters of the function if possible
         const Function * fun = scope->function;
         const std::list<Variable>* setOfVars[] = {&scope->varlist, fun ? &fun->argumentList : nullptr};
-        if (varType) {
-            for (const std::list<Variable>* vars:setOfVars) {
-                if (!vars)
-                    continue;
-                for (const Variable& v:*vars) {
-                    if (v.type() && v.type()->name() == varType->name() && v.name() != var->name()) {
-                        return false;
-                    }
-                }
-            }
-        } else {
-            for (const std::list<Variable>* vars:setOfVars) {
-                if (!vars)
-                    continue;
-                for (const Variable& v:*vars) {
-                    if (v.isFloatingType() == var->isFloatingType() &&
-                        v.isEnumType() == var->isEnumType() &&
-                        v.isClass() == var->isClass() &&
-                        v.isArray() == var->isArray() &&
-                        v.isPointer() == var->isPointer() &&
-                        v.name() != var->name())
-                        return false;
-                }
-            }
+
+        for (const std::list<Variable>* vars:setOfVars) {
+            if (!vars)
+                continue;
+            bool other = std::any_of(vars->cbegin(), vars->cend(), [=](const Variable &v) {
+                if (varType)
+                    return v.type() && v.type()->name() == varType->name() && v.name() != var->name();
+                return v.isFloatingType() == var->isFloatingType() &&
+                       v.isEnumType() == var->isEnumType() &&
+                       v.isClass() == var->isClass() &&
+                       v.isArray() == var->isArray() &&
+                       v.isPointer() == var->isPointer() &&
+                       v.name() != var->name();
+            });
+            if (other)
+                return false;
         }
     } else if (!isUniqueExpression(tok->astOperand1())) {
         return false;
