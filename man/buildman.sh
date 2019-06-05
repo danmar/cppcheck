5301c443745c5bee52063c880c858ae777fff659
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2019-06-05 10:46:49 +0200

    man: comment

--- man/buildman.sh
+++ man/buildman.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+# To install required tools in debian:
+# sudo apt-get install pandoc texlive-latex-base texlive-fonts-recommended texlive-latex-extra
 
-pandoc manual.md  -o manual.pdf -s --pdf-engine=pdflatex --number-sections
+pandoc manual.md  -o manual.pdf -s --number-sections
 pandoc manual.md  -o manual.html -s --number-sections
 
