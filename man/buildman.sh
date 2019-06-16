167950c8e7daacd22976c26fd94dcca6f43bde0e
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2019-06-16 10:08:37 +0200

    Manual: Move .cfg reference to its own document

--- man/buildman.sh
+++ man/buildman.sh
@@ -2,6 +2,8 @@
 # To install required tools in debian:
 # sudo apt-get install pandoc texlive-latex-base texlive-fonts-recommended texlive-latex-extra
 
-pandoc manual.md  -o manual.pdf -s --number-sections
-pandoc manual.md  -o manual.html -s --number-sections
+pandoc manual.md -o manual.pdf -s --number-sections
+pandoc manual.md -o manual.html -s --number-sections
 
+pandoc reference-cfg-format.md -o reference-cfg-format.pdf -s --number-sections
+pandoc reference-cfg-format.md -o reference-cfg-format.html -s --number-sections
