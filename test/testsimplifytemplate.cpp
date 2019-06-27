c2358be6d9b14735efe6a2020ce60ee3c26e23be
Robert Reif <reif@FX6840>
2019-06-27 16:24:23 -0400

    fix a test

--- test/testsimplifytemplate.cpp
+++ test/testsimplifytemplate.cpp
@@ -3533,8 +3533,8 @@ private:
         ASSERT_EQUALS(1U, templateParameters("template<template<template<template<typename>>>> x;"));
         ASSERT_EQUALS(1U, templateParameters("template<template<template<template<template<typename>>>>> x;"));
         ASSERT_EQUALS(2U, templateParameters("template<template<typename>,int> x;"));
+        ASSERT_EQUALS(2U, templateParameters("template<template<template<typename>>,int> x;"));
         ASSERT_EQUALS(2U, templateParameters("template<template<template<template<typename>>>,int> x;"));
-        ASSERT_EQUALS(2U, templateParameters("template<template<template<template<template<typename>>>>,int> x;"));
         ASSERT_EQUALS(2U, templateParameters("template<template<template<template<template<typename>>>>,int> x;"));
         ASSERT_EQUALS(2U, templateParameters("template<template<typename>...Foo,template<template<template<typename>>>> x;"));
         ASSERT_EQUALS(3U, templateParameters("template<template<typename>...Foo,int,template<template<template<typename>>>> x;"));
