danmar/cppcheck

commit 8513fb81d261fe62a822755f22ae4c24e112ae25

Fix #9206 (FP with global variable allocated in condition) (#2007)

lib/checkleakautovar.cpp
@@ -206,6 +206,30 @@ static bool isPointerReleased(const Token *startToken, const Token *endToken, no
     return false;
 }
 
+static bool checkVariable(const Token *varTok, const bool isCpp)
+{
+    // not a local variable nor argument?
+    const Variable *var = varTok->variable();
+    if (var && !var->isArgument() && (!var->isLocal() || var->isStatic()))
+        return false;
+
+    // Don't check reference variables
+    if (var && var->isReference())
+        return false;
+
+    // non-pod variable
+    if (isCpp) {
+        if (!var)
+            return false;
+        // Possibly automatically deallocated memory
+        if (!var->typeStartToken()->isStandardType() && Token::Match(varTok, "%var% = new"))
+            return false;
+        if (!var->isPointer() && !var->typeStartToken()->isStandardType())
+            return false;
+    }
+    return true;
+}
+
 /** checks if nameToken is a name of a function in a function call:
 *     func(arg)
 * or
@@ -318,25 +342,8 @@ void CheckLeakAutoVar::checkScope(const Token * const startToken,
                 leakIfAllocated(varTok, *varInfo);
             varInfo->erase(varTok->varId());
 
-            // not a local variable nor argument?
-            const Variable *var = varTok->variable();
-            if (var && !var->isArgument() && (!var->isLocal() || var->isStatic()))
+            if (!checkVariable(varTok, mTokenizer->isCPP()))
                 continue;
-
-            // Don't check reference variables
-            if (var && var->isReference())
-                continue;
-
-            // non-pod variable
-            if (mTokenizer->isCPP()) {
-                if (!var)
-                    continue;
-                // Possibly automatically deallocated memory
-                if (!var->typeStartToken()->isStandardType() && Token::Match(varTok, "%var% = new"))
-                    continue;
-                if (!var->isPointer() && !var->typeStartToken()->isStandardType())
-                    continue;
-            }
 
             // allocation?
             const Token *const fTok = tokRightAstOperand ? tokRightAstOperand->previous() : nullptr;
@@ -374,6 +381,9 @@ void CheckLeakAutoVar::checkScope(const Token * const startToken,
             const Token * closingParenthesis = tok->linkAt(1);
             for (const Token *innerTok = tok->tokAt(2); innerTok && innerTok != closingParenthesis; innerTok = innerTok->next()) {
                 // TODO: replace with checkTokenInsideExpression()
+
+                if (!checkVariable(innerTok, mTokenizer->isCPP()))
+                    continue;
 
                 if (Token::Match(innerTok, "%var% =") && innerTok->astParent() == innerTok->next()) {
                     // allocation?

test/testleakautovar.cpp
@@ -121,6 +121,7 @@ private:
         TEST_CASE(ifelse12); // #8340 - if ((*p = malloc(4)) == NULL)
         TEST_CASE(ifelse13); // #8392
         TEST_CASE(ifelse14); // #9130 - if (x == (char*)NULL)
+        TEST_CASE(ifelse15); // #9206 - if (global_ptr = malloc(1))
 
         // switch
         TEST_CASE(switch1);
@@ -1360,6 +1361,23 @@ private:
               "    if (buf == (char*)NULL)\n"
               "        return NULL;\n"
               "    return buf;\n"
+              "}");
+        ASSERT_EQUALS("", errout.str());
+    }
+
+    void ifelse15() { // #9206
+        check("struct SSS { int a; };\n"
+              "SSS* global_ptr;\n"
+              "void test_alloc() {\n"
+              "   if ( global_ptr = new SSS()) {}\n"
+              "   return;\n"
+              "}", true);
+        ASSERT_EQUALS("", errout.str());
+
+        check("FILE* hFile;\n"
+              "int openFile( void ) {\n"
+              "   if ((hFile = fopen(\"1.txt\", \"wb\" )) == NULL) return 0;\n"
+              "   return 1;\n"
               "}");
         ASSERT_EQUALS("", errout.str());
     }

commit 295026d9f5d3f822e5ea9f164618446bef83a864

Fix FP memory leak with unknown function call in condition
This was introduced in 8513fb81d261fe62a822755f22ae4c24e112ae25 when
fixing memory leaks for global variables allocated in condition. The
refactored code had an inconsistency where c and c++ code behaved
slightly differently when `var` is NULL. This seemed to not have an
impact as the code was written prior to 8513fb81d261fe62a822755f22ae4c24e112ae25,
but when the same code was used for conditions, FPs were introduced.

The introduced FPs were memleak warnings when there should have been an
information message about missing configurations for code like

	void f() {
		char *p = malloc(10);
		if (set_data(p)) {}
	}

Fix this by always returning true if varTok->Variable() is NULL for
both c and c++ code.

lib/checkleakautovar.cpp
@@ -233,17 +233,17 @@ static bool checkVariable(const Token *varTok, const bool isCpp)
 {
     // not a local variable nor argument?
     const Variable *var = varTok->variable();
-    if (var && !var->isArgument() && (!var->isLocal() || var->isStatic()))
+    if (!var)
+        return true;
+    if (!var->isArgument() && (!var->isLocal() || var->isStatic()))
         return false;
 
     // Don't check reference variables
-    if (var && var->isReference())
+    if (var->isReference())
         return false;
 
     // non-pod variable
     if (isCpp) {
-        if (!var)
-            return false;
         // Possibly automatically deallocated memory
         if (isAutoDealloc(var) && Token::Match(varTok, "%var% = new"))
             return false;

test/testleakautovar.cpp
@@ -1767,18 +1767,26 @@ private:
     }
 
     void configuration3() {
-        check("void f() {\n"
-              "    char *p = malloc(10);\n"
-              "    if (set_data(p)) { }\n"
-              "}");
+        const char * code = "void f() {\n"
+                            "    char *p = malloc(10);\n"
+                            "    if (set_data(p)) { }\n"
+                            "}";
+        check(code);
         ASSERT_EQUALS("[test.c:4]: (information) --check-library: Function set_data() should have <use>/<leak-ignore> configuration\n", errout.str());
+        check(code, true);
+        ASSERT_EQUALS("[test.cpp:4]: (information) --check-library: Function set_data() should have <use>/<leak-ignore> configuration\n", errout.str());
 
-        check("void f() {\n"
-              "    char *p = malloc(10);\n"
-              "    if (set_data(p)) { return; }\n"
-              "}");
+        code = "void f() {\n"
+               "    char *p = malloc(10);\n"
+               "    if (set_data(p)) { return; }\n"
+               "}";
+        check(code);
         ASSERT_EQUALS("[test.c:3]: (information) --check-library: Function set_data() should have <use>/<leak-ignore> configuration\n"
                       "[test.c:4]: (information) --check-library: Function set_data() should have <use>/<leak-ignore> configuration\n"
+                      , errout.str());
+        check(code, true);
+        ASSERT_EQUALS("[test.cpp:3]: (information) --check-library: Function set_data() should have <use>/<leak-ignore> configuration\n"
+                      "[test.cpp:4]: (information) --check-library: Function set_data() should have <use>/<leak-ignore> configuration\n"
                       , errout.str());
     }
 