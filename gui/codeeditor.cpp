dab0845ef1718b614bfe5c143dd4db0b247144de
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2019-07-23 10:28:45 +0200

    Travis: Revert width=>horizontalAdvance change to fix CI

--- gui/codeeditor.cpp
+++ gui/codeeditor.cpp
@@ -265,7 +265,7 @@ int CodeEditor::lineNumberAreaWidth()
         ++digits;
     }
 
-    int space = 3 + fontMetrics().horizontalAdvance(QLatin1Char('9')) * digits;
+    int space = 3 + fontMetrics().width(QLatin1Char('9')) * digits;
     return space;
 }
 
