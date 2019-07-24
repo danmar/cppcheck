33d3c061c1f73dcdd952f7f882e704413e07d331
Robert Reif <reif@FX6840>
2019-07-24 00:16:53 -0400

    fix cppcheck warnings

--- lib/templatesimplifier.cpp
+++ lib/templatesimplifier.cpp
@@ -794,8 +794,14 @@ bool TemplateSimplifier::getTemplateDeclarations()
             continue;
         // skip to last nested template parameter
         const Token *tok1 = tok;
-        while (tok1 && tok1->next() && Token::Match(tok1->next()->findClosingBracket(), "> template <"))
-            tok1 = tok1->next()->findClosingBracket()->next();
+        while (tok1 && tok1->next() && Token::simpleMatch(tok1->next()->findClosingBracket(), "> template <")) {
+            const Token *closing = tok1->next()->findClosingBracket();
+            if (!closing)
+                syntaxError(tok1->next());
+            tok1 = closing->next();
+        }
+        if (!tok1)
+            syntaxError(tok);
         // Some syntax checks, see #6865
         if (!tok->tokAt(2))
             syntaxError(tok->next());
