5b7f1bd621f4694a1500f3c949b19b100250b307
Georgy Komarov <jubnzv@gmail.com>
2019-07-26 07:38:03 +0300

    Update addons structure. Make Y2038 addon works. (#2024)
    * Update addons structure. Make Y2038 addon works.
    
    All addons are now located in same directory, where cppcheck-gui is
    looking for them.
    
    Y2038 addon has been updated to latest cppcheck version. Output is same
    as 303622f01c01ca5acdac2504f2dab442b5343cab commit as it described in
    README: https://github.com/danmar/cppcheck/blob/303622f01c01ca5acdac2504f2dab442b5343cab/addons/y2038/README.
    
    * Clean up .travis.yml
    
    * Update travis.yml paths.
    
    * misra.py: Bring back -P argument for backward compatibility
    
    * Fix paths
    
    * Normalize dumpfile paths tpo cppcheck format
    
    * Fixup 3 test.
    
    * Add arguments regression test.
    
    * Fixing travis build

--- addons/test/test-misra.py
+++ addons/test/test-misra.py
@@ -1,29 +1,14 @@
 # python -m pytest addons/test/test-misra.py
-import json
+
 import pytest
 import re
 import sys
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from io import StringIO
 import subprocess
 
-
-TEST_SOURCE_FILES = ['./addons/test/misra-test.c']
-
-
-def dump_create(fpath, *argv):
-    cmd = ["./cppcheck", "--dump", "--quiet", fpath] + list(argv)
-    p = subprocess.Popen(cmd)
-    p.communicate()
-    if p.returncode != 0:
-        raise OSError("cppcheck returns error code: %d" % p.returncode)
-    subprocess.Popen(["sync"])
+from .util import dump_create, dump_remove, convert_json_output
 
 
-def dump_remove(fpath):
-    subprocess.Popen(["rm", "-f", fpath + ".dump"])
+TEST_SOURCE_FILES = ['./addons/test/misra/misra-test.c']
 
 
 def setup_module(module):
@@ -45,20 +30,20 @@ def checker():
 
 
 def test_loadRuleTexts_structure(checker):
-    checker.loadRuleTexts("./addons/test/assets/misra_rules_structure.txt")
+    checker.loadRuleTexts("./addons/test/misra/misra_rules_structure.txt")
     assert(checker.ruleTexts.get(101, None) is None)
     assert(checker.ruleTexts[102].text == "Rule text.")
     assert(checker.ruleTexts.get(103, None) is None)
 
 
 def test_loadRuleTexts_empty_lines(checker):
-    checker.loadRuleTexts("./addons/test/assets/misra_rules_empty_lines.txt")
+    checker.loadRuleTexts("./addons/test/misra/misra_rules_empty_lines.txt")
     assert(len(checker.ruleTexts) == 3)
     assert(len(checker.ruleTexts[102].text) == len("Rule text."))
 
 
 def test_loadRuleTexts_mutiple_lines(checker):
-    checker.loadRuleTexts("./addons/test/assets/misra_rules_multiple_lines.txt")
+    checker.loadRuleTexts("./addons/test/misra/misra_rules_multiple_lines.txt")
     assert(checker.ruleTexts[101].text == "Multiple lines text.")
     assert(checker.ruleTexts[102].text == "Multiple lines text.")
     assert(checker.ruleTexts[103].text == "Multiple lines text.")
@@ -68,7 +53,7 @@ def test_loadRuleTexts_mutiple_lines(checker):
 
 
 def test_verifyRuleTexts(checker, capsys):
-    checker.loadRuleTexts("./addons/test/assets/misra_rules_dummy.txt")
+    checker.loadRuleTexts("./addons/test/misra/misra_rules_dummy.txt")
     checker.verifyRuleTexts()
     captured = capsys.readouterr().out
     assert("21.3" not in captured)
@@ -76,7 +61,7 @@ def test_verifyRuleTexts(checker, capsys):
 
 
 def test_rules_misra_severity(checker):
-    checker.loadRuleTexts("./addons/test/assets/misra_rules_dummy.txt")
+    checker.loadRuleTexts("./addons/test/misra/misra_rules_dummy.txt")
     assert(checker.ruleTexts[1004].misra_severity == 'Mandatory')
     assert(checker.ruleTexts[401].misra_severity == 'Required')
     assert(checker.ruleTexts[1505].misra_severity == 'Advisory')
@@ -85,26 +70,20 @@ def test_rules_misra_severity(checker):
 
 def test_json_out(checker, capsys):
     sys.argv.append("--cli")
-    checker.loadRuleTexts("./addons/test/assets/misra_rules_dummy.txt")
-    checker.parseDump("./addons/test/misra-test.c.dump")
+    checker.loadRuleTexts("./addons/test/misra/misra_rules_dummy.txt")
+    checker.parseDump("./addons/test/misra/misra-test.c.dump")
     captured = capsys.readouterr()
     captured = captured.out.splitlines()
     sys.argv.remove("--cli")
-    json_output = {}
-    for line in captured:
-        try:
-            json_line = json.loads(line)
-            json_output[json_line['errorId']] = json_line
-        except ValueError:
-            pass
-    assert("Mandatory" in json_output['c2012-10.4']['extra'])
-    assert("Required" in json_output['c2012-21.3']['extra'])
-    assert("Advisory" in json_output['c2012-20.1']['extra'])
+    json_output = convert_json_output(captured)
+    assert("Mandatory" in json_output['c2012-10.4'][0]['extra'])
+    assert("Required" in json_output['c2012-21.3'][0]['extra'])
+    assert("Advisory" in json_output['c2012-20.1'][0]['extra'])
 
 
 def test_rules_cppcheck_severity(checker, capsys):
-    checker.loadRuleTexts("./addons/test/assets/misra_rules_dummy.txt")
-    checker.parseDump("./addons/test/misra-test.c.dump")
+    checker.loadRuleTexts("./addons/test/misra/misra_rules_dummy.txt")
+    checker.parseDump("./addons/test/misra/misra-test.c.dump")
     captured = capsys.readouterr().err
     assert("(error)" not in captured)
     assert("(warning)" not in captured)
@@ -112,13 +91,13 @@ def test_rules_cppcheck_severity(checker, capsys):
 
 
 def test_rules_suppression(checker, capsys):
-    test_sources = ["addons/test/misra-suppressions1-test.c",
-                    "addons/test/misra-suppressions2-test.c"]
+    test_sources = ["addons/test/misra/misra-suppressions1-test.c",
+                    "addons/test/misra/misra-suppressions2-test.c"]
 
     for src in test_sources:
         re_suppressed= r"\[%s\:[0-9]+\]" % src
         dump_remove(src)
-        dump_create(src, "--suppressions-list=addons/test/suppressions.txt")
+        dump_create(src, "--suppressions-list=addons/test/misra/suppressions.txt")
         checker.parseDump(src + ".dump")
         captured = capsys.readouterr().err
         found = re.search(re_suppressed, captured)
