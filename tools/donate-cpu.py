ee72bb71073432e966701c96522655d3fa3493de
Sebastian <versat@users.noreply.github.com>
2019-07-26 13:50:04 +0200

    donate-cpu.py: Improve detection of Qt library usage (#2042)

--- tools/donate-cpu.py
+++ tools/donate-cpu.py
@@ -40,7 +40,7 @@ import platform
 # Version scheme (MAJOR.MINOR.PATCH) should orientate on "Semantic Versioning" https://semver.org/
 # Every change in this script should result in increasing the version number accordingly (exceptions may be cosmetic
 # changes)
-CLIENT_VERSION = "1.1.26"
+CLIENT_VERSION = "1.1.27"
 
 
 def check_requirements():
@@ -264,7 +264,7 @@ def scan_package(work_path, cppcheck_path, jobs):
                        'opengl': ['<GL/gl.h>', '<GL/glu.h>', '<GL/glut.h>'],
                        # 'openmp': ['<omp.h>'], <= enable after release of version 1.89
                        'python': ['<Python.h>', '"Python.h"'],
-                       'qt': ['<QApplication>', '<QString>', '<QWidget>', '<QtWidgets>', '<QtGui'],
+                       'qt': ['<QApplication>', '<QList>', '<QObject>', '<QString>', '<QWidget>', '<QtWidgets>', '<QtGui'],
                        'ruby': ['<ruby.h>', '<ruby/'],
                        'sdl': ['<SDL.h>'],
                        'sqlite3': ['<sqlite3.h>', '"sqlite3.h"'],
