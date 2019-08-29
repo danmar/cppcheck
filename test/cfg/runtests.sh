6ffb21880008ad687590f321e32b50a30a860f9a
Sebastian <versat@users.noreply.github.com>
2019-08-29 15:11:59 +0200

    libcurl.cfg: Add library configuration for libcurl (#2120)
    Add curl_easy_*() functions and deprecated functions with warnings.
    Add tests and prepare donate-cpu.py
    Reference: https://curl.haxx.se/libcurl/c/

--- test/cfg/runtests.sh
+++ test/cfg/runtests.sh
@@ -226,6 +226,33 @@ else
 fi
 ${CPPCHECK} ${CPPCHECK_OPT} --library=lua ${DIR}lua.c
 
+# libcurl.c
+set +e
+pkg-config --version
+PKGCONFIG_RETURNCODE=$?
+set -e
+if [ $PKGCONFIG_RETURNCODE -ne 0 ]; then
+    echo "pkg-config needed to retrieve libcurl configuration is not available, skipping syntax check."
+else
+    set +e
+    LIBCURLCONFIG=$(pkg-config --cflags libcurl)
+    LIBCURLCONFIG_RETURNCODE=$?
+    set -e
+    if [ $LIBCURLCONFIG_RETURNCODE -eq 0 ]; then
+        set +e
+        echo -e "#include <curl/curl.h>" | ${CC} ${CC_OPT} ${LIBCURLCONFIG} -x c -
+        LIBCURLCONFIG_RETURNCODE=$?
+        set -e
+        if [ $LIBCURLCONFIG_RETURNCODE -ne 0 ]; then
+            echo "libcurl not completely present or not working, skipping syntax check with ${CC}."
+        else
+            echo "libcurl found and working, checking syntax with ${CC} now."
+            ${CC} ${CC_OPT} ${LIBCURLCONFIG} ${DIR}libcurl.c
+        fi
+    fi
+fi
+${CPPCHECK} ${CPPCHECK_OPT} --library=libcurl ${DIR}libcurl.c
+
 # Check the syntax of the defines in the configuration files
 set +e
 xmlstarlet --version
