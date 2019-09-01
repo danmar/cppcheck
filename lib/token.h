a2bc3cbf153bca8b7b8ddd41372b14be14c7940a
Ken-Patrick Lehrmann <kp.lehrmann@gmail.com>
2019-09-01 14:33:48 +0200

    Fix issue 8897: Huge array initializations
    iscpp11init would take a lot of time when parsing huge arrays.
    This patch add memoization to keeps track that we are parsing an array,
    and allows to propagate the result without re-parsing the array for each
    of its members.

--- lib/token.h
+++ lib/token.h
@@ -117,8 +117,12 @@ struct TokenImpl {
     };
     struct CppcheckAttributes *mCppcheckAttributes;
 
+    // For memoization, to speed up parsing of huge arrays #8897
+    enum Cpp11init {UNKNOWN, CPP11INIT, NOINIT} mCpp11init;
+
     void setCppcheckAttribute(CppcheckAttributes::Type type, MathLib::bigint value);
     bool getCppcheckAttribute(CppcheckAttributes::Type type, MathLib::bigint *value) const;
+
 
     TokenImpl()
         : mVarId(0)
@@ -139,6 +143,7 @@ struct TokenImpl {
         , mTemplateSimplifierPointers()
         , mScopeInfo(nullptr)
         , mCppcheckAttributes(nullptr)
+        , mCpp11init(UNKNOWN)
     {}
 
     ~TokenImpl();
@@ -1220,6 +1225,9 @@ public:
 
     void scopeInfo(std::shared_ptr<ScopeInfo2> newScopeInfo);
     std::shared_ptr<ScopeInfo2> scopeInfo() const;
+
+    void setCpp11init(bool cpp11init) const { mImpl->mCpp11init=cpp11init ? TokenImpl::CPP11INIT : TokenImpl::NOINIT; }
+    TokenImpl::Cpp11init isCpp11init() const { return mImpl->mCpp11init; }
 };
 
 /// @}
