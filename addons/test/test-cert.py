dc1cdd2b761e3f521269d44ead39d1a5d7488ff2
versat <versat@users.noreply.github.com>
2019-09-20 13:39:38 +0200

    addons/test/test-*.py: Improve documentation about running the tests

--- addons/test/test-cert.py
+++ addons/test/test-cert.py
@@ -1,4 +1,11 @@
+# Running the test with Python 2:
+# Be sure to install pytest version 4.6.4 (newer should also work)
+# Command in cppcheck directory:
 # python -m pytest addons/test/test-cert.py
+#
+# Running the test with Python 3:
+# Command in cppcheck directory:
+# PYTHONPATH=./addons python3 -m pytest addons/test/test-cert.py
 
 import sys
 import pytest
