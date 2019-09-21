46ac0d79c1036afb0c565e2bc330d9eb5ffa9eb1
Rikard Falkeborn <rikard.falkeborn@gmail.com>
2019-09-21 14:59:54 +0200

    Checkmemleakautovar: fix crash and FP (#2196)
    This fixes crashes found by daca where valueType() is NULL. Also,
    somewhat related, it removes warnings when casting to a type that is
    unknown to cppcheck, for example, there is no longer a warning for the
    following code:
    
    	void* f() {
    		void *x = malloc(1);
    		return (mytype)x;
    	}

--- lib/checkleakautovar.cpp
+++ lib/checkleakautovar.cpp
@@ -936,7 +936,10 @@ void CheckLeakAutoVar::ret(const Token *tok, const VarInfo &varInfo)
                     continue;
 
                 tok2 = tok2->next();
-                while (tok2 && tok2->isCast() && (tok2->valueType() && (tok2->valueType()->pointer || (tok2->valueType()->typeSize(*mSettings) >= mSettings->sizeof_pointer))))
+                while (tok2 && tok2->isCast() && tok2->valueType() &&
+                       (tok2->valueType()->pointer ||
+                        (tok2->valueType()->typeSize(*mSettings) == 0) ||
+                        (tok2->valueType()->typeSize(*mSettings) >= mSettings->sizeof_pointer)))
                     tok2 = tok2->astOperand2() ? tok2->astOperand2() : tok2->astOperand1();
                 if (Token::Match(tok2, "%varid%", varid))
                     tok2 = tok2->next();
