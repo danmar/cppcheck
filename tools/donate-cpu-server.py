783f2631c4009e13ad2c16ef28e437976f5c7fb0
Rikard Falkeborn <rikard.falkeborn@gmail.com>
2019-10-16 23:21:00 +0200

    donate-cpu-server: Bump version

--- tools/donate-cpu-server.py
+++ tools/donate-cpu-server.py
@@ -18,7 +18,7 @@ import operator
 # Version scheme (MAJOR.MINOR.PATCH) should orientate on "Semantic Versioning" https://semver.org/
 # Every change in this script should result in increasing the version number accordingly (exceptions may be cosmetic
 # changes)
-SERVER_VERSION = "1.1.6"
+SERVER_VERSION = "1.1.7"
 
 OLD_VERSION = '1.89'
 
