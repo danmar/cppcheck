c95b5d0a3c3b45a18d0e093da6d9cb7a70dd8a26
Armin Müller <27155134+arm-in@users.noreply.github.com>
2019-11-03 08:27:05 +0100

    donate-cpu-server.py: Add explanation how to read timing list (#2315)
    * Add explanation how to read timing list
    
    * fixed HTML

--- tools/donate-cpu-server.py
+++ tools/donate-cpu-server.py
@@ -621,6 +621,8 @@ def timeReport(resultPath: str) -> str:
             break
 
     html += '\n'
+    html += '(listed above are all suspicious timings with a factor &lt;0.50 or &gt;2.00)\n'
+    html += '\n'
     if total_time_base > 0.0:
         total_time_factor = total_time_head / total_time_base
     else:
