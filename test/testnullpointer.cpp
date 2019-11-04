e1c32e85b83b435d1d75bbd19d2d8518304877c7
Paul <pfultz2@yahoo.com>
2019-11-03 20:07:19 -0600

    Fix issue 9395 and 9423: False positive: nullPointerRedundantCheck

--- test/testnullpointer.cpp
+++ test/testnullpointer.cpp
@@ -82,6 +82,7 @@ private:
         TEST_CASE(nullpointer40);
         TEST_CASE(nullpointer41);
         TEST_CASE(nullpointer42);
+        TEST_CASE(nullpointer44); // #9395, #9423
         TEST_CASE(nullpointer_addressOf); // address of
         TEST_CASE(nullpointerSwitch); // #2626
         TEST_CASE(nullpointer_cast); // #4692
@@ -1535,6 +1536,35 @@ private:
         ASSERT_EQUALS(
             "[test.cpp:3] -> [test.cpp:4]: (warning) Either the condition 'a->g()==nullptr' is redundant or there is possible null pointer dereference: a->g().\n",
             errout.str());
+    }
+
+    void nullpointer44() {
+        // #9395
+        check("int foo( ) {\n"
+              "    const B* b = getB();\n"
+              "    const double w = ( nullptr != b) ? 42. : 0.0;\n"
+              "    if ( w == 0.0 )\n"
+              "        return 0;\n"
+              "    return b->get();\n"
+              "}\n");
+        ASSERT_EQUALS("", errout.str());
+        // #9423
+        check("extern F* GetF();\n"
+              "extern L* GetL();\n"
+              "void Foo() {\n"
+              "    const F* const fPtr = GetF();\n"
+              "    const bool fPtrOk = fPtr != NULL;\n"
+              "    assert(fPtrOk);\n"
+              "    if (!fPtrOk)\n"
+              "        return;\n"
+              "    L* const lPtr = fPtr->l;\n"
+              "    const bool lPtrOk = lPtr != NULL;\n"
+              "    assert(lPtrOk);\n"
+              "    if (!lPtrOk)\n"
+              "        return;\n"
+              "    lPtr->Clear();\n"
+              "}\n");
+        ASSERT_EQUALS("", errout.str());
     }
 
     void nullpointer_addressOf() { // address of
