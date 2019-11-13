da163ae0e80fb1fc1b6692fbcee19cfd43db539e
Georgy Komarov <jubnzv@gmail.com>
2019-11-13 22:36:31 +0300

    misra.py: Add regression test for R21.1 crash (#2356)

--- addons/test/misra/misra-test.cpp
+++ addons/test/misra/misra-test.cpp
@@ -1,4 +1,3 @@
-
 // #8441
 class C {
     int a;
@@ -6,3 +5,13 @@ class C {
     C() : a(1), b(1) { c; }
 };
 
+class misra_21_1_C {
+    public:
+       misra_21_1_C operator=(const misra_21_1_C &);
+};
+
+static bool test_misra_21_1_crash(const C* cc)
+{
+    auto misra_21_1_C a, b;
+    a = b;
+}
