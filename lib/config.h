678560bf0d1195ab78a306d2c38280ac96591793
Dmitry-Me <Dmitry-Me@users.noreply.github.com>
2019-12-25 18:19:00 +0300

    Use "override" keyword in clang (#2459)

--- lib/config.h
+++ lib/config.h
@@ -20,7 +20,9 @@
 #endif
 
 // C++11 override
-#if defined(_MSC_VER) || (defined(__GNUC__) && (__GNUC__ >= 5)) || defined(__CPPCHECK__)
+#if defined(_MSC_VER) || (defined(__GNUC__) && (__GNUC__ >= 5)) \
+  || (defined(__clang__) && (defined (__cplusplus)) && (__cplusplus >= 201103L)) \
+  || defined(__CPPCHECK__)
 #  define OVERRIDE override
 #else
 #  define OVERRIDE
