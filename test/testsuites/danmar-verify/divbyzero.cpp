ad352838c0c404f6d1eaf7c9bb13d2c2a1fd9930
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2019-12-28 22:23:11 +0100

    Verification; Add some more testcases

---
+++ test/testsuites/danmar-verify/divbyzero.cpp
@@ -0,0 +1,26 @@
+
+void pointer1(int *p) {
+    // cppcheck-suppress verificationDivByZero
+    return 100000 / *p;
+}
+
+void pointer2(int *p) {
+    // cppcheck-suppress verificationDivByZero
+    return 100000 / p[32];
+}
+
+void float(float f) {
+    // cppcheck-suppress verificationDivByZero
+    return 100000 / (int)f;
+}
+
+void stdmap(std::map<int,int> &data) {
+    // cppcheck-suppress verificationDivByZero
+    return 100000 / data[43];
+}
+
+struct S { int x };
+void struct1(struct S *s) {
+    // cppcheck-suppress verificationDivByZero
+    return 100000 / s->x;
+}
