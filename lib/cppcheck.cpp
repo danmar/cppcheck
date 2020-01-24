7db54bde6f16dfbf52f70f14fd5a4aa3187ca9dc
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2020-01-24 18:04:39 +0100

    Run normal checks on --clang output

--- lib/cppcheck.cpp
+++ lib/cppcheck.cpp
@@ -340,12 +340,13 @@ unsigned int CppCheck::check(const std::string &path)
         ValueFlow::setValues(&tokenizer.list, const_cast<SymbolDatabase *>(tokenizer.getSymbolDatabase()), this, &mSettings);
         if (mSettings.debugnormal)
             tokenizer.printDebugOutput(1);
-
 #ifdef USE_Z3
         if (mSettings.bugHunting)
             ExprEngine::runChecks(this, &tokenizer, &mSettings);
+        else
 #endif
-        return 0;
+        checkNormalTokens(tokenizer);
+        return mExitCode;
     }
 
     std::ifstream fin(path);
