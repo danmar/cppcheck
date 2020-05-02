b204be474a76720003fa88715dae4256f7576542
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2020-05-02 21:57:36 +0200

    Refactoring bug hunting test cases for CVE issues. Leave source code unmodified.

--- test/bug-hunting/cve.py
+++ test/bug-hunting/cve.py
@@ -14,32 +14,45 @@ else:
     CPPCHECK_PATH = '../../cppcheck'
     TEST_SUITE = 'cve'
 
-RUN_CLANG = ('--clang' in sys.argv)
+def test(test_folder):
+    print(test_folder)
 
-def check():
+    cmd_file = os.path.join(test_folder, 'cmd.txt')
+    expected_file = os.path.join(test_folder, 'expected.txt')
+
     cmd = [CPPCHECK_PATH,
            '-D__GNUC__',
            '--bug-hunting',
            '--inconclusive',
            '--platform=unix64',
-           '--inline-suppr',
-           '--enable=information',
-           TEST_SUITE]
-    if RUN_CLANG:
-        cmd.append('--clang')
-    print(' '.join(cmd))
+           '--template={file}:{line}:{id}',
+           '-rp=' + test_folder,
+           test_folder]
+
+    if os.path.isfile(cmd_file):
+        for line in open(cmd_file, 'rt'):
+            if len(line) > 1:
+                cmd.append(line.strip())
 
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     comm = p.communicate()
     stdout = comm[0].decode(encoding='utf-8', errors='ignore')
     stderr = comm[1].decode(encoding='utf-8', errors='ignore')
 
-    # Ensure there are no unmatched suppressions
-    if '[unmatchedSuppression]' in stderr:
-        print('FAILED: There are unmatched suppressions')
-        sys.exit(1)
-    else:
-        print('SUCCESS')
+    with open(expected_file, 'rt') as f:
+        for expected in f.readlines():
+            if expected.strip() not in stderr.split('\n'):
+                print('FAILED. Expected result not found: ' + expected)
+                print('Command:')
+                print(' '.join(cmd))
+                print('Output:')
+                print(stderr)
+                sys.exit(1)
 
+if len(sys.argv) > 1:
+    test(sys.argv[1])
+    sys.exit(0)
 
-check()
+for test_folder in sorted(glob.glob(TEST_SUITE + '/CVE*')):
+    test(test_folder)
+
