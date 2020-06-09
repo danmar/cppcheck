0c659a149953b9a67901a5b30259b7ef534bbaad
Paul Fultz II <pfultz2@yahoo.com>
2020-06-09 01:16:53 -0500

    Fix incorrect logic for condition (#2675)

--- lib/forwardanalyzer.cpp
+++ lib/forwardanalyzer.cpp
@@ -299,7 +299,7 @@ struct ForwardTraversal {
                     if (!condTok->hasKnownIntValue()) {
                         if (!analyzer->lowerToPossible())
                             return Progress::Break;
-                    } else if (condTok->values().front().intvalue == !inElse) {
+                    } else if (condTok->values().front().intvalue == inElse) {
                         return Progress::Break;
                     }
                     analyzer->assume(condTok, !inElse, tok);
