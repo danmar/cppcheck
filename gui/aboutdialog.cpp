0f28e7b88e98865c6d74d525cc2f97d0d6263300
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2020-06-14 07:35:37 +0200

    AboutDialog: Updated copyright year so we don't have to manually tweak it. Thank you Z3 library.

--- gui/aboutdialog.cpp
+++ gui/aboutdialog.cpp
@@ -31,6 +31,8 @@ AboutDialog::AboutDialog(const QString &version, const QString &extraVersion, QW
         fmtVersion += " (" + extraVersion + ")";
     }
     mUI.mVersion->setText(mUI.mVersion->text().arg(fmtVersion));
+    QString date = __DATE__;
+    mUI.mCopyright->setText(mUI.mCopyright->text().arg(date.right(4)));
     QString url = "<a href=\"http://cppcheck.net/\">http://cppcheck.net/</a>";
     mUI.mHomepage->setText(mUI.mHomepage->text().arg(url));
     connect(mUI.mButtons, &QDialogButtonBox::accepted, this, &AboutDialog::accept);
