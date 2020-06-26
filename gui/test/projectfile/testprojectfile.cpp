303032b210f8e12ff060b2b2aaff5509c943699c
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2020-06-26 20:35:18 +0200

    gui/test/projectfile: mocked constructors

--- gui/test/projectfile/testprojectfile.cpp
+++ gui/test/projectfile/testprojectfile.cpp
@@ -27,6 +27,10 @@ const char Settings::SafeChecks::XmlClasses[] = "class-public";
 const char Settings::SafeChecks::XmlExternalFunctions[] = "external-functions";
 const char Settings::SafeChecks::XmlInternalFunctions[] = "internal-functions";
 const char Settings::SafeChecks::XmlExternalVariables[] = "external-variables";
+Settings::Settings() : maxCtuDepth(10), maxTemplateRecursion(100) {}
+cppcheck::Platform::Platform() {}
+Library::Library() {}
+ImportProject::ImportProject() {}
 
 void TestProjectFile::loadInexisting()
 {
