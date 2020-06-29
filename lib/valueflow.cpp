07d8cb4f01b1eabbe7db45b0e44be7f43dffbdc8
Paul <pfultz2@yahoo.com>
2020-06-29 11:55:59 -0500

    Extend scope of afterCondition until end of function

--- lib/valueflow.cpp
+++ lib/valueflow.cpp
@@ -4232,7 +4232,10 @@ struct ValueFlowConditionHandler {
                             // TODO: constValue could be true if there are no assignments in the conditional blocks and
                             //       perhaps if there are no && and no || in the condition
                             bool constValue = false;
-                            forward(after, top->scope()->bodyEnd, cond.vartok, values, constValue);
+                            const Scope* scope = top->scope();
+                            while(scope->nestedIn && scope->type != Scope::eFunction)
+                                scope = scope->nestedIn;
+                            forward(after, scope->bodyEnd, cond.vartok, values, constValue);
                         }
                     }
                 }
