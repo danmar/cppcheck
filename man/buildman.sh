60362ce8cdf7551486b897822f83a48622fa3082
Simon Large <simon@skirridsystems.co.uk>
2020-07-23 15:39:23 +0100

    Add styling to PDF manual
    This uses the xelatex engine and a few style settings to make the PDF manual look more like the HTML manual.

--- man/buildman.sh
+++ man/buildman.sh
@@ -2,7 +2,8 @@
 # To install required tools in debian:
 # sudo apt-get install pandoc texlive-latex-base texlive-fonts-recommended texlive-latex-extra
 
-pandoc manual.md -o manual.pdf -s --number-sections --toc --css manual.css
+pandoc manual.md -o manual.pdf -s --number-sections --toc -f markdown --pdf-engine=xelatex --listings -V mainfont="Segoe UI" -V monofont="Consolas" -V geometry:a4paper -V geometry:margin=2.4cm -H manual-style.tex
+
 pandoc manual.md -o manual.html -s --number-sections --toc --css manual.css
 
 pandoc reference-cfg-format.md -o reference-cfg-format.pdf -s --number-sections --toc
