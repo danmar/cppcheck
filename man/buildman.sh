ec99be033229e3c5a9653d8a9896b65f3c5605ab
Simon Large <simon@skirridsystems.co.uk>
2020-07-24 18:54:06 +0100

    Create separate scripts for building HTML and PDF documents
    The PDF build command line is getting very long, and it needs another option. Create separate build scripts for HTML and PDF and just pass in the name of the markdown file.
    
    Adds -V subparagraph to fix a LaTeX error.
    
    Adds fallback font selection to the PDF script.

--- man/buildman.sh
+++ man/buildman.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # To install required tools in debian:
 # sudo apt-get install pandoc texlive-latex-base texlive-fonts-recommended texlive-latex-extra
+# For Windows you can use the MiKTeX installer https://miktex.org/download
 
-pandoc manual.md -o manual.pdf -s --number-sections --toc -f markdown --pdf-engine=xelatex --listings -V mainfont="Segoe UI" -V monofont="Consolas" -V geometry:a4paper -V geometry:margin=2.4cm -H manual-style.tex
+./build-pdf.sh manual
+./build-html.sh manual
 
-pandoc manual.md -o manual.html -s --number-sections --toc --css manual.css
-
-pandoc reference-cfg-format.md -o reference-cfg-format.pdf -s --number-sections --toc
-pandoc reference-cfg-format.md -o reference-cfg-format.html -s --number-sections --toc
+./build-pdf.sh reference-cfg-format
+./build-html.sh reference-cfg-format
