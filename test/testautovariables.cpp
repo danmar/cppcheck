a266a00a448622a41c8b7adb6f32ea2ad40a506d
Paul <pfultz2@yahoo.com>
2020-08-31 20:42:24 -0500

    Fix issue 9853: False positive: returnReference when using a pointer to container

--- test/testautovariables.cpp
+++ test/testautovariables.cpp
@@ -2142,6 +2142,13 @@ private:
               "    return std::vector<char*>{&a};\n"
               "}\n");
         ASSERT_EQUALS("[test.cpp:3] -> [test.cpp:3] -> [test.cpp:2] -> [test.cpp:3]: (error) Returning object that points to local variable 'a' that will be invalid when returning.\n", errout.str());
+
+        check("std::vector<int>* g();\n"
+              "int& f() {\n"
+              "    auto* p = g();\n"
+              "    return p->front();\n"
+              "}\n");
+        ASSERT_EQUALS("", errout.str());
     }
 
     void danglingLifetime() {
