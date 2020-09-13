88c00ca2d52f6d729882a579e55b166184762c3c
shane <thatguyiknow5@hotmail.com>
2020-09-14 00:19:49 +0800

    add test

--- test/testtokenize.cpp
+++ test/testtokenize.cpp
@@ -490,6 +490,8 @@ private:
         TEST_CASE(noCrash1);
         TEST_CASE(noCrash2);
 
+        TEST_CASE(noCrash3);
+
         // --check-config
         TEST_CASE(checkConfiguration);
 
@@ -8452,6 +8454,10 @@ private:
                             "template <> d<int>::d(const int &, a::b, double, double);\n"
                             "template <> d<int>::d(const d &) {}\n"
                             "template <> d<c>::d(const d &) {}\n"));
+    }
+
+    void noCrash3() {
+        ASSERT_NO_THROW(tokenizeAndStringify("void a(X<int> x, typename Y1::Y2<int, A::B::C, 2> y, Z z = []{});"));
     }
 
     void checkConfig(const char code[]) {
