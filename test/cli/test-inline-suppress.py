27fbe28804dfc3027fab4204fd592bd35b95b120
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2020-10-02 21:24:17 +0200

    Try to make Travis happy

--- test/cli/test-inline-suppress.py
+++ test/cli/test-inline-suppress.py
@@ -24,8 +24,3 @@ def test_unmatched_suppression_path_with_extra_stuf():
     ret, stdout, stderr = cppcheck(['--inline-suppr', '--enable=information', '--error-exitcode=1', './proj-inline-suppress/2.c'])
     assert ret == 1
     assert 'Unmatched suppression: some_warning_id' in stderr
-
-def test_backwards_compatibility():
-    ret, stdout, stderr = cppcheck(['--inline-suppr', '--enable=unusedFunction', 'proj-inline-suppress/3.cpp'])
-    assert ret == 0
-    assert stderr == ''
