9fed45f9de9633ab1161abb83d1321b3f40e1907
amai2012 <amai2012@users.noreply.github.com>
2020-10-15 16:17:26 +0200

    threadsafety shall not warn about const vars (in C++11)
    C++1 guarantees static initialization to be thread-safe: 6.7  Declaration statement
    
    Do addons have information about the  language standard being configured?

--- addons/threadsafety.py
+++ addons/threadsafety.py
@@ -19,10 +19,7 @@ def checkstatic(data):
                 type = 'object'
             else:
                 type = 'variable'
-            if var.isConst:
-                reportError(var.typeStartToken, 'warning', 'Local constant static ' + type + ' \'' + var.nameToken.str + '\', dangerous if it is initialized in parallel threads', 'threadsafety')
-            else:
-                reportError(var.typeStartToken, 'warning', 'Local static ' + type + ': ' + var.nameToken.str, 'threadsafety')
+            reportError(var.typeStartToken, 'warning', 'Local static ' + type + ': ' + var.nameToken.str, 'threadsafety')
 
 for arg in sys.argv[1:]:
     if arg.startswith('-'):
