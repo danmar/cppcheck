43c4a48d4832b283b60b2c2e3f4148316d85f8a9
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2020-11-15 12:46:12 +0100

    Fixed #9971 (missing system includes in addon/test test files)

--- addons/test/naming_test.c
+++ addons/test/naming_test.c
@@ -2,4 +2,4 @@
 // ~/cppcheck/cppcheck --dump naming_test.c && python ../naming.py --var='[a-z].*' --function='[a-z].*' naming_test.c.dump
 
 // Should not crash when there is no name
-void func(int number, bool);
+void func(int number, int);
