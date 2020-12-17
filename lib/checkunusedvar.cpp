14d0d328dea18607dc3c9aec3bec8c9226047ad7
miltolstoy <miltolstoy@gmail.com>
2020-12-17 20:23:05 +0200

    update search in pointersToGlobals

--- lib/checkunusedvar.cpp
+++ lib/checkunusedvar.cpp
@@ -1620,9 +1620,7 @@ bool CheckUnusedVar::isFunctionWithoutSideEffects(const Function& func, const To
                 return false;
             }
             // check if global variable is changed
-            if (bodyVariable->isGlobal() ||
-                (std::find(pointersToGlobals.begin(), pointersToGlobals.end(), bodyVariable) != pointersToGlobals.end()))
-            {
+            if (bodyVariable->isGlobal() || (pointersToGlobals.find(bodyVariable) != pointersToGlobals.end()) ) {
                 if (bodyVariable->isPointer() || bodyVariable->isArray()) {
                     return false; // TODO: Update astutils.cpp:1544 isVariableChanged() and remove this. Unhandled case: `*(global_arr + 1) = new_val`
                 }
