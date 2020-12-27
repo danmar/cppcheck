f1169bf2b43fc1beab0da21e430adaa0a7a9eb49
Ken-Patrick Lehrmann <kp.lehrmann+github@gmail.com>
2020-12-27 10:14:46 +0100

    10062: Fix FP due to assignment in reverseanalyzer (#2983)
    reverseanalyzer has a special process for assignments, and would not see
    a stop condition on a rhs of an assignment.

--- lib/reverseanalyzer.cpp
+++ lib/reverseanalyzer.cpp
@@ -177,7 +177,9 @@ struct ReverseTraversal {
                 }
                 if (!continueB)
                     break;
-                valueFlowGenericForward(assignTop->astOperand2(), analyzer, settings);
+                Analyzer::Action a = valueFlowGenericForward(assignTop->astOperand2(), analyzer, settings);
+                if (a.isModified())
+                    break;
                 tok = previousBeforeAstLeftmostLeaf(assignTop)->next();
                 continue;
             }
