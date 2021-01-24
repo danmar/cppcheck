cc1002fcd6862dd24ee70c4c21d8e74c724e69b7
shane <thatguyiknow5@hotmail.com>
2021-01-25 00:46:22 +0800

    Add generic assertEquals

--- test/testsuite.cpp
+++ test/testsuite.cpp
@@ -110,7 +110,7 @@ std::string TestFixture::getLocationStr(const char * const filename, const unsig
     return ret.str();
 }
 
-static std::string writestr(const std::string &str, bool gccStyle = false)
+std::string TestFixture::writestr(const std::string &str, bool gccStyle)
 {
     std::ostringstream ostr;
     if (gccStyle)
