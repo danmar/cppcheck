f17d5b719a3128791570e6109bdb8ec317c0afbe
Mathias Schmid <50880400+matzeschmid@users.noreply.github.com>
2021-01-29 14:54:19 +0100

    Fix library data platform type (#3084)

--- gui/test/cppchecklibrarydata/testcppchecklibrarydata.cpp
+++ gui/test/cppchecklibrarydata/testcppchecklibrarydata.cpp
@@ -39,6 +39,14 @@ void TestCppcheckLibraryData::unhandledElement()
     loadCfgFile(":/files/unhandled_element.cfg", fileLibraryData, result);
     QCOMPARE(result.isNull(), false);
     qDebug() << result;
+
+    loadCfgFile(":/files/platform_type_unhandled_element.cfg", fileLibraryData, result);
+    QCOMPARE(result.isNull(), false);
+    qDebug() << result;
+
+    loadCfgFile(":/files/memory_resource_unhandled_element.cfg", fileLibraryData, result);
+    QCOMPARE(result.isNull(), false);
+    qDebug() << result;
 }
 
 void TestCppcheckLibraryData::mandatoryAttributeMissing()
@@ -134,11 +142,8 @@ void TestCppcheckLibraryData::typechecksValid()
     for (int idx=0; idx < libraryData.typeChecks.size(); idx++) {
         CppcheckLibraryData::TypeChecks lhs = libraryData.typeChecks[idx];
         CppcheckLibraryData::TypeChecks rhs = fileLibraryData.typeChecks[idx];
-        QCOMPARE(lhs.size(), lhs.size());
-        for (int num=0; num < lhs.size(); num++) {
-            QCOMPARE(lhs[num].first, rhs[num].first);
-            QCOMPARE(lhs[num].second, rhs[num].second);
-        }
+        QCOMPARE(lhs.size(), rhs.size());
+        QCOMPARE(lhs, rhs);
     }
 }
 
@@ -171,8 +176,164 @@ void TestCppcheckLibraryData::smartPointerValid()
     // Verify no data got lost or modified
     QCOMPARE(libraryData.smartPointers.size(), fileLibraryData.smartPointers.size());
     QCOMPARE(libraryData.smartPointers.size(), 3);
-    for (int i=0; i < libraryData.smartPointers.size(); i++) {
-        QCOMPARE(libraryData.smartPointers[i], fileLibraryData.smartPointers[i]);
+    QCOMPARE(libraryData.smartPointers, fileLibraryData.smartPointers);
+}
+
+void TestCppcheckLibraryData::platformTypeValid()
+{
+    // Load library data from file
+    loadCfgFile(":/files/platform_type_valid.cfg", fileLibraryData, result);
+    QCOMPARE(result.isNull(), true);
+
+    // Swap libray data read from file to other object
+    libraryData.swap(fileLibraryData);
+
+    // Do size and content checks against swapped data.
+    QCOMPARE(libraryData.platformTypes.size(), 3);
+
+    QCOMPARE(libraryData.platformTypes[0].name, "platform");
+    QCOMPARE(libraryData.platformTypes[0].value, "with attribute and empty");
+    QCOMPARE(libraryData.platformTypes[0].types.size(), 0);
+    QCOMPARE(libraryData.platformTypes[0].platforms.size(), 2);
+    QCOMPARE(libraryData.platformTypes[0].platforms[0], "win64");
+    QCOMPARE(libraryData.platformTypes[0].platforms[1].isEmpty(), true);
+
+    QCOMPARE(libraryData.platformTypes[1].name, "types");
+    QCOMPARE(libraryData.platformTypes[1].value, "all");
+    QCOMPARE(libraryData.platformTypes[1].types.size(), 5);
+    QCOMPARE(libraryData.platformTypes[1].types,
+            QStringList({"unsigned", "long", "pointer", "const_ptr", "ptr_ptr"}));
+    QCOMPARE(libraryData.platformTypes[1].platforms.isEmpty(), true);
+
+    QCOMPARE(libraryData.platformTypes[2].name, "types and platform");
+    QCOMPARE(libraryData.platformTypes[2].value.isEmpty(), true);
+    QCOMPARE(libraryData.platformTypes[2].types.size(), 2);
+    QCOMPARE(libraryData.platformTypes[2].types, QStringList({"pointer", "ptr_ptr"}));
+    QCOMPARE(libraryData.platformTypes[2].platforms.size(), 1);
+    QCOMPARE(libraryData.platformTypes[2].platforms[0], "win32");
+
+    // Save library data to file
+    saveCfgFile(TempCfgFile, libraryData);
+
+    fileLibraryData.clear();
+    QCOMPARE(fileLibraryData.platformTypes.size(), 0);
+
+    // Reload library data from file
+    loadCfgFile(TempCfgFile, fileLibraryData, result, true);
+    QCOMPARE(result.isNull(), true);
+
+    // Verify no data got lost or modified
+    QCOMPARE(libraryData.platformTypes.size(), fileLibraryData.platformTypes.size());
+    QCOMPARE(libraryData.platformTypes.size(), 3);
+    for (int idx=0; idx < libraryData.platformTypes.size(); idx++) {
+        CppcheckLibraryData::PlatformType lhs = libraryData.platformTypes[idx];
+        CppcheckLibraryData::PlatformType rhs = fileLibraryData.platformTypes[idx];
+        QCOMPARE(lhs.name, rhs.name);
+        QCOMPARE(lhs.value, rhs.value);
+        QCOMPARE(lhs.types.size(), rhs.types.size());
+        QCOMPARE(lhs.types, rhs.types);
+        QCOMPARE(lhs.platforms.size(), rhs.platforms.size());
+        QCOMPARE(lhs.platforms, rhs.platforms);
+    }
+}
+
+void TestCppcheckLibraryData::memoryResourceValid()
+{
+    // Load library data from file
+    loadCfgFile(":/files/memory_resource_valid.cfg", fileLibraryData, result);
+    QCOMPARE(result.isNull(), true);
+
+    // Swap libray data read from file to other object
+    libraryData.swap(fileLibraryData);
+
+    // Do size and content checks against swapped data.
+    QCOMPARE(libraryData.memoryresource.size(), 2);
+    QCOMPARE(libraryData.memoryresource[0].type, "memory");
+    QCOMPARE(libraryData.memoryresource[0].alloc.size(), 4);
+    QCOMPARE(libraryData.memoryresource[0].dealloc.size(), 1);
+    QCOMPARE(libraryData.memoryresource[0].use.size(), 0);
+
+    QCOMPARE(libraryData.memoryresource[0].alloc[0].name, "malloc");
+    QCOMPARE(libraryData.memoryresource[0].alloc[0].bufferSize, "malloc");
+    QCOMPARE(libraryData.memoryresource[0].alloc[0].isRealloc, false);
+    QCOMPARE(libraryData.memoryresource[0].alloc[0].init, false);
+    QCOMPARE(libraryData.memoryresource[0].alloc[0].arg, -1);
+    QCOMPARE(libraryData.memoryresource[0].alloc[0].reallocArg, -1);
+
+    QCOMPARE(libraryData.memoryresource[0].alloc[1].name, "calloc");
+    QCOMPARE(libraryData.memoryresource[0].alloc[1].bufferSize, "calloc");
+    QCOMPARE(libraryData.memoryresource[0].alloc[1].isRealloc, false);
+    QCOMPARE(libraryData.memoryresource[0].alloc[1].init, true);
+    QCOMPARE(libraryData.memoryresource[0].alloc[1].arg, -1);
+    QCOMPARE(libraryData.memoryresource[0].alloc[1].reallocArg, -1);
+
+    QCOMPARE(libraryData.memoryresource[0].alloc[2].name, "realloc");
+    QCOMPARE(libraryData.memoryresource[0].alloc[2].bufferSize, "malloc:2");
+    QCOMPARE(libraryData.memoryresource[0].alloc[2].isRealloc, true);
+    QCOMPARE(libraryData.memoryresource[0].alloc[2].init, false);
+    QCOMPARE(libraryData.memoryresource[0].alloc[2].arg, -1);
+    QCOMPARE(libraryData.memoryresource[0].alloc[2].reallocArg, -1);
+
+    QCOMPARE(libraryData.memoryresource[0].alloc[3].name, "UuidToString");
+    QCOMPARE(libraryData.memoryresource[0].alloc[3].bufferSize.isEmpty(), true);
+    QCOMPARE(libraryData.memoryresource[0].alloc[3].isRealloc, false);
+    QCOMPARE(libraryData.memoryresource[0].alloc[3].init, false);
+    QCOMPARE(libraryData.memoryresource[0].alloc[3].arg, 2);
+    QCOMPARE(libraryData.memoryresource[0].alloc[3].reallocArg, -1);
+
+    QCOMPARE(libraryData.memoryresource[0].dealloc[0].name, "HeapFree");
+    QCOMPARE(libraryData.memoryresource[0].dealloc[0].arg, 3);
+
+    QCOMPARE(libraryData.memoryresource[1].type, "resource");
+    QCOMPARE(libraryData.memoryresource[1].alloc.size(), 1);
+    QCOMPARE(libraryData.memoryresource[1].dealloc.size(), 1);
+    QCOMPARE(libraryData.memoryresource[1].use.size(), 0);
+
+    QCOMPARE(libraryData.memoryresource[1].alloc[0].name, "_wfopen_s");
+    QCOMPARE(libraryData.memoryresource[1].alloc[0].bufferSize.isEmpty(), true);
+    QCOMPARE(libraryData.memoryresource[1].alloc[0].isRealloc, false);
+    QCOMPARE(libraryData.memoryresource[1].alloc[0].init, true);
+    QCOMPARE(libraryData.memoryresource[1].alloc[0].arg, 1);
+    QCOMPARE(libraryData.memoryresource[1].alloc[0].reallocArg, -1);
+
+    QCOMPARE(libraryData.memoryresource[1].dealloc[0].name, "fclose");
+    QCOMPARE(libraryData.memoryresource[1].dealloc[0].arg, -1);
+
+    // Save library data to file
+    saveCfgFile(TempCfgFile, libraryData);
+
+    fileLibraryData.clear();
+    QCOMPARE(fileLibraryData.memoryresource.size(), 0);
+
+    // Reload library data from file
+    loadCfgFile(TempCfgFile, fileLibraryData, result, true);
+    QCOMPARE(result.isNull(), true);
+
+    // Verify no data got lost or modified
+    QCOMPARE(libraryData.memoryresource.size(), fileLibraryData.memoryresource.size());
+    QCOMPARE(libraryData.memoryresource.size(), 2);
+
+    for (int idx=0; idx < libraryData.memoryresource.size(); idx++) {
+        CppcheckLibraryData::MemoryResource lhs = libraryData.memoryresource[idx];
+        CppcheckLibraryData::MemoryResource rhs = fileLibraryData.memoryresource[idx];
+
+        QCOMPARE(lhs.type, rhs.type);
+        QCOMPARE(lhs.alloc.size(), rhs.alloc.size());
+        QCOMPARE(lhs.dealloc.size(), rhs.dealloc.size());
+        QCOMPARE(lhs.use, rhs.use);
+
+        for (int num=0; num < lhs.alloc.size(); num++) {
+            QCOMPARE(lhs.alloc[num].name, rhs.alloc[num].name);
+            QCOMPARE(lhs.alloc[num].bufferSize, rhs.alloc[num].bufferSize);
+            QCOMPARE(lhs.alloc[num].isRealloc, rhs.alloc[num].isRealloc);
+            QCOMPARE(lhs.alloc[num].init, rhs.alloc[num].init);
+            QCOMPARE(lhs.alloc[num].arg, rhs.alloc[num].arg);
+            QCOMPARE(lhs.alloc[num].reallocArg, rhs.alloc[num].reallocArg);
+        }
+        for (int num=0; num < lhs.dealloc.size(); num++) {
+            QCOMPARE(lhs.dealloc[num].name, rhs.dealloc[num].name);
+            QCOMPARE(lhs.dealloc[num].arg, rhs.dealloc[num].arg);
+        }
     }
 }
 
