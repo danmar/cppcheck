e75d469aea1e9f0add42613050dfa4c05e3a0e68
Robert Reif <reif@FX6840>
2021-02-09 08:47:06 -0500

    fix #10172 (debug: Executable scope 'x' with unknown function.)

--- test/testsimplifyusing.cpp
+++ test/testsimplifyusing.cpp
@@ -80,6 +80,7 @@ private:
         TEST_CASE(simplifyUsing10054);
         TEST_CASE(simplifyUsing10136);
         TEST_CASE(simplifyUsing10171);
+        TEST_CASE(simplifyUsing10172);
     }
 
     std::string tok(const char code[], bool simplify = true, Settings::PlatformType type = Settings::Native, bool debugwarnings = true) {
@@ -1032,6 +1033,72 @@ private:
                            "void B :: f ( const std :: vector < unsigned char > & ) const { } "
                            "}";
         ASSERT_EQUALS(exp, tok(code, false));
+        ASSERT_EQUALS("", errout.str());
+    }
+
+    void simplifyUsing10172() {
+        {
+            const char code[] = "namespace ns {\n"
+                                "    class A {\n"
+                                "    public:\n"
+                                "        using h = std::function<void()>;\n"
+                                "    };\n"
+                                "    class B : public A {\n"
+                                "        void f(h);\n"
+                                "    };\n"
+                                "}\n"
+                                "namespace ns {\n"
+                                "    void B::f(h) { }\n"
+                                "}";
+            const char exp[] = "namespace ns { "
+                               "class A { "
+                               "public: "
+                               "} ; "
+                               "class B : public A { "
+                               "void f ( std :: function < void ( ) > ) ; "
+                               "} ; "
+                               "} "
+                               "namespace ns { "
+                               "void B :: f ( std :: function < void ( ) > ) { } "
+                               "}";
+            ASSERT_EQUALS(exp, tok(code, false));
+            ASSERT_EQUALS("", errout.str());
+        }
+        {
+            const char code[] = "namespace ns {\n"
+                                "namespace external {\n"
+                                "class A {\n"
+                                "public: \n"
+                                "  using h = std::function<void()>;\n"
+                                "};\n"
+                                "class B : public A {\n"
+                                "  void f(h);\n"
+                                "};\n"
+                                "}\n"
+                                "}\n"
+                                "namespace ns {\n"
+                                "namespace external {\n"
+                                "void B::f(h) {}\n"
+                                "}\n"
+                                "}";
+            const char exp[] = "namespace ns { "
+                               "namespace external { "
+                               "class A { "
+                               "public: "
+                               "} ; "
+                               "class B : public A { "
+                               "void f ( std :: function < void ( ) > ) ; "
+                               "} ; "
+                               "} "
+                               "} "
+                               "namespace ns { "
+                               "namespace external { "
+                               "void B :: f ( std :: function < void ( ) > ) { } "
+                               "} "
+                               "}";
+            ASSERT_EQUALS(exp, tok(code, false));
+            ASSERT_EQUALS("", errout.str());
+        }
     }
 };
 
