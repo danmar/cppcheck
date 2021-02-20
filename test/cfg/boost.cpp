99ff242ddec3e09cf833a0b570a0a608b9298b85
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2021-02-20 13:33:57 +0100

    astyle formatting
    [ci skip]

--- test/cfg/boost.cpp
+++ test/cfg/boost.cpp
@@ -84,11 +84,10 @@ void throwexception2(int * buf)
 
 void macros()
 {
-    #define DECL(z, n, text) text ## n = n;
+#define DECL(z, n, text) text ## n = n;
     BOOST_PP_REPEAT(5, DECL, int x)
 
-    BOOST_SCOPED_ENUM_DECLARE_BEGIN(future_errc)
-    {
+    BOOST_SCOPED_ENUM_DECLARE_BEGIN(future_errc) {
         no_state
     }
     BOOST_SCOPED_ENUM_DECLARE_END(future_errc)
