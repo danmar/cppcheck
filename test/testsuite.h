0c8a29a6068bf03a47c6865a89a8c182d1e989cd
shane <thatguyiknow5@hotmail.com>
2021-02-22 10:56:24 +0800

    code review

--- test/testsuite.h
+++ test/testsuite.h
@@ -42,8 +42,6 @@ private:
     std::string mTemplateLocation;
     std::string mTestname;
 
-    static std::string writestr(const std::string& str, bool gccStyle = false);
-
 protected:
     std::string testToRun;
     bool quiet_tests;
--- test/testvalueflow.cpp
+++ test/testvalueflow.cpp
@@ -3123,7 +3123,7 @@ private:
         values = tokenValues(code, ">");
         ASSERT_EQUALS(1, values.size());
         ASSERT(values.front().isPossible());
-        ASSERT_EQUALS(true, values.front().intvalue);
+        ASSERT_EQUALS(1, values.front().intvalue);
 
         code = "void foo() {\n"
                "    struct ISO_PVD_s pvd;\n"
