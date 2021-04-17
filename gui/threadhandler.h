3b3eb9cb9fa6687ac65288bf8b3b08f0ac165991
Paul <pfultz2@yahoo.com>
2021-04-17 17:24:30 -0500

    Make settings param const

--- gui/threadhandler.h
+++ gui/threadhandler.h
@@ -64,7 +64,7 @@ public:
     * @brief Load settings
     * @param settings QSettings to load settings from
     */
-    void loadSettings(QSettings &settings);
+    void loadSettings(const QSettings &settings);
 
     /**
     * @brief Save settings
