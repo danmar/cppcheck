bdbf577bd84604773578bc40931f9071b2ce8913
Paul <pfultz2@yahoo.com>
2021-06-03 12:16:14 -0500

    Use unix platform

--- test/testtype.cpp
+++ test/testtype.cpp
@@ -258,16 +258,18 @@ private:
     }
 
     void signConversion() {
+        Settings settings;
+        settings.platform(Settings::Unix64);
         check("x = -4 * (unsigned)y;");
         ASSERT_EQUALS("[test.cpp:1]: (warning) Expression '-4' has a negative value. That is converted to an unsigned value and used in an unsigned calculation.\n", errout.str());
 
         check("x = (unsigned)y * -4;");
         ASSERT_EQUALS("[test.cpp:1]: (warning) Expression '-4' has a negative value. That is converted to an unsigned value and used in an unsigned calculation.\n", errout.str());
 
-        check("unsigned long long dostuff(long long x) {\n" // x is signed
+        check("unsigned int dostuff(int x) {\n" // x is signed
               "  if (x==0) {}\n"
               "  return (x-1)*sizeof(int);\n"
-              "}");
+              "}", &settings);
         ASSERT_EQUALS("[test.cpp:2] -> [test.cpp:3]: (warning) Expression 'x-1' can have a negative value. That is converted to an unsigned value and used in an unsigned calculation.\n", errout.str());
 
         check("unsigned int f1(signed int x, unsigned int y) {" // x is signed
