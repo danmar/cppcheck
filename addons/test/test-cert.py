c27213703ac49dbc73345be68e721593e3c00584
Georgy Komarov <jubnzv@gmail.com>
2021-07-06 22:00:51 +0300

    addons: Fix arguments parser when running pytest
    The problem is if we call argparse functions through pytest, the addon
    will treat pytest options from sysv as its own options and will fail.
    
    For example, if we run a specific test with the following command:
    
    PYTHONPATH=addons python3 -m pytest addons/test/test-misra.py -k test_loadRuleTexts_structure
    
    The MISRA addon will fail because it considers '-k' as an argument for
    misra.py.
    
    This commit decouples parser and parser.get_args() logic from the
    implementation in addons.

--- addons/test/test-cert.py
+++ addons/test/test-cert.py
@@ -18,18 +18,20 @@ def test_arguments_regression():
     # Arguments with expected SystemExit
     args_exit = ["--non-exists", "--non-exists-param=42", "-h", "--help"]
 
-    from addons.cert import get_args
+    from addons.cert import get_args_parser
 
     for arg in args_exit:
         sys.argv.append(arg)
         with pytest.raises(SystemExit):
-            get_args()
+            parser = get_args_parser()
+            parser.parse_args()
         sys.argv.remove(arg)
 
     for arg in args_ok:
         sys.argv.append(arg)
         try:
-            get_args()
+            parser = get_args_parser()
+            parser.parse_args()
         except SystemExit:
             pytest.fail("Unexpected SystemExit with '%s'" % arg)
         sys.argv.remove(arg)
