c77235f12cd1bb4d965d9b3f018f8e93be3d3bd6
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2021-07-09 09:47:23 +0200

    misra; implement rule 5.8

--- addons/test/misra/misra-ctu-1-test.c
+++ addons/test/misra/misra-ctu-1-test.c
@@ -5,6 +5,8 @@
 
 MISRA_2_3_A  misra_2_3_a;
 
+x = MISRA_2_5_OK_1;
+
 // cppcheck-suppress misra-c2012-2.3
 // cppcheck-suppress misra-c2012-5.6
 typedef int MISRA_5_6_VIOLATION;
@@ -15,4 +17,9 @@ struct misra_5_7_violation_t {
 };
 static misra_5_7_violation_t misra_5_7_var;
 
-x = MISRA_2_5_OK_1;
+// cppcheck-suppress misra-c2012-5.8
+int misra_5_8_var;
+// cppcheck-suppress misra-c2012-5.8
+static void misra_5_8_f(void) {}
+
+
--- addons/test/misra/misra-ctu-2-test.c
+++ addons/test/misra/misra-ctu-2-test.c
@@ -5,6 +5,8 @@
 
 MISRA_2_3_B  misra_2_3_b;
 
+x = MISRA_2_5_OK_2;
+
 // cppcheck-suppress misra-c2012-5.6
 typedef int MISRA_5_6_VIOLATION;
 static MISRA_5_6_VIOLATION misra_5_6_x;
@@ -15,4 +17,8 @@ struct misra_5_7_violation_t {
 };
 static misra_5_7_violation_t misra_5_7_var;
 
-x = MISRA_2_5_OK_2;
+// cppcheck-suppress misra-c2012-5.8
+static int misra_5_8_var;
+// cppcheck-suppress misra-c2012-5.8
+void misra_5_8_f(void) {}
+
