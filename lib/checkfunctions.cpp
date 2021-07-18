ae79b4759d9479d6ecabf8b3e36272ad9dc74852
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2021-07-18 19:31:19 +0200

    CI; Fix self check false positives

--- lib/checkfunctions.cpp
+++ lib/checkfunctions.cpp
@@ -273,7 +273,7 @@ void CheckFunctions::checkMissingReturn()
         }
         if (inconclusive && Token::simpleMatch(scope->bodyEnd->tokAt(-2), ") ; }")) {
             const Token *ftok = scope->bodyEnd->linkAt(-2)->previous();
-            if (mSettings->library.isNotLibraryFunction(ftok)) {
+            if (mSettings->library.isNotLibraryFunction(ftok) && !ftok->isKeyword()) {
                 const Token *tok = ftok;
                 while (Token::Match(tok->tokAt(-2), "%name% :: %name%"))
                     tok = tok->tokAt(-2);
