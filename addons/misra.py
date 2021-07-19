79570ae85334633b2fe463edb35d4b335de04ab4
escherstair <ernestviga@gmail.com>
2021-07-19 08:23:25 +0200

    misra: show violation even if no text is available

--- addons/misra.py
+++ addons/misra.py
@@ -3384,7 +3384,7 @@ class MisraChecker:
             elif len(self.ruleTexts) == 0:
                 errmsg = 'misra violation (use --rule-texts=<file> to get proper output)'
             else:
-                return
+                errmsg = f'misra violation {ruleNum} with no text in the supplied rule-texts-file'
 
             if self.severity:
                 cppcheck_severity = self.severity
