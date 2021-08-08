7c7ec3b74cd41fc4b76b406a26d2b281d719aa9c
Dávid Slivka <david.slivka.sk@gmail.com>
2021-08-08 15:11:21 +0200

    Optimalization of getting info from git blame

--- htmlreport/info_from_cmd.py
+++ htmlreport/info_from_cmd.py
@@ -5,11 +5,7 @@ import locale
 
 
 def git_blame(line, path, file, blame_options):
-    git_blame_dict = {
-        'author': 'Unknown',
-        'author_mail': '---',
-        'date': '---'
-    }
+    git_blame_dict = {}
     head, tail = os.path.split(file)
     if head != "":
         path = head
@@ -17,36 +13,35 @@ def git_blame(line, path, file, blame_options):
     try:
         os.chdir(path)
     except:
-        return git_blame_dict
+        return {'author': 'Unknown', 'author-mail': '---', 'author-time': '---'}
 
     try:
-        result = subprocess.check_output('git blame -L %d%s%s --porcelain -- %s' % (line, " -w" if "-w" in blame_options else "", " -M" if "-M" in blame_options else "", file))
+        result = subprocess.check_output('git blame -L %d%s%s --porcelain -- %s' % (
+            line, " -w" if "-w" in blame_options else "", " -M" if "-M" in blame_options else "", file))
         result = result.decode(locale.getpreferredencoding())
     except:
-        return git_blame_dict
+        return {'author': 'Unknown', 'author-mail': '---', 'author-time': '---'}
 
     if result.startswith('fatal'):
-        return git_blame_dict
+        return {'author': 'Unknown', 'author-mail': '---', 'author-time': '---'}
 
-    author = result[result.find('author') + 6:result.find('author-mail') - 1]
-    author_mail = result[result.find('author-mail') + 11:result.find('author-time') - 1]
-    commit_time = result[result.find('author-time') + 11:result.find('author-tz') - 1]
+    disallowed_characters = '<>'
+    for line in result.split('\n')[1:]:
+        space_pos = line.find(' ')
+        if space_pos > 30:
+            break
+        key = line[:space_pos]
+        val = line[space_pos + 1:]
 
-    disallowed_characters = '\ <>'
+        for character in disallowed_characters:
+            val = val.replace(character, "")
+        git_blame_dict[key] = val
 
-    for character in disallowed_characters:
-        author = author.replace(character, "")
-
-    for character in disallowed_characters:
-        author_mail = author_mail.replace(character, "")
-
-    datetime_object = datetime.date.fromtimestamp(int(commit_time))
+    datetime_object = datetime.date.fromtimestamp(float(git_blame_dict['author-time']))
     year = datetime_object.strftime("%Y")
     month = datetime_object.strftime("%m")
     day = datetime_object.strftime("%d")
 
-    git_blame_dict['author'] = author
-    git_blame_dict['author_mail'] = author_mail
-    git_blame_dict['date'] = '%s/%s/%s' % (day, month, year)
+    git_blame_dict['author-time'] = '%s/%s/%s' % (day, month, year)
 
     return git_blame_dict
