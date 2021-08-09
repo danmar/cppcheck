239b6cf361ac627eda98213abd5d60830aa393a9
Dávid Slivka <david.slivka.sk@gmail.com>
2021-08-09 14:11:44 +0200

    Changing tr creation

--- htmlreport/info_from_cmd.py
+++ htmlreport/info_from_cmd.py
@@ -16,7 +16,7 @@ def git_blame(line, path, file, blame_options):
         return {'author': 'Unknown', 'author-mail': '---', 'author-time': '---'}
 
     try:
-        result = subprocess.check_output('git blame -L %d%s%s --porcelain -- %s' % (
+        result = subprocess.check_output('git blame -L %d %s %s --porcelain -- %s' % (
             line, " -w" if "-w" in blame_options else "", " -M" if "-M" in blame_options else "", file))
         result = result.decode(locale.getpreferredencoding())
     except:
@@ -45,3 +45,28 @@ def git_blame(line, path, file, blame_options):
     git_blame_dict['author-time'] = '%s/%s/%s' % (day, month, year)
 
     return git_blame_dict
+
+
+def tr_str(tr_class, td_th, datahtml, line, id, cwe, severity, error_class, message, add_author=False, author=None, author_mail=None, date=None):
+    ret = ''
+    if datahtml:
+        ret += '<%s><a href="%s#line-%d">%d</a></%s>' % (td_th, datahtml, line, line, td_th)
+        for item in (id, cwe, severity):
+            ret += '<%s>%s</%s>' % (td_th, item, td_th)
+    else:
+        for item in (line, id, cwe, severity):
+            ret += '<%s>%s</%s>' % (td_th, item, td_th)
+
+    if error_class:
+        ret += '<%s %s>%s</%s>' % (td_th, error_class, message, td_th)
+    else:
+        ret += '<%s>%s</%s>' % (td_th, message, td_th)
+
+    if add_author:
+        for item in (author, author_mail, date):
+            ret += '<%s>%s</%s>' % (td_th, item, td_th)
+    if tr_class:
+        tr_attributes = ' class="%s"' % tr_class
+    else:
+        tr_attributes = ''
+    return '<tr%s>%s</tr>' % (tr_attributes, ret)
