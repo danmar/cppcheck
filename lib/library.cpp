db21afe4d1caecddd23f450d87ca497761f40317
chrchr <christian.riesch@live.com>
2021-09-08 21:29:39 +0200

    Consistently use operator |

--- lib/library.cpp
+++ lib/library.cpp
@@ -1257,11 +1257,11 @@ bool Library::isCompliantValidationExpression(const char* p)
         if (std::isdigit(*p))
             error |= (*(p + 1) == '-');
         else if (*p == ':') {
-            error |= range || (*(p + 1) == '.');
+            error |= range | (*(p + 1) == '.');
             range = true;
             has_dot = false;
             has_E = false;
-        } else if ((*p == '-') || (*p == '+'))
+        } else if ((*p == '-') | (*p == '+'))
             error |= (!std::isdigit(*(p + 1)));
         else if (*p == ',') {
             range = false;
@@ -1269,9 +1269,9 @@ bool Library::isCompliantValidationExpression(const char* p)
             has_dot = false;
             has_E = false;
         } else if (*p == '.') {
-            error |= has_dot || (!std::isdigit(*(p + 1)));
+            error |= has_dot | (!std::isdigit(*(p + 1)));
             has_dot = true;
-        } else if (*p == 'E' || *p == 'e') {
+        } else if ((*p == 'E') | (*p == 'e')) {
             error |= has_E;
             has_E = true;
         } else
