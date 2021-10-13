e18cea8358720f826c5cd9292fc1398a90b8c491
Pavel Simovec <psimovec@redhat.com>
2021-10-13 15:56:04 +0200

    add tests strcpy and strncpy memory overlap

--- test/testother.cpp
+++ test/testother.cpp
@@ -189,6 +189,8 @@ private:
         TEST_CASE(redundantInitialization);
         TEST_CASE(redundantMemWrite);
 
+        TEST_CASE(memOverlap);
+
         TEST_CASE(varFuncNullUB);
 
         TEST_CASE(checkPipeParameterSize); // ticket #3521
@@ -7872,6 +7874,22 @@ private:
               "    strcpy(buf, x);\n"
               "}");
         TODO_ASSERT_EQUALS("error", "", errout.str());
+    }
+
+    void memOverlap() {
+        check("void f()\n"
+              "{\n"
+              "    char str[] = \"test\";\n"
+              "    strcpy(str, str + 1);\n"
+              "}");
+        TODO_ASSERT_EQUALS("[test.cpp:4]: (error) Overlapping read/write in strcpy() is undefined behavior\n","", errout.str());
+
+        check("void f()\n"
+              "{\n"
+              "    char str[] = \"test\";\n"
+              "    strncpy(str, str + 1, 2);\n"
+              "}");
+        ASSERT_EQUALS("[test.cpp:4]: (error) Overlapping read/write in strncpy() is undefined behavior\n", errout.str());
     }
 
     void varFuncNullUB() { // #4482
