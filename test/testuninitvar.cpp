0b0d62fe7251079ffe06882b71059c2abda8e37f
Paul <pfultz2@yahoo.com>
2021-10-19 13:15:02 -0500

    Uncomment todo assert

--- test/testuninitvar.cpp
+++ test/testuninitvar.cpp
@@ -5387,9 +5387,9 @@ private:
                         "  foo(123, &abc);\n"
                         "  return abc.b;\n"
                         "}");
-        /* TODO ASSERT_EQUALS("[test.cpp:5]: (error) Uninitialized variable: abc.a\n"
+        TODO_ASSERT_EQUALS("[test.cpp:5]: (error) Uninitialized variable: abc.a\n"
                       "[test.cpp:5]: (error) Uninitialized variable: abc.b\n"
-                      "[test.cpp:5]: (error) Uninitialized variable: abc.c\n", errout.str()); */
+                      "[test.cpp:5]: (error) Uninitialized variable: abc.c\n", "", errout.str());
 
         valueFlowUninit("struct ABC { int a; int b; int c; };\n"
                         "void foo() {\n"
