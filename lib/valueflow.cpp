a309919ed3edd3f5186b222b0f4ec4ae390f353f
Paul <pfultz2@yahoo.com>
2021-10-31 10:01:40 -0500

    Use static array to store iterator infer models instead of constructing a vector everytime

--- lib/valueflow.cpp
+++ lib/valueflow.cpp
@@ -5538,8 +5538,8 @@ static void valueFlowInferCondition(TokenList* tokenlist,
             setTokenValue(tok, value, settings);
         } else if (Token::Match(tok, "%comp%|-") && tok->astOperand1() && tok->astOperand2()) {
             if (astIsIterator(tok->astOperand1()) || astIsIterator(tok->astOperand2())) {
-                for (ValuePtr<InferModel> model :
-                     std::vector<ValuePtr<InferModel>>{EndIteratorInferModel{}, StartIteratorInferModel{}}) {
+                static const std::array<ValuePtr<InferModel>, 2> iteratorModels = {EndIteratorInferModel{}, StartIteratorInferModel{}};
+                for (ValuePtr<InferModel> model : iteratorModels) {
                     std::vector<ValueFlow::Value> result =
                         infer(model, tok->str(), tok->astOperand1()->values(), tok->astOperand2()->values());
                     for (ValueFlow::Value value : result) {
