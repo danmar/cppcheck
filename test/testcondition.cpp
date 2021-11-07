7d65b2a0d8fc8807ca2cd9d9ea79d723b7d30fd5
Paul <pfultz2@yahoo.com>
2021-11-06 22:09:27 -0500

    Fix 10582: ValueFlow; wrong known value

--- test/testcondition.cpp
+++ test/testcondition.cpp
@@ -3800,6 +3800,15 @@ private:
               "    do {} while (i++ == 0);\n"
               "}\n");
         ASSERT_EQUALS("", errout.str());
+
+        // #10582
+        check("static void fun(message_t *message) {\n"
+            "    if (message->length >= 1) {\n"
+            "        switch (data[0]) {}\n"
+            "    }\n"
+            "    uint8_t d0 = message->length > 0 ? data[0] : 0xff;\n"
+            "}\n");
+        ASSERT_EQUALS("", errout.str());
     }
 
     void alwaysTrueSymbolic()
