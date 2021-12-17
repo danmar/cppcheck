1ac72495f871497d9ed51e507a8a5fd86bf9fda1
chrchr <christian.riesch@live.com>
2021-12-17 11:02:12 +0100

    Add test case

--- test/testother.cpp
+++ test/testother.cpp
@@ -5615,7 +5615,6 @@ private:
               "void f() {\n"
               "    if (std::is_same_v<T, char> || std::is_same_v<T, unsigned char>) {}\n"
               "}\n");
-
         ASSERT_EQUALS("", errout.str());
 
         check("typedef long long int64_t;"
@@ -5623,7 +5622,13 @@ private:
               "void f() {\n"
               "    if (std::is_same_v<T, long> || std::is_same_v<T, int64_t>) {}\n"
               "}\n");
+        ASSERT_EQUALS("", errout.str());
 
+        checkP("#define int32_t int"
+              "template<typename T>\n"
+              "void f() {\n"
+              "    if (std::is_same_v<T, int> || std::is_same_v<T, int32_t>) {}\n"
+              "}\n");
         ASSERT_EQUALS("", errout.str());
     }
 
