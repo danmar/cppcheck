1121646fe48c05f060488fa6ee0665c9399271e4
Paul <pfultz2@yahoo.com>
2022-01-07 20:55:18 -0600

    Fix 10683: FP danglingTemporaryLifetime with pointer to vector

--- test/testautovariables.cpp
+++ test/testautovariables.cpp
@@ -3295,6 +3295,16 @@ private:
               "}\n");
         ASSERT_EQUALS("[test.cpp:9] -> [test.cpp:9] -> [test.cpp:10]: (error) Using iterator that is a temporary.\n",
                       errout.str());
+
+        check("void f(bool b) {\n"
+              "  std::vector<int> ints = g();\n"
+              "  auto *ptr = &ints;\n"
+              "  if (b)\n"
+              "    ptr = &ints;\n"
+              "  for (auto it = ptr->begin(); it != ptr->end(); ++it)\n"
+              "  {}\n"
+              "}\n");
+        ASSERT_EQUALS("", errout.str());
     }
 
     void danglingLifetimeBorrowedMembers()
