0411a2b32b19824f133ef2cd4f5e07b8b614405e
Paul <pfultz2@yahoo.com>
2022-01-11 21:01:18 -0600

    Format

--- lib/templatesimplifier.cpp
+++ lib/templatesimplifier.cpp
@@ -319,7 +319,7 @@ void TemplateSimplifier::checkComplicatedSyntaxErrorsInTemplates()
             // parse this statement and see if the '<' and '>' are matching
             unsigned int level = 0;
             for (const Token *tok2 = tok; tok2 && !Token::simpleMatch(tok2, ";"); tok2 = tok2->next()) {
-                if (Token::simpleMatch(tok2, "{") && 
+                if (Token::simpleMatch(tok2, "{") &&
                     (!Token::Match(tok2->previous(), ">|%type%") || Token::simpleMatch(tok2->link(), "} ;")))
                     break;
                 if (tok2->str() == "(")
