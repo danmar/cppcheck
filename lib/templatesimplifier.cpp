0c966ce73e428ffeb8281ccb515f1491d37e7884
Paul <pfultz2@yahoo.com>
2022-01-11 21:00:02 -0600

    Fix 10001: Syntax error on valid C++ code

--- lib/templatesimplifier.cpp
+++ lib/templatesimplifier.cpp
@@ -312,14 +312,15 @@ void TemplateSimplifier::checkComplicatedSyntaxErrorsInTemplates()
             break;
 
         // template variable or type..
-        if (Token::Match(tok, "%type% <")) {
+        if (Token::Match(tok, "%type% <") && !Token::simpleMatch(tok, "template")) {
             // these are used types..
             std::set<std::string> usedtypes;
 
             // parse this statement and see if the '<' and '>' are matching
             unsigned int level = 0;
             for (const Token *tok2 = tok; tok2 && !Token::simpleMatch(tok2, ";"); tok2 = tok2->next()) {
-                if (Token::simpleMatch(tok2, "{") && (!Token::Match(tok2->previous(), ">|%type%") || Token::simpleMatch(tok2->link(), "} ;")))
+                if (Token::simpleMatch(tok2, "{") && 
+                    (!Token::Match(tok2->previous(), ">|%type%") || Token::simpleMatch(tok2->link(), "} ;")))
                     break;
                 if (tok2->str() == "(")
                     tok2 = tok2->link();
