43fb66d89a9d33a9b42db27c662439d94abbd168
chrchr <christian.riesch@live.com>
2022-01-17 16:04:52 +0100

    Format

--- lib/symboldatabase.cpp
+++ lib/symboldatabase.cpp
@@ -2208,7 +2208,7 @@ Function::Function(const Tokenizer *mTokenizer,
         else
             type = Function::eConstructor;
 
-        isExplicit(tokenDef->strAt(-1) == "explicit" ||  tokenDef->strAt(-2) == "explicit");
+        isExplicit(tokenDef->strAt(-1) == "explicit" || tokenDef->strAt(-2) == "explicit");
     }
 
     const Token *tok1 = setFlags(tok, scope);
