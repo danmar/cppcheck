7f35a46e91738d05a67507872da7ecf2691a1edc
Paul <pfultz2@yahoo.com>
2022-01-20 22:25:33 -0600

    Format

--- test/teststl.cpp
+++ test/teststl.cpp
@@ -5471,11 +5471,11 @@ private:
         ASSERT_EQUALS("", errout.str());
 
         check("void f() {\n"
-                  "    std::vector<int> v;\n"
-                  "    auto& rv = v;\n"
-                  "    rv.push_back(42);\n"
-                  "    for (auto i : v) {}\n"
-                  "}\n",
+              "    std::vector<int> v;\n"
+              "    auto& rv = v;\n"
+              "    rv.push_back(42);\n"
+              "    for (auto i : v) {}\n"
+              "}\n",
               true);
         ASSERT_EQUALS("", errout.str());
     }
