1a949c00b0470eae0a405288310e2431d0d3ff46
Paul Fultz II <pfultz2@yahoo.com>
2022-01-25 23:28:13 -0600

    Fix 10723:  Assertion 'tok && tok->exprId() > 0 && "Missing expr id for symbolic value"' in valueFlowSmartPointer() (#3755)
    * Fix 10723:  Assertion 'tok && tok->exprId() > 0 && "Missing expr id for symbolic value"' in valueFlowSmartPointer()
    
    * Format

--- lib/symboldatabase.cpp
+++ lib/symboldatabase.cpp
@@ -717,6 +717,9 @@ void SymbolDatabase::createSymbolDatabaseFindAllScopes()
             // syntax error?
             if (!scope)
                 mTokenizer->syntaxError(tok);
+            // End of scope or list should be handled above
+            if (tok->str() == "}")
+                mTokenizer->syntaxError(tok);
         }
     }
 }
@@ -3046,11 +3049,9 @@ void SymbolDatabase::addNewFunction(Scope **scope, const Token **tok)
     // find start of function '{'
     bool foundInitList = false;
     while (tok1 && tok1->str() != "{" && tok1->str() != ";") {
-        if (tok1->link() && Token::Match(tok1, "(|<")) {
+        if (tok1->link() && Token::Match(tok1, "(|[|<")) {
             tok1 = tok1->link();
-        } else if (foundInitList &&
-                   Token::Match(tok1, "%name%|> {") &&
-                   Token::Match(tok1->linkAt(1), "} ,|{")) {
+        } else if (foundInitList && Token::Match(tok1, "%name%|> {") && Token::Match(tok1->linkAt(1), "} ,|{")) {
             tok1 = tok1->linkAt(1);
         } else {
             if (tok1->str() == ":")
