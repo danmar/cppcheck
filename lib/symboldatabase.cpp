f429245da2182fde262c4ccd1710f334f2cb7811
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2022-01-27 19:43:52 +0100

    Fix #8557 FP format string requires unsigned long (for sizeof(var)) (#3727)

--- lib/symboldatabase.cpp
+++ lib/symboldatabase.cpp
@@ -6553,8 +6553,10 @@ void SymbolDatabase::setValueTypeInTokenList(bool reportDebugWarnings, Token *to
             }
 
             else if (Token::simpleMatch(tok->previous(), "sizeof (")) {
-                // TODO: use specified size_t type
                 ValueType valuetype(ValueType::Sign::UNSIGNED, ValueType::Type::LONG, 0U);
+                if (mSettings->platformType == cppcheck::Platform::Win64)
+                    valuetype.type = ValueType::Type::LONGLONG;
+
                 valuetype.originalTypeName = "size_t";
                 setValueType(tok, valuetype);
 
