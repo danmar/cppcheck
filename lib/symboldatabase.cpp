8f96eac1ec266fc548fe5c5f5133fb5248381352
chrchr <christian.riesch@live.com>
2022-01-31 16:38:01 +0100

    knownConditionTrueFalse

--- lib/symboldatabase.cpp
+++ lib/symboldatabase.cpp
@@ -7102,7 +7102,7 @@ ValueType::MatchResult ValueType::matchParameter(const ValueType *call, const Va
     if (call->type != func->type) {
         if (call->type == ValueType::Type::VOID || func->type == ValueType::Type::VOID)
             return ValueType::MatchResult::FALLBACK1;
-        if (call->pointer > 0 && func->pointer > 0)
+        if (call->pointer > 0)
             return func->type == ValueType::UNKNOWN_TYPE ? ValueType::MatchResult::UNKNOWN : ValueType::MatchResult::NOMATCH;
         if (call->isIntegral() && func->isIntegral())
             return call->type < func->type ?
