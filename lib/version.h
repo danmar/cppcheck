631a9eefd263649134679d864099750aaf960f5c
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2022-02-05 12:17:54 +0100

    2.7: Set versions

--- lib/version.h
+++ lib/version.h
@@ -2,7 +2,7 @@
 // After a release the DEVMINOR is incremented. MAJOR=x MINOR=y, DEVMINOR=y+1
 
 #define CPPCHECK_MAJOR 2
-#define CPPCHECK_MINOR 6
+#define CPPCHECK_MINOR 7
 #define CPPCHECK_DEVMINOR 7
 
 #define STRINGIFY(x) STRING(x)
