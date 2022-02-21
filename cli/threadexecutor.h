a88cea1a4fed4cc79c78eb46aa54ebf8b1e1e5cd
firewave <firewave@users.noreply.github.com>
2022-02-21 21:44:13 +0100

    added CMake option USE_THREADS to be able to use threads instead of fork() for -j

--- cli/threadexecutor.h
+++ cli/threadexecutor.h
@@ -24,10 +24,14 @@
 #include <map>
 #include <string>
 
-#if ((defined(__GNUC__) || defined(__sun)) && !defined(__MINGW32__) && !defined(__CYGWIN__)) || defined(__CPPCHECK__)
+#if defined(_WIN32)
+#define THREADING_MODEL_THREAD
+#define STDCALL __stdcall
+#elif defined(USE_THREADS)
+#define THREADING_MODEL_THREAD
+#define STDCALL
+#elif ((defined(__GNUC__) || defined(__sun)) && !defined(__MINGW32__) && !defined(__CYGWIN__)) || defined(__CPPCHECK__)
 #define THREADING_MODEL_FORK
-#elif defined(_WIN32)
-#define THREADING_MODEL_WIN
 #else
 #error "No threading model defined"
 #endif
@@ -63,7 +67,6 @@ private:
     const std::map<std::string, std::size_t> &mFiles;
     Settings &mSettings;
     ErrorLogger &mErrorLogger;
-    unsigned int mFileCount;
     std::list<std::string> mErrorList;
 
     /** @brief Key is file name, and value is the content of the file */
@@ -92,10 +95,10 @@ private:
      */
     void reportInternalChildErr(const std::string &childname, const std::string &msg);
 
-#elif defined(THREADING_MODEL_WIN)
+#elif defined(THREADING_MODEL_THREAD)
 
     class LogWriter;
-    static unsigned __stdcall threadProc(LogWriter *threadExecutor);
+    static unsigned int STDCALL threadProc(LogWriter *threadExecutor);
 
 #endif
 
