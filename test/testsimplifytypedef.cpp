df300a801efaf61d6f3324e5a6cbcd0abf7235c7
chrchr <christian.riesch@live.com>
2022-03-01 12:16:25 +0100

    Format

--- test/testsimplifytypedef.cpp
+++ test/testsimplifytypedef.cpp
@@ -3028,16 +3028,16 @@ private:
             tok(code));
     }
 
-    void simplifyTypedef140() { // #10798      
+    void simplifyTypedef140() { // #10798
         {
-          const char code[] = "typedef void (*b)();\n"
-                              "enum class E { a, b, c };\n";
-          ASSERT_EQUALS("enum class E { a , b , c } ;", tok(code));
+            const char code[] = "typedef void (*b)();\n"
+                                "enum class E { a, b, c };\n";
+            ASSERT_EQUALS("enum class E { a , b , c } ;", tok(code));
         }
         {
-          const char code[] = "typedef int A;\n"
-                              "enum class E { A };\n";
-          ASSERT_EQUALS("enum class E { A } ;", tok(code));
+            const char code[] = "typedef int A;\n"
+                                "enum class E { A };\n";
+            ASSERT_EQUALS("enum class E { A } ;", tok(code));
         }
 
     }
