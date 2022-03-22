6953fef1f7b77de291131d62b182641963e1ebf5
firewave <firewave@users.noreply.github.com>
2022-03-22 14:23:18 +0100

    some minor QRegularExpression usage optimizations and cleanups

--- gui/statsdialog.cpp
+++ gui/statsdialog.cpp
@@ -406,7 +406,7 @@ QLineSeries *StatsDialog::numberOfReports(const QString &fileName, const QString
         QTextStream in(&f);
         while (!in.atEnd()) {
             QString line = in.readLine();
-            const QRegularExpression rxdate("^\\[(\\d\\d)\\.(\\d\\d)\\.(\\d\\d\\d\\d)\\]$");
+            static const QRegularExpression rxdate("^\\[(\\d\\d)\\.(\\d\\d)\\.(\\d\\d\\d\\d)\\]$");
             const QRegularExpressionMatch matchRes = rxdate.match(line);
             if (matchRes.hasMatch()) {
                 int y = matchRes.captured(3).toInt();
