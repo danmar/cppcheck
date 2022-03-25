372098867762bd3a5191485fa03fbf754aea30b6
chrchr-github <christian.riesch@live.com>
2022-03-26 00:15:05 +0100

    simpleMatch

--- lib/checkother.cpp
+++ lib/checkother.cpp
@@ -1755,7 +1755,7 @@ static bool isConstStatement(const Token *tok, bool cpp)
         return false;
     if (Token::Match(tok, "<<|>>") && !astIsIntegral(tok, false))
         return false;
-    if (tok->astTop() && Token::Match(tok->astTop()->astOperand1(), "delete"))
+    if (tok->astTop() && Token::simpleMatch(tok->astTop()->astOperand1(), "delete"))
         return false;
     if (Token::Match(tok, "!|~|%cop%") && (tok->astOperand1() || tok->astOperand2()))
         return true;
