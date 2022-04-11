b0700e664469a9eb3d8ffda71e21b3a0b86b45f7
chrchr <christian.riesch@live.com>
2022-04-11 17:41:46 +0200

    Undo

--- lib/checksizeof.cpp
+++ lib/checksizeof.cpp
@@ -388,7 +388,7 @@ void CheckSizeof::suspiciousSizeofCalculation()
                     else if (varTok && varTok->str() == "*") {
                         const Token* arrTok = lPar->astParent()->astOperand1();
                         arrTok = arrTok ? arrTok->astOperand2() : nullptr;
-                        while (Token::Match(arrTok, ".|["))
+                        while (Token::Match(arrTok, "."))
                             arrTok = arrTok->astOperand2();
                         var = arrTok ? arrTok->variable() : nullptr;
                         if (var && var->isPointer() && !var->isArray())
