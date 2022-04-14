251bf66603b7082f8ea7ef992998529b0356c7a0
Paul <pfultz2@yahoo.com>
2022-04-13 22:23:29 -0500

    Fix 10956: ValueFlow: Incorrect value when assigned to two variables

--- lib/valueflow.cpp
+++ lib/valueflow.cpp
@@ -2287,6 +2287,7 @@ struct ValueFlowAnalyzer : Analyzer {
             return v.isSymbolicValue() && currValue->equalValue(v);
         }))
             return false;
+        const bool isPoint = currValue->bound == ValueFlow::Value::Bound::Point && currValue->isIntValue();
         const bool exact = !currValue->isIntValue() || currValue->isImpossible();
         for (const ValueFlow::Value& v : tok->values()) {
             if (!v.isSymbolicValue())
@@ -2296,7 +2297,7 @@ struct ValueFlowAnalyzer : Analyzer {
             const bool toImpossible = v.isImpossible() && currValue->isKnown();
             if (!v.isKnown() && !toImpossible)
                 continue;
-            if (exact && v.intvalue != 0)
+            if (exact && v.intvalue != 0 && !isPoint)
                 continue;
             std::vector<MathLib::bigint> r;
             ValueFlow::Value::Bound bound = currValue->bound;
