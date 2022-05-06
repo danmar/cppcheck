7ee548c0f5ac72e77bbb3b1170fafe15d1b34f9e
chrchr-github <christian.riesch@live.com>
2022-05-06 22:12:58 +0200

    TODO -> ASSERT

--- test/testautovariables.cpp
+++ test/testautovariables.cpp
@@ -3388,9 +3388,8 @@ private:
               "    std::vector<int*> v{&i, &i};\n"
               "    return v;\n"
               "}");
-        TODO_ASSERT_EQUALS(
+        ASSERT_EQUALS(
             "[test.cpp:3] -> [test.cpp:3] -> [test.cpp:2] -> [test.cpp:4]: (error) Returning object that points to local variable 'i' that will be invalid when returning.\n",
-            "",
             errout.str());
 
         check("std::vector<int*> f() {\n"
