ec13f5fe5c27488f4b03caf153637ce1846d0c0b
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2022-06-06 22:22:52 +0200

    misra: When checking C++ code only activate certain misra c++ compliant rules. It will be supported to run the addon on C++ code.

--- addons/test/misra/misra-test.cpp
+++ addons/test/misra/misra-test.cpp
@@ -7,7 +7,7 @@ class C {
 
 class misra_21_1_C {
     public:
-       misra_21_1_C operator=(const misra_21_1_C &); // 8.2
+       misra_21_1_C operator=(const misra_21_1_C &);
 };
 
 class C2 {
@@ -20,6 +20,6 @@ C2::C2(void) : f(NULL) {}
 
 static void test_misra_21_1_crash(void)
 {
-    auto misra_21_1_C a, b; // 12.3
+    auto misra_21_1_C a, b;
     a = b;
 }
