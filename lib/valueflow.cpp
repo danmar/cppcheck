e3485ce2c4dcdc7b0f436edd2d29036920e2d39b
Paul <pfultz2@yahoo.com>
2022-06-15 21:20:49 -0500

    Fix ubsan error

--- lib/valueflow.cpp
+++ lib/valueflow.cpp
@@ -105,6 +105,7 @@
 #include <array>
 #include <cassert>
 #include <climits>
+#include <cmath>
 #include <cstdlib>
 #include <cstring>
 #include <exception>
@@ -918,9 +919,9 @@ static void setTokenValue(Token* tok,
                 const double floatValue1 = value1.isFloatValue() ? value1.floatValue : value1.intvalue;
                 const double floatValue2 = value2.isFloatValue() ? value2.floatValue : value2.intvalue;
                 const MathLib::bigint intValue1 =
-                    value1.isFloatValue() ? static_cast<MathLib::bigint>(value1.floatValue) : value1.intvalue;
+                    value1.isFloatValue() ? std::llround(value1.floatValue) : value1.intvalue;
                 const MathLib::bigint intValue2 =
-                    value2.isFloatValue() ? static_cast<MathLib::bigint>(value2.floatValue) : value2.intvalue;
+                    value2.isFloatValue() ? std::llround(value2.floatValue) : value2.intvalue;
                 if ((value1.isFloatValue() || value2.isFloatValue()) && Token::Match(parent, "&|^|%|<<|>>|==|!=|%or%"))
                     continue;
                 if (Token::Match(parent, "==|!=")) {
