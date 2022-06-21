190ed2a7d159832f64d425c55690fd1d52045a13
chrchr <christian.riesch@live.com>
2022-06-21 14:27:43 +0200

    Fix #11143 FP: unreadVariable (remove simplifyMathExpressions())

--- lib/tokenize.h
+++ lib/tokenize.h
@@ -432,11 +432,6 @@ public:
     void findComplicatedSyntaxErrorsInTemplates();
 
     /**
-     * Simplify e.g. 'sin(0)' into '0'
-     */
-    void simplifyMathExpressions();
-
-    /**
      * Modify strings in the token list by replacing hex and oct
      * values. E.g. "\x61" -> "a" and "\000" -> "\0"
      * @param source The string to be modified, e.g. "\x61"
