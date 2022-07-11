f9337b725b39c5e04e94ffe508a5467742e49868
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2022-07-11 22:58:08 +0200

    Fix nullptr deref (#4262)
    * Fix some FNs related to c_str()
    
    * Format, fix FP
    
    * Fix nullptr deref
    
    * Fix merge

--- test/teststl.cpp
+++ test/teststl.cpp
@@ -4043,6 +4043,13 @@ private:
               "    return a.c_str() + b;\n"
               "}\n");
         ASSERT_EQUALS("[test.cpp:2]: (performance) Concatenating the result of c_str() and a std::string is slow and redundant.\n", errout.str());
+
+        check("std::vector<double> v;\n" // don't crash
+              "int i;\n"
+              "void f() {\n"
+              "    const double* const QM_R__ buf(v.data() + i);\n"
+              "}\n");
+        ASSERT_EQUALS("", errout.str());
     }
 
     void uselessCalls() {
