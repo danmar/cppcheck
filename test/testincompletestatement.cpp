93ce6b66737eeef0de55c78b4945c7e21abf74a3
chrchr <christian.riesch@live.com>
2022-08-02 18:55:40 +0200

    Fix test

--- test/testincompletestatement.cpp
+++ test/testincompletestatement.cpp
@@ -670,13 +670,12 @@ private:
 
         check("void f() {\n" // #5475
               "    std::string(\"a\") + \"a\";\n"
-              "    return 0;\n"
               "}\n"
               "void f(std::string& a) {\n"
               "    a.erase(3) + \"suf\";\n"
               "}\n", /*inconclusive*/ true);
         ASSERT_EQUALS("[test.cpp:2]: (warning, inconclusive) Found suspicious operator '+', result is not used.\n"
-                      "[test.cpp:6]: (warning, inconclusive) Found suspicious operator '+', result is not used.\n",
+                      "[test.cpp:5]: (warning, inconclusive) Found suspicious operator '+', result is not used.\n",
                       errout.str());
     }
 
