69762283f0c26e59ce110d01434f9bd7dfe5668c
firewave <firewave@users.noreply.github.com>
2022-08-12 16:01:06 +0200

    donate_cpu_lib.py: enabled `--debug-warnings`

--- tools/donate_cpu_lib.py
+++ tools/donate_cpu_lib.py
@@ -403,6 +403,7 @@ def scan_package(cppcheck_path, source_path, jobs, libraries, capture_callstack=
 
     # Reference for GNU C: https://gcc.gnu.org/onlinedocs/cpp/Common-Predefined-Macros.html
     options = libs + ' --showtime=top5 --check-library --inconclusive --enable=style,information --inline-suppr --template=daca2'
+    options += ' --debug-warnings --suppress=autoNoType --suppress=valueFlowBailout --suppress=bailoutUninitVar --suppress=symbolDatabaseWarning --suppress=valueFlowBailoutIncompleteVar'
     options += ' -D__GNUC__ --platform=unix64'
     options_rp = options + ' -rp={}'.format(dir_to_scan)
     if __make_cmd == 'msbuild.exe':
