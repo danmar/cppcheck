a2cc066e15628e573b2795d624d47490c69a33b8
firewave <firewave@users.noreply.github.com>
2022-08-12 15:49:17 +0200

    donate_cpu_lib.py: be more verbose about failures in `compile_cppcheck()`

--- tools/donate_cpu_lib.py
+++ tools/donate_cpu_lib.py
@@ -179,7 +179,6 @@ def compile_cppcheck(cppcheck_path, jobs):
             build_env["_CL_"] = jobs.replace('j', 'MP', 1)
             # TODO: processes still exhaust all threads of the system
             subprocess.check_call([__make_cmd, '-t:cli', os.path.join(cppcheck_path, 'cppcheck.sln'), '/property:Configuration=Release;Platform=x64'], cwd=cppcheck_path, env=build_env)
-            subprocess.check_call([os.path.join(cppcheck_path, 'bin', 'cppcheck.exe'), '--version'], cwd=os.path.join(cppcheck_path, 'bin'))
         else:
             rdynamic = ''
             build_env = os.environ
@@ -190,9 +189,20 @@ def compile_cppcheck(cppcheck_path, jobs):
                 # TODO: MinGW is not detected by Makefile - so work around it for now
                 rdynamic = 'RDYNAMIC=-lshlwapi'
             subprocess.check_call([__make_cmd, jobs, 'MATCHCOMPILER=yes', 'CXXFLAGS=-O2 -g -w', rdynamic], cwd=cppcheck_path, env=build_env)
-            subprocess.check_call([os.path.join(cppcheck_path, 'cppcheck'), '--version'], cwd=cppcheck_path)
-    except:
+    except Exception as e:
+        print('Compilation failed: {}'.format(e))
         return False
+
+    try:
+        if __make_cmd == 'msbuild.exe':
+            subprocess.check_call([os.path.join(cppcheck_path, 'bin', 'cppcheck.exe'), '--version'], cwd=os.path.join(cppcheck_path, 'bin'))
+        else:
+            subprocess.check_call([os.path.join(cppcheck_path, 'cppcheck'), '--version'], cwd=cppcheck_path)
+    except Exception as e:
+        print('Running Cppcheck failed: {}'.format(e))
+        # TODO: remove binary
+        return False
+
     return True
 
 
