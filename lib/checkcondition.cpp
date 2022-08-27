b7c406fd4d05256f13ed3edc5c7a2d0288ffc7aa
chrchr-github <christian.riesch@live.com>
2022-08-28 00:22:02 +0200

    Fix #10426 FN (style) Condition 's.empty()' is always false

--- lib/checkcondition.cpp
+++ lib/checkcondition.cpp
@@ -1516,7 +1516,7 @@ void CheckCondition::alwaysTrueFalse()
                 tok->astParent() && Token::Match(tok->astTop()->astOperand1(), "if|while") && !tok->astTop()->astOperand1()->isConstexpr() &&
                 (Token::Match(tok->astParent(), "%oror%|&&") || Token::Match(tok->astParent()->astOperand1(), "if|while"));
             const bool constValExpr = tok->isNumber() && Token::Match(tok->astParent(),"%oror%|&&|?"); // just one number in boolean expression
-            const bool compExpr = Token::Match(tok, "%comp%|!"); // a compare expression
+            const bool compExpr = Token::Match(tok, "%comp%|!|("); // a compare expression
             const bool ternaryExpression = Token::simpleMatch(tok->astParent(), "?");
             const bool returnExpression = Token::simpleMatch(tok->astTop(), "return") && (tok->isComparisonOp() || Token::Match(tok, "&&|%oror%"));
 
