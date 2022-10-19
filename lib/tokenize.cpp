eafaef47cd8e4091bdc7cf3862c1e9bf5077b076
chrchr <christian.riesch@live.com>
2022-10-19 11:42:25 +0200

    Fix crash in arraySize()

--- lib/tokenize.cpp
+++ lib/tokenize.cpp
@@ -3160,7 +3160,7 @@ void Tokenizer::arraySize()
 {
     auto getStrTok = [](Token* tok, bool addLength, Token** endStmt) -> Token* {
         if (addLength) {
-            *endStmt = tok->tokAt(6);
+            *endStmt = tok->tokAt(5);
             return tok->tokAt(4);
         }
         if (Token::Match(tok, "%var% [ ] =")) {
