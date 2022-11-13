18ce7209df59cd0c54cd6889f4fed7bd66103021
chrchr-github <christian.riesch@live.com>
2022-11-13 21:11:27 +0100

    Fix crash on nullptr

--- test/testconstructors.cpp
+++ test/testconstructors.cpp
@@ -1440,12 +1440,21 @@ private:
                       "[test.cpp:9]: (warning) Member variable 'B::ca' is not assigned a value in 'B::operator='.\n",
                       errout.str());
 
-        check("class C : B {\n"
+        check("class C : B {\n" // don't crash
               "    virtual C& operator=(C& c);\n"
               "};\n"
               "class D : public D {\n"
               "    virtual C& operator=(C& c) { return C::operator=(c); };\n"
               "};\n");
+        ASSERT_EQUALS("", errout.str());
+
+        check("struct D : B { D& operator=(const D&); };\n" // don't crash
+              "struct E : D { E& operator=(const E& rhs); };\n"
+              "E& E::operator=(const E& rhs) {\n"
+              "    if (this != &rhs)\n"
+              "        D::operator=(rhs);\n"
+              "    return *this;\n"
+              "}\n");
         ASSERT_EQUALS("", errout.str());
 
     }
