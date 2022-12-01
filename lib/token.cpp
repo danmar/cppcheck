f749834d2152af16e08a6fe2f075650ebd205219
chrchr <christian.riesch@live.com>
2022-12-01 20:33:27 +0100

    Use utility function

--- lib/token.cpp
+++ lib/token.cpp
@@ -2264,9 +2264,8 @@ std::pair<const Token*, const Token*> Token::typeDecl(const Token * tok)
                 if (r.first)
                     return r;
             }
-            const Token* parent = var->nameToken()->astParent();
-            if (Token::simpleMatch(parent, ":") && astIsContainer(parent->astOperand2())) { // range-based for
-                const ValueType* vt = parent->astOperand2()->valueType();
+            if (astIsRangeBasedForDecl(var->nameToken()) && astIsContainer(var->nameToken()->astParent()->astOperand2())) { // range-based for
+                const ValueType* vt = var->nameToken()->astParent()->astOperand2()->valueType();
                 if (vt && vt->containerTypeToken)
                     return { vt->containerTypeToken, vt->containerTypeToken->linkAt(-1) };
             }
