ed0e6c9811ec06f047db0f62ccbfd43fbf8997c1
chrchr <christian.riesch@live.com>
2022-12-05 19:06:18 +0100

    Get type from iterator

--- test/testfunctions.cpp
+++ test/testfunctions.cpp
@@ -1916,6 +1916,18 @@ private:
               "}\n");
         ASSERT_EQUALS("", errout.str());
 
+        check("void f() {\n"
+              "    auto p = std::make_shared<std::vector<int>>();\n"
+              "    p->push_back(1);\n"
+              "}\n");
+        ASSERT_EQUALS("", errout.str());
+
+        check("void f(std::vector<std::vector<int>>& v) {\n"
+              "    auto it = v.begin();\n"
+              "    it->push_back(1);\n"
+              "}\n");
+        ASSERT_EQUALS("", errout.str());
+
         settings.severity = severity_old;
         settings.checkLibrary = false;
     }
