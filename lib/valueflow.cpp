c4a21d3a44f210320f89bcd78c9e863d0654c5da
Paul <pfultz2@yahoo.com>
2022-12-08 14:54:03 -0600

    Add semicolon

--- lib/valueflow.cpp
+++ lib/valueflow.cpp
@@ -5330,7 +5330,7 @@ static void valueFlowForwardConst(Token* start,
                                   int /*unused*/ = 0)
 {
     if (!precedes(start, end))
-        throw InternalError(var->nameToken(), "valueFlowForwardConst: start token does not precede the end token.")
+        throw InternalError(var->nameToken(), "valueFlowForwardConst: start token does not precede the end token.");
     for (Token* tok = start; tok != end; tok = tok->next()) {
         if (tok->varId() == var->declarationId()) {
             for (const ValueFlow::Value& value : values)
