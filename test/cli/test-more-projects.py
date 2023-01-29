1a1e66a3119f93ae5f5e9faeb7b6964e049be2db
firewave <firewave@users.noreply.github.com>
2023-01-29 20:37:07 +0100

    test/cli/test-more-projects.py: added more tests for platform / small cleanup

--- test/cli/test-more-projects.py
+++ test/cli/test-more-projects.py
@@ -39,22 +39,31 @@ def test_project_force_U(tmpdir):
     assert stderr == '[bug1.cpp:2]: (error) Division by zero.\n'
 
 
+def __write_cppcheck_project_file(tmpdir, platform=None):
+    project_file = os.path.join(tmpdir, 'Project.cppcheck')
+
+    if platform is not None:
+        platform = '<platform>{}</platform>'.format(platform)
+
+    with open(project_file, 'wt') as f:
+        f.write(
+"""<?xml version="1.0" encoding="UTF-8"?>
+<project version="1">
+    {}
+    <paths>
+        <dir name="."/>
+    </paths>
+</project>""".format(platform)
+        )
+
+    return project_file
+
+
 def test_project_custom_platform(tmpdir):
     """
     import cppcheck project that contains a custom platform file
     """
-    project_file = os.path.join(tmpdir, 'Project.cppcheck')
-
-    with open(project_file, 'wt') as f:
-        f.write("""
-                <?xml version="1.0" encoding="UTF-8"?>
-                <project version="1">
-                  <platform>p1.xml</platform>
-                  <paths>
-                    <dir name="."/>
-                  </paths>
-                </project>
-                """)
+    project_file = __write_cppcheck_project_file(tmpdir, 'p1.xml')
 
     with open(os.path.join(tmpdir, 'p1.xml'), 'wt') as f:
         f.write('<?xml version="1.0"?>\n<platform/>')
@@ -62,6 +71,52 @@ def test_project_custom_platform(tmpdir):
     with open(os.path.join(tmpdir, '1.c'), 'wt') as f:
         f.write("int x;")
 
-    ret, stdout, stderr = cppcheck(['--project=' + project_file, '--template=cppcheck1'])
+    ret, stdout, stderr = cppcheck(['--platform=native', '--project=' + project_file, '--template=cppcheck1', '-q'])
     assert ret == 0, stdout
+    assert stdout == ''
+    assert stderr == ''
+
+
+def test_project_empty_platform(tmpdir):
+    """
+    import cppcheck project that contains an empty platform type
+    """
+    project_file = __write_cppcheck_project_file(tmpdir, '')
+
+    with open(os.path.join(tmpdir, '1.c'), 'wt') as f:
+        f.write("int x;")
+
+    ret, stdout, stderr = cppcheck(['--platform=native', '--project=' + project_file, '--template=cppcheck1', '-q'])
+    assert ret == 0, stdout
+    assert stdout == ''
+    assert stderr == ''
+
+
+def test_project_unspecified_platform(tmpdir):
+    """
+    import cppcheck project that contains the deprecated platform type "Unspecified"
+    """
+    project_file = __write_cppcheck_project_file(tmpdir, 'Unspecified')
+
+    with open(os.path.join(tmpdir, '1.c'), 'wt') as f:
+        f.write("int x;")
+
+    ret, stdout, stderr = cppcheck(['--platform=native', '--project=' + project_file, '--template=cppcheck1', '-q'])
+    assert ret == 0, stdout
+    assert stdout == "cppcheck: 'Unspecified' is a deprecated platform type and will be removed in Cppcheck 2.14. Please use 'unspecified' instead.\n"
+    assert stderr == ''
+
+
+def test_project_unknown_platform(tmpdir):
+    """
+    import cppcheck project that contains an unknown platform type
+    """
+    project_file = __write_cppcheck_project_file(tmpdir, 'dummy')
+
+    with open(os.path.join(tmpdir, '1.c'), 'wt') as f:
+        f.write("int x;")
+
+    ret, stdout, stderr = cppcheck(['--project=' + project_file, '--template=cppcheck1'])
+    assert ret == 1, stdout
+    assert stdout == "cppcheck: error: unrecognized platform: 'dummy'.\n"
     assert stderr == ''
