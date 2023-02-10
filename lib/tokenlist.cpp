17fb4eb82b18f001e40099a63c7061b1744c9600
chrchr <christian.riesch@live.com>
2023-02-10 10:41:21 +0100

    Revert "Simplify"
    This reverts commit 50e0a3a9b4969206e2a034c95821c7c6b6e91291.

--- lib/tokenlist.cpp
+++ lib/tokenlist.cpp
@@ -1768,8 +1768,8 @@ void TokenList::validateAst() const
             tok = tok->link();
             continue;
         }
-        if (Token::simpleMatch(tok, ". decltype (") && tok->linkAt(2)) { // skip trailing return type
-            tok = tok->linkAt(2);
+        if (Token::Match(tok, ". decltype (") && tok->tokAt(2)->link()) { // skip trailing return type
+            tok = tok->tokAt(2)->link();
             continue;
         }
 
