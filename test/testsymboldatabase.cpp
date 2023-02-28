958504600523d75ed71c534f50128588ad6450d0
chrchr-github <christian.riesch@live.com>
2023-02-28 23:50:34 +0100

    Add test

--- test/testsymboldatabase.cpp
+++ test/testsymboldatabase.cpp
@@ -8111,16 +8111,40 @@ private:
     }
 
     void valueType3() {
-        GET_SYMBOL_DB("void f(std::vector<std::unordered_map<int, std::unordered_set<int>>>& v, int i, int j) {\n"
-                      "    auto& s = v[i][j];\n"
-                      "    s.insert(0);\n"
-                      "}\n");
-        ASSERT_EQUALS("", errout.str());
+        {
+            GET_SYMBOL_DB("void f(std::vector<std::unordered_map<int, std::unordered_set<int>>>& v, int i, int j) {\n"
+                          "    auto& s = v[i][j];\n"
+                          "    s.insert(0);\n"
+                          "}\n");
+            ASSERT_EQUALS("", errout.str());
 
-        const Token* tok = tokenizer.tokens();
-        tok = Token::findsimplematch(tok, "s .");
-        ASSERT(tok && tok->valueType());
-        ASSERT_EQUALS("container(std :: set|unordered_set <)", tok->valueType()->str());
+            const Token* tok = tokenizer.tokens();
+            tok = Token::findsimplematch(tok, "s .");
+            ASSERT(tok && tok->valueType());
+            ASSERT_EQUALS("container(std :: set|unordered_set <)", tok->valueType()->str());
+        }
+        {
+            GET_SYMBOL_DB("void f(std::vector<int> v) {\n"
+                          "    auto it = std::find(v.begin(), v.end(), 0);\n"
+                          "}\n");
+            ASSERT_EQUALS("", errout.str());
+
+            const Token* tok = tokenizer.tokens();
+            tok = Token::findsimplematch(tok, "auto");
+            ASSERT(tok && tok->valueType());
+            ASSERT_EQUALS("iterator(std :: vector <)", tok->valueType()->str());
+        }
+        {
+            GET_SYMBOL_DB("void f(std::vector<int>::iterator beg, std::vector<int>::iterator end) {\n"
+                          "    auto it = std::find(beg, end, 0);\n"
+                          "}\n");
+            ASSERT_EQUALS("", errout.str());
+
+            const Token* tok = tokenizer.tokens();
+            tok = Token::findsimplematch(tok, "auto");
+            ASSERT(tok && tok->valueType());
+            ASSERT_EQUALS("iterator(std :: vector <)", tok->valueType()->str());
+        }
     }
 
     void valueTypeThis() {
