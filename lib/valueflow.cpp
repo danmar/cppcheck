d1356950ce8dd09bb8a0de47a07898350485fdf0
firewave <firewave@users.noreply.github.com>
2023-02-28 15:59:04 +0100

    ValueFlow: avoid unnecessary `ValuePtr` and `Value` copies [skip ci]

--- lib/valueflow.cpp
+++ lib/valueflow.cpp
@@ -3362,14 +3362,14 @@ static void valueFlowConditionExpressions(const TokenList& tokenlist,
                             condTok2,
                             makeConditionValue(1, condTok2, /*assume*/ true, !isBool, settings),
                             settings); // don't set '1' for non-boolean expressions
-                        valueFlowGenericForward(startTok, startTok->link(), a1, tokenlist, errorLogger, settings);
+                        valueFlowGenericForward(startTok, startTok->link(), std::move(a1), tokenlist, errorLogger, settings);
                     }
 
                     auto a2 = makeOppositeExpressionAnalyzer(true,
                                                              condTok2,
                                                              makeConditionValue(0, condTok2, true, false, settings),
                                                              settings);
-                    valueFlowGenericForward(startTok, startTok->link(), a2, tokenlist, errorLogger, settings);
+                    valueFlowGenericForward(startTok, startTok->link(), std::move(a2), tokenlist, errorLogger, settings);
                 }
             }
 
@@ -3384,7 +3384,7 @@ static void valueFlowConditionExpressions(const TokenList& tokenlist,
                     auto a1 = makeSameExpressionAnalyzer(condTok2,
                                                          makeConditionValue(0, condTok2, false, false, settings),
                                                          settings);
-                    valueFlowGenericForward(startTok, startTok->link(), a1, tokenlist, errorLogger, settings);
+                    valueFlowGenericForward(startTok, startTok->link(), std::move(a1), tokenlist, errorLogger, settings);
 
                     if (is1) {
                         auto a2 =
@@ -3392,7 +3392,7 @@ static void valueFlowConditionExpressions(const TokenList& tokenlist,
                                                            condTok2,
                                                            makeConditionValue(isOp, condTok2, false, false, settings),
                                                            settings);
-                        valueFlowGenericForward(startTok, startTok->link(), a2, tokenlist, errorLogger, settings);
+                        valueFlowGenericForward(startTok, startTok->link(), std::move(a2), tokenlist, errorLogger, settings);
                     }
                 }
             }
@@ -3410,7 +3410,7 @@ static void valueFlowConditionExpressions(const TokenList& tokenlist,
                     auto a1 = makeSameExpressionAnalyzer(condTok2,
                                                          makeConditionValue(0, condTok2, false, false, settings),
                                                          settings);
-                    valueFlowGenericForward(startTok->link()->next(), scope2->bodyEnd, a1, tokenlist, errorLogger, settings);
+                    valueFlowGenericForward(startTok->link()->next(), scope2->bodyEnd, std::move(a1), tokenlist, errorLogger, settings);
 
                     if (is1) {
                         auto a2 = makeOppositeExpressionAnalyzer(true,
@@ -3419,7 +3419,7 @@ static void valueFlowConditionExpressions(const TokenList& tokenlist,
                                                                  settings);
                         valueFlowGenericForward(startTok->link()->next(),
                                                 scope2->bodyEnd,
-                                                a2,
+                                                std::move(a2),
                                                 tokenlist,
                                                 errorLogger,
                                                 settings);
@@ -5404,7 +5404,7 @@ static void valueFlowInjectParameter(const TokenList& tokenlist,
         auto a = makeMultiValueFlowAnalyzer(arg, settings);
         valueFlowGenericForward(const_cast<Token*>(functionScope->bodyStart),
                                 functionScope->bodyEnd,
-                                a,
+                                std::move(a),
                                 tokenlist,
                                 errorLogger,
                                 settings);
@@ -5932,7 +5932,7 @@ static void valueFlowUninit(TokenList& tokenlist, ErrorLogger& errorLogger, cons
                 }
                 auto partialReadsAnalyzer = std::make_shared<PartialReadContainer>();
                 auto analyzer = makeMemberExpressionAnalyzer(memVar.nameToken()->str(), tok, uninitValue, partialReadsAnalyzer, settings);
-                valueFlowGenericForward(start, tok->scope()->bodyEnd, analyzer, tokenlist, errorLogger, settings);
+                valueFlowGenericForward(start, tok->scope()->bodyEnd, std::move(analyzer), tokenlist, errorLogger, settings);
 
                 for (auto&& p : *partialReadsAnalyzer) {
                     Token* tok2 = p.first;
