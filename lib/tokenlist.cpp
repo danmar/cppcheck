322a1a5e8c4fc8988870136c26660845122f07bf
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2023-03-13 16:30:27 +0100

    Partial fix for #11378 internalAstError regressions (iscpp11init) (#4889)

--- lib/tokenlist.cpp
+++ lib/tokenlist.cpp
@@ -878,7 +878,7 @@ static void compilePrecedence2(Token *&tok, AST_state& state)
                 tok = tok->previous();
             return !Token::Match(tok, "new ::| %type%");
         }
-        return true;
+        return !findLambdaEndTokenWithoutAST(tok);
     };
 
     if (doCompileScope(tok))
@@ -1325,6 +1325,17 @@ const Token* isLambdaCaptureList(const Token * tok)
     if (!Token::simpleMatch(params->astOperand1(), "{"))
         return nullptr;
     return params->astOperand1();
+}
+
+const Token* findLambdaEndTokenWithoutAST(const Token* tok) {
+    if (!(Token::simpleMatch(tok, "[") && tok->link()))
+        return nullptr;
+    tok = tok->link()->next();
+    if (Token::simpleMatch(tok, "(") && tok->link())
+        tok = tok->link()->next();
+    if (!(Token::simpleMatch(tok, "{") && tok->link()))
+        return nullptr;
+    return tok->link()->next();
 }
 
 static Token * createAstAtToken(Token *tok, bool cpp);
