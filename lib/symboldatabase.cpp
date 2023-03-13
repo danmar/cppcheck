6dcc5a490f307c80cf8355359c5aece57c1fc8d9
chrchr <christian.riesch@live.com>
2023-03-13 12:30:54 +0100

    Partial fix for #11378 internalAstError regressions (iscpp11init)

--- lib/symboldatabase.cpp
+++ lib/symboldatabase.cpp
@@ -4128,17 +4128,6 @@ static const Type* findVariableTypeIncludingUsedNamespaces(const SymbolDatabase*
 
 //---------------------------------------------------------------------------
 
-static const Token* findLambdaEndTokenWithoutAST(const Token* tok) {
-    if (!(Token::simpleMatch(tok, "[") && tok->link()))
-        return nullptr;
-    tok = tok->link()->next();
-    if (Token::simpleMatch(tok, "(") && tok->link())
-        tok = tok->link()->next();
-    if (!(Token::simpleMatch(tok, "{") && tok->link()))
-        return nullptr;
-    return tok->link()->next();
-}
-
 void Function::addArguments(const SymbolDatabase *symbolDatabase, const Scope *scope)
 {
     // check for non-empty argument list "( ... )"
