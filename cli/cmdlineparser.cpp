147a0f9caf1b6d2285bcb5cb669f6d84c2e53a85
firewave <firewave@users.noreply.github.com>
2023-03-21 20:09:59 +0100

    cmdlineparser.cpp: added TODO

--- cli/cmdlineparser.cpp
+++ cli/cmdlineparser.cpp
@@ -287,6 +287,7 @@ bool CmdLineParser::parseFromArgs(int argc, const char* const argv[])
             }
 
             else if (std::strncmp(argv[i], "--cppcheck-build-dir=", 21) == 0) {
+                // TODO: bail out when the folder does not exist? will silently do nothing
                 mSettings.buildDir = Path::fromNativeSeparators(argv[i] + 21);
                 if (endsWith(mSettings.buildDir, '/'))
                     mSettings.buildDir.pop_back();
