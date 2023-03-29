ae943c52274654aaa330c5a753e2d9a8402ec2cf
chrchr <christian.riesch@live.com>
2023-03-29 10:51:30 +0200

    simpleMatch()

--- lib/tokenize.cpp
+++ lib/tokenize.cpp
@@ -8044,7 +8044,7 @@ void Tokenizer::findGarbageCode() const
             if (!isCPP() || mSettings->standards.cpp < Standards::CPP20 || !Token::Match(tok->previous(), "%name% : %num% ="))
                 syntaxError(tok, tok->next()->str() + " " + tok->strAt(2));
         }
-        else if (Token::Match(tok, ") return") && !Token::Match(tok->link()->previous(), "if|while|for ("))
+        else if (Token::simpleMatch(tok, ") return") && !Token::Match(tok->link()->previous(), "if|while|for ("))
             syntaxError(tok);
 
         if (tok->isControlFlowKeyword() && Token::Match(tok, "if|while|for|switch")) { // if|while|for|switch (EXPR) { ... }
