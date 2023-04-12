c3ce334fcc3f9f139e661d3453bbd460f8026f9a
Paul <pfultz2@yahoo.com>
2023-04-12 14:28:39 -0500

    Add constructor since aggregate constructor doesnt work on older compilers

--- lib/valueflow.cpp
+++ lib/valueflow.cpp
@@ -9049,6 +9049,15 @@ const ValueFlow::Value *ValueFlow::valueFlowConstantFoldAST(Token *expr, const S
 }
 
 struct ValueFlowState {
+    explicit ValueFlowState(TokenList* tokenlist = nullptr, SymbolDatabase* symboldatabase = nullptr, ErrorLogger* errorLogger = nullptr, const Settings* settings = nullptr)
+    : 
+    tokenlist(tokenlist),
+    symboldatabase(symboldatabase),
+    errorLogger(errorLogger),
+    settings(settings)
+    {}
+    
+
     TokenList* tokenlist = nullptr;
     SymbolDatabase* symboldatabase = nullptr;
     ErrorLogger* errorLogger = nullptr;
