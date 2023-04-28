565831997657c9a4a0fb8a0088cdcd52ff997c46
Oliver Stöneberg <firewave@users.noreply.github.com>
2023-04-28 12:30:01 +0200

    CppCheck: removed changing of `debugwarnings` for non-accepted files (#4965)

--- lib/cppcheck.cpp
+++ lib/cppcheck.cpp
@@ -625,10 +625,6 @@ unsigned int CppCheck::checkFile(const std::string& filename, const std::string 
 {
     mExitCode = 0;
 
-    // only show debug warnings for accepted C/C++ source files
-    if (!Path::acceptFile(filename))
-        mSettings.debugwarnings = false;
-
     if (Settings::terminated())
         return mExitCode;
 
