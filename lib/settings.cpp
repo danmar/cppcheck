527b6fbb451c06ced6f3221bb7905a2fdd9af1b2
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2023-05-07 18:28:01 +0200

    Revert "ValueFlow: Set max iterations to 2 in normal analysis (#4951)"
    This reverts commit 02a3d03a0591b06fed014f7b79e8ec5d732d0ddc.

--- lib/settings.cpp
+++ lib/settings.cpp
@@ -71,7 +71,7 @@ Settings::Settings()
     showtime(SHOWTIME_MODES::SHOWTIME_NONE),
     templateMaxTime(0),
     typedefMaxTime(0),
-    valueFlowMaxIterations(2),
+    valueFlowMaxIterations(4),
     verbose(false),
     xml(false),
     xml_version(2)
@@ -229,7 +229,6 @@ void Settings::setCheckLevelExhaustive()
 {
     // Checking can take a little while. ~ 10 times slower than normal analysis is OK.
     performanceValueFlowMaxIfCount = -1;
-    valueFlowMaxIterations = 4;
     performanceValueFlowMaxSubFunctionArgs = 256;
 }
 
@@ -238,5 +237,4 @@ void Settings::setCheckLevelNormal()
     // Checking should finish in reasonable time.
     performanceValueFlowMaxSubFunctionArgs = 8;
     performanceValueFlowMaxIfCount = 100;
-    valueFlowMaxIterations = 2;
 }
