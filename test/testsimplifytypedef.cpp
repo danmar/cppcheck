2b74a2084e78fe4eda9f602872040ff20c0152e9
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2023-05-12 20:39:08 +0200

    Fixed #11716 (simplifyTypedef: function with const should be handled better) (#5054)

--- test/testsimplifytypedef.cpp
+++ test/testsimplifytypedef.cpp
@@ -42,7 +42,7 @@ public:
 private:
     // If there are unused templates, keep those
     const Settings settings0 = settingsBuilder().severity(Severity::style).checkUnusedTemplates().build();
-    const Settings settings1 = settingsBuilder().checkUnusedTemplates().build();
+    const Settings settings1 = settingsBuilder().severity(Severity::portability).checkUnusedTemplates().build();
     const Settings settings2 = settingsBuilder().severity(Severity::style).checkUnusedTemplates().build();
 
     void run() override {
@@ -57,6 +57,9 @@ private:
         TEST_CASE(cstruct3);
         TEST_CASE(cstruct3);
         TEST_CASE(cstruct4);
+        TEST_CASE(cfunction1);
+        TEST_CASE(cfunction2);
+        TEST_CASE(cfunction3);
         TEST_CASE(cfp1);
         TEST_CASE(cfp2);
         TEST_CASE(cfp4);
@@ -380,6 +383,26 @@ private:
         const char code[] = "typedef struct s { int a; int b; } t;\n"
                             "struct t x{};";
         ASSERT_EQUALS("struct s { int a ; int b ; } ; struct s x { } ;", simplifyTypedefC(code));
+    }
+
+    void cfunction1() {
+        const char code[] = "typedef int callback(int);\n"
+                            "callback* cb;";
+        ASSERT_EQUALS("int ( * cb ) ( int ) ;", simplifyTypedefC(code));
+    }
+
+    void cfunction2() {
+        const char code[] = "typedef int callback(int);\n"
+                            "typedef callback* callbackPtr;\n"
+                            "callbackPtr cb;";
+        ASSERT_EQUALS("int ( * cb ) ( int ) ;", simplifyTypedefC(code));
+    }
+
+    void cfunction3() {
+        const char code[] = "typedef int f(int);\n"
+                            "typedef const f cf;\n";
+        simplifyTypedefC(code);
+        ASSERT_EQUALS("[file.c:2]: (portability) It is unspecified behavior to const qualify a function type.\n", errout.str());
     }
 
     void cfp1() {
@@ -3532,7 +3555,7 @@ private:
                                 "func7 f7;";
 
             // The expected result..
-            const char expected[] = "; C f1 ( ) ; "
+            const char expected[] = "C f1 ( ) ; "
                                     "C ( * f2 ) ( ) ; "
                                     "C ( & f3 ) ( ) ; "
                                     "C ( * f4 ) ( ) ; "
@@ -3561,7 +3584,7 @@ private:
 
             // The expected result..
             // C const -> const C
-            const char expected[] = "; const C f1 ( ) ; "
+            const char expected[] = "const C f1 ( ) ; "
                                     "const C ( * f2 ) ( ) ; "
                                     "const C ( & f3 ) ( ) ; "
                                     "const C ( * f4 ) ( ) ; "
@@ -3589,7 +3612,7 @@ private:
                                 "func7 f7;";
 
             // The expected result..
-            const char expected[] = "; const C f1 ( ) ; "
+            const char expected[] = "const C f1 ( ) ; "
                                     "const C ( * f2 ) ( ) ; "
                                     "const C ( & f3 ) ( ) ; "
                                     "const C ( * f4 ) ( ) ; "
@@ -3617,7 +3640,7 @@ private:
                                 "func7 f7;";
 
             // The expected result..
-            const char expected[] = "; C * f1 ( ) ; "
+            const char expected[] = "C * f1 ( ) ; "
                                     "C * ( * f2 ) ( ) ; "
                                     "C * ( & f3 ) ( ) ; "
                                     "C * ( * f4 ) ( ) ; "
@@ -3645,7 +3668,7 @@ private:
                                 "func7 f7;";
 
             // The expected result..
-            const char expected[] = "; const C * f1 ( ) ; "
+            const char expected[] = "const C * f1 ( ) ; "
                                     "const C * ( * f2 ) ( ) ; "
                                     "const C * ( & f3 ) ( ) ; "
                                     "const C * ( * f4 ) ( ) ; "
@@ -3674,7 +3697,7 @@ private:
 
             // The expected result..
             // C const -> const C
-            const char expected[] = "; const C * f1 ( ) ; "
+            const char expected[] = "const C * f1 ( ) ; "
                                     "const C * ( * f2 ) ( ) ; "
                                     "const C * ( & f3 ) ( ) ; "
                                     "const C * ( * f4 ) ( ) ; "
