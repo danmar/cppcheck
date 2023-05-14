1999bc68bfa014c3ecf9481171ae312a8e24bbc5
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2023-05-14 17:01:07 +0200

    Set values when assigning init list (#5057)
    * Assign values to pointers with C++11 init
    
    * Handle assigning empty init list
    
    * Fix #10596 FN uninitdata with value initialization
    
    * Fix test
    
    * Set values when assigning init list

--- test/testvalueflow.cpp
+++ test/testvalueflow.cpp
@@ -5094,6 +5094,22 @@ private:
         ASSERT_EQUALS(true, value.isKnown());
         ASSERT_EQUALS(1, value.intvalue);
 
+        code = "bool f() {\n"
+               "  int* p = { 0 };\n"
+               "  return p == nullptr;\n" // <- known value
+               "}";
+        value = valueOfTok(code, "==");
+        ASSERT_EQUALS(true, value.isKnown());
+        ASSERT_EQUALS(1, value.intvalue);
+
+        code = "bool f() {\n"
+               "  int i = { 1 };\n"
+               "  return i == 1;\n" // <- known value
+               "}";
+        value = valueOfTok(code, "==");
+        ASSERT_EQUALS(true, value.isKnown());
+        ASSERT_EQUALS(1, value.intvalue);
+
         // calculation with known result
         code = "int f(int x) { a = x & 0; }"; // <- & is 0
         value = valueOfTok(code, "&");
