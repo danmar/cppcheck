a0b9a7cf28e3b87192f502d7962f25975077f767
chrchr <christian.riesch@live.com>
2023-05-15 14:33:49 +0200

    Fix tests

--- lib/checkmemoryleak.cpp
+++ lib/checkmemoryleak.cpp
@@ -921,7 +921,7 @@ void CheckMemoryLeakStructMember::checkStructVariable(const Variable * const var
                 }
 
                 // Returning from function..
-                else if (tok3->str() == "return") {
+                else if (tok3->scope()->type != Scope::ScopeType::eLambda && tok3->str() == "return") {
                     // Returning from function without deallocating struct member?
                     if (!Token::Match(tok3, "return %varid% ;", structid) &&
                         !Token::Match(tok3, "return & %varid%", structid) &&
