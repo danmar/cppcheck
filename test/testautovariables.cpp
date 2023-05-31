238cf75f6fd96cde52723c29171c112468ccd13d
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2023-05-31 20:55:55 +0200

    Fix #11732 FN autoVariables with array and std::string (#5096)
    * Fix #11732 FP autoVariables when reassigning argv
    
    * Fix #11732 FN autoVariables with array and std::string
    
    * Add test
    
    * Format
    
    * Format

--- test/testautovariables.cpp
+++ test/testautovariables.cpp
@@ -524,6 +524,18 @@ private:
               "    argv[0] = &a[0];\n"
               "}\n");
         ASSERT_EQUALS("", errout.str());
+
+        check("void f(char* c[]) {\n"
+              "    char a[] = \"abc\";\n"
+              "    c[0] = a;\n"
+              "}\n"
+              "void g(char* c[]) {\n"
+              "    std::string a = \"abc\";\n"
+              "    c[0] = a.data();\n"
+              "}\n");
+        ASSERT_EQUALS("[test.cpp:3]: (error) Address of local auto-variable assigned to a function parameter.\n"
+                      "[test.cpp:7]: (error) Address of local auto-variable assigned to a function parameter.\n",
+                      errout.str());
     }
 
     void testautovar_normal() {
