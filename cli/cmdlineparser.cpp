586379ab201458c6294c43c7e7f88144169221d7
samopolacek <samopolacekdk@gmail.com>
2023-07-20 13:57:17 +0000

    unusedFunction disabled warning is not printed if --cppcheck-build-dir is used

--- cli/cmdlineparser.cpp
+++ cli/cmdlineparser.cpp
@@ -1031,7 +1031,7 @@ bool CmdLineParser::parseFromArgs(int argc, const char* const argv[])
     else if ((def || mSettings.preprocessOnly) && !maxconfigs)
         mSettings.maxConfigs = 1U;
 
-    if (mSettings.checks.isEnabled(Checks::unusedFunction) && mSettings.jobs > 1) {
+    if (mSettings.checks.isEnabled(Checks::unusedFunction) && mSettings.jobs > 1 && mSettings.buildDir.empty()) {
         printMessage("unusedFunction check can't be used with '-j' option. Disabling unusedFunction check.");
     }
 
