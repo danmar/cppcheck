98afe8ef31a24eb692b61a00d292e7f477ac0c11
chrchr <christian.riesch@live.com>
2023-08-11 15:47:31 +0200

    Fix #11804 FP uninitvar for array in struct in struct

--- test/testuninitvar.cpp
+++ test/testuninitvar.cpp
@@ -6139,6 +6139,27 @@ private:
                         "    int& r = *p;\n"
                         "}\n");
         ASSERT_EQUALS("", errout.str());
+
+        valueFlowUninit("void f(std::stringstream& ss) {\n" // #11805
+                        "    int x;\n"
+                        "    int* p = &x;\n"
+                        "    ss >> *p;\n"
+                        "}\n"
+                        "void g() {\n"
+                        "    int x;\n"
+                        "    int* p = &x;\n"
+                        "    int& r = *p;\n"
+                        "}\n");
+        ASSERT_EQUALS("", errout.str());
+
+        valueFlowUninit("struct S1 { char a[10]; };\n" // #11804
+                        "struct S2 { struct S1 s1; };\n"
+                        "void init(char* c);\n"
+                        "void f() {\n"
+                        "    struct S2 s2;\n"
+                        "    init(s2.s1.a);\n"
+                        "}\n");
+        ASSERT_EQUALS("", errout.str());
     }
 
     void valueFlowUninitBreak() { // Do not show duplicate warnings about the same uninitialized value
