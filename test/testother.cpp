ad09364ee321cbff939d52ea6ce377b638153eb2
Paul <pfultz2@yahoo.com>
2023-08-18 18:54:50 -0500

    Improve knownArgument to check arguments to any nary function

--- test/testother.cpp
+++ test/testother.cpp
@@ -10905,6 +10905,12 @@ private:
               "}");
         ASSERT_EQUALS("[test.cpp:3]: (style) Argument '(int)((x&0x01)>>7)' to function g is always 0. It does not matter what value 'x' has.\n", errout.str());
 
+        check("void g(int, int);\n"
+              "void f(int x) {\n"
+              "   g(x, (x & 0x01) >> 7);\n"
+              "}");
+        ASSERT_EQUALS("[test.cpp:3]: (style) Argument '(x&0x01)>>7' to function g is always 0. It does not matter what value 'x' has.\n", errout.str());
+
         check("void g(int);\n"
               "void f(int x) {\n"
               "    g(0);\n"
