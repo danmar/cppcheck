7a86c6c38dab57a58e5037fed805b4766e3f6cc6
chrchr-github <christian.riesch@live.com>
2023-08-18 23:13:39 +0200

    incorrectStringBooleanError: check char literal conversions, simplify

--- test/teststring.cpp
+++ test/teststring.cpp
@@ -735,7 +735,9 @@ private:
               "  if('\\0'){}\n"
               "  if(L'\\0'){}\n"
               "}");
-        ASSERT_EQUALS("", errout.str());
+        ASSERT_EQUALS("[test.cpp:2]: (warning) Conversion of char literal '\\0' to bool always evaluates to false.\n"
+                      "[test.cpp:3]: (warning) Conversion of char literal L'\\0' to bool always evaluates to false.\n",
+                      errout.str());
 
         check("void f() {\n"
               "  if('\\0' || cond){}\n"
@@ -743,6 +745,29 @@ private:
               "}");
         ASSERT_EQUALS("[test.cpp:2]: (warning) Conversion of char literal '\\0' to bool always evaluates to false.\n"
                       "[test.cpp:3]: (warning) Conversion of char literal L'\\0' to bool always evaluates to false.\n", errout.str());
+
+        check("void f(bool b);\n" // #9450
+              "void f(std::string s);\n"
+              "void g() {\n"
+              "    f(\"abc\");\n"
+              "}\n");
+        ASSERT_EQUALS("[test.cpp:4]: (warning) Conversion of string literal \"abc\" to bool always evaluates to true.\n", errout.str());
+
+        check("void g(bool);\n"
+              "    void f(std::map<std::string, std::vector<int>>&m) {\n"
+              "    if (m.count(\"abc\"))\n"
+              "        g(m[\"abc\"][0] ? true : false);\n"
+              "}\n");
+        ASSERT_EQUALS("", errout.str());
+
+        check("void g(bool b);\n"
+              "void f() {\n"
+              "    g('\\0');\n"
+              "    g('a');\n"
+              "}\n");
+        ASSERT_EQUALS("[test.cpp:3]: (warning) Conversion of char literal '\\0' to bool always evaluates to false.\n"
+                      "[test.cpp:4]: (warning) Conversion of char literal 'a' to bool always evaluates to true.\n",
+                      errout.str());
     }
 
     void deadStrcmp() {
