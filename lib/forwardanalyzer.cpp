b17bf58ee62ca152b23ad6587643b236702236cf
chrchr-github <christian.riesch@live.com>
2023-08-22 22:01:07 +0200

    Fix

--- lib/forwardanalyzer.cpp
+++ lib/forwardanalyzer.cpp
@@ -143,7 +143,7 @@ struct ForwardTraversal {
             // Traverse the parameters of the function before escaping
             traverseRecursive(tok->next()->astOperand2(), f, traverseUnknown);
             return Break(Analyzer::Terminate::Escape);
-        } else if (isUnevaluated(tok)) {
+        } else if (isUnevaluated(tok->previous())) {
             if (out)
                 *out = tok->link();
             return Progress::Skip;
@@ -835,7 +835,7 @@ struct ForwardTraversal {
             return false;
         if (Token::simpleMatch(tok->link(), ") {"))
             return false;
-        if (isUnevaluated(tok))
+        if (isUnevaluated(tok->previous()))
             return false;
         return Token::Match(tok->previous(), "%name%|)|]|>");
     }
