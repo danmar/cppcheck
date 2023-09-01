f7fc132f451d79e93dc59bd5b9d19c064552bc05
Paul <pfultz2@yahoo.com>
2023-09-01 16:49:49 -0500

    Fix 11898: FP knownEmptyContainer

--- test/testvalueflow.cpp
+++ test/testvalueflow.cpp
@@ -6643,6 +6643,17 @@ private:
 
         code = "int f() { auto a = std::array<int, 2>{}; return a[1]; }";
         ASSERT_EQUALS("values.size():0", isKnownContainerSizeValue(tokenValues(code, "a ["), 0));
+
+        code = "void g(std::vector<int>* w) {\n"
+                "  std::vector<int> &r = *w;\n"
+                "  r.push_back(0);\n"
+                "}\n"
+                "int f() {\n"
+                "  std::vector<int> v;\n"
+                "  g(&v);\n"
+                "  return v[0];\n"
+                "}\n";
+        ASSERT(!isKnownContainerSizeValue(tokenValues(code, "v ["), 0).empty());
     }
 
     void valueFlowContainerElement()
