f0061cf0ffd061c0d099656d2c77273a94b19feb
firewave <firewave@users.noreply.github.com>
2023-09-06 10:52:30 +0200

    moved test-related scripts from `tools` to `test/scripts`

--- test/scripts/extract_and_run_more_tests.sh
+++ test/scripts/extract_and_run_more_tests.sh
@@ -4,5 +4,5 @@ cd ~/cppcheck || exit 1
 rm -rf test1
 python tools/extracttests.py --code=test1 test/testleakautovar.cpp
 cd ~/cppcheck/test1 || exit 1
-~/cppcheck/tools/run_more_tests.sh
+~/cppcheck/test/scripts/run_more_tests.sh
 
