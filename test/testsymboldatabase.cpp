53ab6bd1e20ae89b035351ee33ec836c0542849e
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2023-09-12 19:20:14 +0200

    test2

--- test/testsymboldatabase.cpp
+++ test/testsymboldatabase.cpp
@@ -7376,22 +7376,43 @@ private:
     void findFunction51() {
         // Both A and B defines the method test but with different arguments.
         // The call to test in B should match the method in B. The match is close enough.
-        GET_SYMBOL_DB("class A {\n"
-                      "public:\n"
-                      "    void test(bool a = true);\n"
-                      "};\n"
-                      "\n"
-                      "class B : public A {\n"
-                      "public:\n"
-                      "  B(): b_obj(this) { b_obj->test(\"1\"); }\n"
-                      "  void test(const std::string& str_obj);\n"
-                      "  B* b_obj;\n"
-                      "};");
-        const Token* call = Token::findsimplematch(tokenizer.tokens(), "test ( \"1\" ) ;");
-        ASSERT(call);
-        ASSERT(call->function());
-        ASSERT(call->function()->tokenDef);
-        ASSERT_EQUALS(9, call->function()->tokenDef->linenr());
+        {
+            GET_SYMBOL_DB("class A {\n"
+                          "public:\n"
+                          "    void test(bool a = true);\n"
+                          "};\n"
+                          "\n"
+                          "class B : public A {\n"
+                          "public:\n"
+                          "  B(): b_obj(this) { b_obj->test(\"1\"); }\n"
+                          "  void test(const std::string& str_obj);\n"
+                          "  B* b_obj;\n"
+                          "};");
+            const Token* call = Token::findsimplematch(tokenizer.tokens(), "test ( \"1\" ) ;");
+            ASSERT(call);
+            ASSERT(call->function());
+            ASSERT(call->function()->tokenDef);
+            ASSERT_EQUALS(9, call->function()->tokenDef->linenr());
+        }
+        {
+            GET_SYMBOL_DB("struct STR { STR(const char * p); };\n"
+                          "class A {\n"
+                          "public:\n"
+                          "    void test(bool a = true);\n"
+                          "};\n"
+                          "\n"
+                          "class B : public A {\n"
+                          "public:\n"
+                          "  B(): b_obj(this) { b_obj->test(\"1\"); }\n"
+                          "  void test(const STR& str_obj);\n"
+                          "  B* b_obj;\n"
+                          "};");
+            const Token* call = Token::findsimplematch(tokenizer.tokens(), "test ( \"1\" ) ;");
+            ASSERT(call);
+            ASSERT(call->function());
+            ASSERT(call->function()->tokenDef);
+            ASSERT_EQUALS(10, call->function()->tokenDef->linenr());
+        }
     }
 
     void findFunctionContainer() {
