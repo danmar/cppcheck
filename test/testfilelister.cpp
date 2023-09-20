21e001cd1a1f2778ca23dd91228a8229970b9c93
Oliver Stöneberg <firewave@users.noreply.github.com>
2023-09-20 10:49:00 +0200

    refs #11993 - error out in `TestFileLister::recursiveAddFiles()` instead of looping infinitely when repository root directory is not found (#5462)
    Not a proper fix yet but at least it prevents the hang and gives some
    feedback.

--- test/testfilelister.cpp
+++ test/testfilelister.cpp
@@ -43,7 +43,11 @@ private:
     static std::string findBaseDir() {
         std::string basedir;
         while (!Path::isDirectory(Path::join(basedir, ".github"))) {
+            const std::string abspath = Path::getAbsoluteFilePath(basedir);
             basedir += "../";
+            // no more going up
+            if (Path::getAbsoluteFilePath(basedir) == abspath)
+                throw std::runtime_error("could not find repository root directory");
         }
         return basedir;
     }
