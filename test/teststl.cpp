736f861d754705cc6f667cb1ba4bf274c0ba1596
chrchr-github <christian.riesch@live.com>
2023-10-11 22:25:13 +0200

    Format

--- test/teststl.cpp
+++ test/teststl.cpp
@@ -5438,7 +5438,7 @@ private:
         ASSERT_EQUALS("[test.cpp:2]: (style) Consider using std::any_of algorithm instead of a raw loop.\n",
                       errout.str());
 
-        check("void f() {\n" // #12064 
+        check("void f() {\n" // #12064
               "    for (const auto& animal : { \"cat\", \"bat\", \"tiger\", \"rat\" })\n"
               "        if (std::strlen(animal) > 4)\n"
               "            throw 1;\n"
