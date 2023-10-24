89df134fed1393e600864f0548ab4d835cd0c24c
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2023-10-24 10:05:32 +0200

    Fix #12109 Crash in calculate.h (#5587)

--- test/testvalueflow.cpp
+++ test/testvalueflow.cpp
@@ -911,6 +911,8 @@ private:
         ASSERT(tokenValues(";-1>>10;",">>").empty());
         ASSERT(tokenValues(";10>>-1;",">>").empty());
         ASSERT(tokenValues(";10>>64;",">>").empty());
+        ASSERT(tokenValues(";((-1) * 9223372036854775807LL - 1) / (-1);", "/").empty()); // #12109
+        ASSERT_EQUALS(tokenValues(";((-1) * 9223372036854775807LL - 1) % (-1);", "%").size(), 1);
 
         code = "float f(const uint16_t& value) {\n"
                "    const uint16_t uVal = value; \n"
