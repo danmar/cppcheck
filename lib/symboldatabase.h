3e0aea414784de2779917e4e34c88da23fbf3bca
firewave <firewave@users.noreply.github.com>
2023-11-20 19:32:35 +0100

    fixed `functionStatic` selfcheck warning

--- lib/symboldatabase.h
+++ lib/symboldatabase.h
@@ -1438,7 +1438,7 @@ private:
     void debugSymbolDatabase() const;
 
     void addClassFunction(Scope **scope, const Token **tok, const Token *argStart);
-    Function *addGlobalFunctionDecl(Scope*& scope, const Token* tok, const Token *argStart, const Token* funcStart);
+    static Function *addGlobalFunctionDecl(Scope*& scope, const Token* tok, const Token *argStart, const Token* funcStart);
     Function *addGlobalFunction(Scope*& scope, const Token*& tok, const Token *argStart, const Token* funcStart);
     void addNewFunction(Scope **scope, const Token **tok);
     bool isFunction(const Token *tok, const Scope* outerScope, const Token **funcStart, const Token **argStart, const Token** declEnd) const;
