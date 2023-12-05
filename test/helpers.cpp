02f12a721b67484d74d5ddc338b3a7f37a02194e
firewave <firewave@users.noreply.github.com>
2023-12-05 19:50:21 +0100

    test/helpers.cpp: added checks to `ScopeFile` to indicate leftover files from previously aborted testrun

--- test/helpers.cpp
+++ test/helpers.cpp
@@ -51,6 +51,8 @@ ScopedFile::ScopedFile(std::string name, const std::string &content, std::string
     , mFullPath(Path::join(mPath, mName))
 {
     if (!mPath.empty() && mPath != Path::getCurrentPath()) {
+        if (Path::isDirectory(mPath))
+            throw std::runtime_error("ScopedFile(" + mFullPath + ") - directory already exists");
 #ifdef _WIN32
         if (!CreateDirectoryA(mPath.c_str(), nullptr))
             throw std::runtime_error("ScopedFile(" + mFullPath + ") - could not create directory");
@@ -59,6 +61,9 @@ ScopedFile::ScopedFile(std::string name, const std::string &content, std::string
             throw std::runtime_error("ScopedFile(" + mFullPath + ") - could not create directory");
 #endif
     }
+
+    if (Path::isFile(mFullPath))
+        throw std::runtime_error("ScopedFile(" + mFullPath + ") - file already exists");
 
     std::ofstream of(mFullPath);
     if (!of.is_open())
