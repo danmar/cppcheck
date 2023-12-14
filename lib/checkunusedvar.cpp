f90775c9ffe3b1aa7287af6e6141cd9a58d5140a
Oleksandr Labetskyi <o.labetskyi@beetroot.se>
2023-12-14 15:31:29 +0200

    #12260: clean up

--- lib/checkunusedvar.cpp
+++ lib/checkunusedvar.cpp
@@ -1630,10 +1630,6 @@ bool CheckUnusedVar::isRecordTypeWithoutSideEffects(const Type* type)
 
 bool CheckUnusedVar::isVariableWithoutSideEffects(const Variable& var)
 {
-    // TODO why pointer is an automatic true
-    //if (var.isPointer())
-    //    return true;
-
     const Type* variableType = var.type();
     if (variableType) {
         if (!isRecordTypeWithoutSideEffects(variableType))
