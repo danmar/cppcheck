3f2478fa67b977e6569c318d9e9ed38a2ce0f3aa
firewave <firewave@users.noreply.github.com>
2024-01-14 11:23:03 +0100

    test/cli/qml_test.py: added more tests and improved existing ones

--- test/cli/qml_test.py
+++ test/cli/qml_test.py
@@ -2,17 +2,18 @@
 # python3 -m pytest test-qml.py
 
 import os
-import pytest
 from testutils import cppcheck
 
 __script_dir = os.path.dirname(os.path.abspath(__file__))
 
+# there are unused functions. But fillSampleData is not unused because that is referenced from main.qml
 PROJECT_DIR = os.path.join(__script_dir, 'QML-Samples-TableView')
 
 
 def test_unused_functions():
     ret, stdout, stderr = cppcheck(['-q', '--template=simple', '--library=qt', '--enable=unusedFunction', PROJECT_DIR])
     # there are unused functions. But fillSampleData is not unused because that is referenced from main.qml
+    assert stdout.splitlines() == []
     assert stderr.splitlines() == [
         "{}/samplemodel.cpp:9:0: style: The function 'rowCount' is never used. [unusedFunction]".format(PROJECT_DIR),
         "{}/samplemodel.cpp:15:0: style: The function 'data' is never used. [unusedFunction]".format(PROJECT_DIR),
@@ -21,16 +22,41 @@ def test_unused_functions():
     assert ret == 0, stdout
 
 
-@pytest.mark.xfail
-def test_unused_functions_j(tmpdir):
+def test_unused_functions_j():
+    ret, stdout, stderr = cppcheck(['-q', '--template=simple', '--library=qt', '--enable=unusedFunction', '-j2', PROJECT_DIR])
+    assert stdout.splitlines() == [
+        "cppcheck: unusedFunction check can't be used with '-j' option. Disabling unusedFunction check."
+    ]
+    assert stderr.splitlines() == []
+    assert ret == 0, stdout # TODO: abil out on this
+
+
+# TODO: fillSampleData is not unused
+def test_unused_functions_builddir(tmpdir):
     build_dir = os.path.join(tmpdir, 'b1')
     os.mkdir(build_dir)
-    ret, stdout, stderr = cppcheck(['--template=simple', '--library=qt', '--enable=unusedFunction', '-j2', '--cppcheck-build-dir={}'.format(build_dir), PROJECT_DIR])
-    # there are unused functions. But fillSampleData is not unused because that is referenced from main.qml
+    ret, stdout, stderr = cppcheck(['-q', '--template=simple', '--library=qt', '--enable=unusedFunction', '--cppcheck-build-dir={}'.format(build_dir), PROJECT_DIR])
+    assert stdout.splitlines() == []
     assert stderr.splitlines() == [
-        "{}/samplemodel.cpp:9:0: style: The function 'rowCount' is never used. [unusedFunction]".format(PROJECT_DIR),
         "{}/samplemodel.cpp:15:0: style: The function 'data' is never used. [unusedFunction]".format(PROJECT_DIR),
-        "{}/samplemodel.cpp:38:0: style: The function 'roleNames' is never used. [unusedFunction]".format(PROJECT_DIR)
+        "{}/samplemodel.cpp:47:0: style: The function 'fillSampleData' is never used. [unusedFunction]".format(PROJECT_DIR),
+        "{}/samplemodel.cpp:38:0: style: The function 'roleNames' is never used. [unusedFunction]".format(PROJECT_DIR),
+        "{}/samplemodel.cpp:9:0: style: The function 'rowCount' is never used. [unusedFunction]".format(PROJECT_DIR),
+    ]
+    assert ret == 0, stdout
+
+
+# TODO: fillSampleData is not unused
+def test_unused_functions_builddir_j(tmpdir):
+    build_dir = os.path.join(tmpdir, 'b1')
+    os.mkdir(build_dir)
+    ret, stdout, stderr = cppcheck(['-q', '--template=simple', '--library=qt', '--enable=unusedFunction', '-j2', '--cppcheck-build-dir={}'.format(build_dir), PROJECT_DIR])
+    assert stdout.splitlines() == []
+    assert stderr.splitlines() == [
+        "{}/samplemodel.cpp:15:0: style: The function 'data' is never used. [unusedFunction]".format(PROJECT_DIR),
+        "{}/samplemodel.cpp:47:0: style: The function 'fillSampleData' is never used. [unusedFunction]".format(PROJECT_DIR),
+        "{}/samplemodel.cpp:38:0: style: The function 'roleNames' is never used. [unusedFunction]".format(PROJECT_DIR),
+        "{}/samplemodel.cpp:9:0: style: The function 'rowCount' is never used. [unusedFunction]".format(PROJECT_DIR),
     ]
     assert ret == 0, stdout
 
