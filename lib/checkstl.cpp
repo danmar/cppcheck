7f70c3902a278dc88ba5e5f477f4e6af23952311
chrchr <christian.riesch@live.com>
2024-01-25 12:00:39 +0100

    FP mismatchingContainerIterator with container holding iterators

--- lib/checkstl.cpp
+++ lib/checkstl.cpp
@@ -704,6 +704,11 @@ static bool isSameIteratorContainerExpression(const Token* tok1,
     if (kind == ValueFlow::Value::LifetimeKind::Address) {
         return isSameExpression(true, false, getAddressContainer(tok1), getAddressContainer(tok2), library, false, false);
     }
+    if (astContainerYield(tok2) == Library::Container::Yield::ITEM)
+        return true;
+    const Token* const parent = tok2->astParent();
+    if (Token::simpleMatch(parent, ".") && Token::simpleMatch(parent->astParent(), "(") && Token::simpleMatch(parent->astParent()->astParent(), "*"))
+        return true;
     return false;
 }
 
