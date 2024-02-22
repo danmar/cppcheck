7ebb7bfbd9b110d18860586957919143656709a8
Oliver Stöneberg <firewave@users.noreply.github.com>
2024-02-22 18:55:43 +0100

    fixed `test/cli/fuzz_test.py` not triggering all the crashes / minimized fuzz data / fixed another fuzzing crash / fixed fuzzing timeout test (#6018)
    Not all the crashes were triggered by the test as the input has to be
    treated as C++. Also fixed another crash which was triggered by the
    reduced `crash-9ef938bba7d752386e24f2438c73cec66f6b972b`.

--- test/cli/fuzz_test.py
+++ test/cli/fuzz_test.py
@@ -11,7 +11,7 @@ def test_fuzz_crash():
 
     fuzz_crash_dir = os.path.join(__script_dir, 'fuzz-crash')
     for f in os.listdir(fuzz_crash_dir):
-        ret, stdout, _ = cppcheck(['-q', '--enable=all', '--inconclusive', f], cwd=fuzz_crash_dir)
+        ret, stdout, _ = cppcheck(['-q', '--language=c++', '--enable=all', '--inconclusive', f], cwd=fuzz_crash_dir)
         if ret != 0:
             failures[f] = stdout
 
@@ -27,7 +27,7 @@ def test_fuzz_timeout():
         return
     for f in os.listdir(fuzz_timeout_dir):
         try:
-            ret, stdout, _ = cppcheck(['-q', '--enable=all', '--inconclusive', f], cwd=fuzz_timeout_dir, timeout=5)
+            ret, stdout, _ = cppcheck(['-q', '--language=c++', '--enable=all', '--inconclusive', f], cwd=fuzz_timeout_dir, timeout=5)
         except subprocess.TimeoutExpired:
             failures.append(f)
 
