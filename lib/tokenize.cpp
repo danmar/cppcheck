92cbc6c23ab02111816c7f8ade23c537a0ccfac0
firewave <firewave@users.noreply.github.com>
2024-03-11 11:16:26 +0100

    tokenize.cpp: mitigated `unusedFunction` selfcheck warning

--- lib/tokenize.cpp
+++ lib/tokenize.cpp
@@ -3440,6 +3440,7 @@ bool Tokenizer::simplifyTokens1(const std::string &configuration)
     return true;
 }
 
+// cppcheck-suppress unusedFunction - used in tests only
 bool Tokenizer::tokenize(std::istream &code,
                          const char FileName[],
                          const std::string &configuration)
