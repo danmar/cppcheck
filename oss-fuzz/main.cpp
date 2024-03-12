eb40380cc9d216607a33e885fb5067f2db192d4a
firewave <firewave@users.noreply.github.com>
2024-03-12 15:21:59 +0100

    oss-fuzz: run with `-q` and `--check-level=exhaustive` / added TODO about `std.cfg`

--- oss-fuzz/main.cpp
+++ oss-fuzz/main.cpp
@@ -48,8 +48,11 @@ static const FileWithDetails s_file("test.cpp");
 static void doCheck(const std::string& code)
 {
     CppCheck cppcheck(s_errorLogger, false, nullptr);
+    // TODO: load std.cfg when settings are no longer owned by CppCheck
+    cppcheck.settings().quiet = true;
     cppcheck.settings().addEnabled("all");
     cppcheck.settings().certainty.setEnabled(Certainty::inconclusive, true);
+    cppcheck.settings().checkLevel = Settings::CheckLevel::exhaustive;
     cppcheck.check(s_file, code);
 }
 
