bef87656cc5b901bf0af69af06866d08480efb4c
chrchr <christian.riesch@live.com>
2024-03-12 15:56:58 +0100

    syntaxError

--- lib/tokenize.cpp
+++ lib/tokenize.cpp
@@ -8401,6 +8401,9 @@ void Tokenizer::findGarbageCode() const
         else if (Token::Match(tok, "%assign% [") && Token::simpleMatch(tok->linkAt(1), "] ;"))
             syntaxError(tok, tok->str() + "[...];");
 
+        else if (Token::Match(tok, "[({<] %assign%"))
+            syntaxError(tok);
+
         // UNKNOWN_MACRO(return)
         if (tok->isKeyword() && Token::Match(tok, "throw|return )") && Token::Match(tok->linkAt(1)->previous(), "%name% ("))
             unknownMacroError(tok->linkAt(1)->previous());
