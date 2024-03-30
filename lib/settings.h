ef932b5e055d193b7d3795154290d3415df03078
firewave <firewave@users.noreply.github.com>
2024-03-30 08:16:14 +0100

    compile `--rule-file` pattern only once / extracted regular expressions code to separate file

--- lib/settings.h
+++ lib/settings.h
@@ -44,6 +44,10 @@
 #include <cstdio>
 #endif
 
+#ifdef HAVE_RULES
+#include "regex.h"
+#endif
+
 struct Suppressions;
 enum class SHOWTIME_MODES : std::uint8_t;
 namespace ValueFlow {
@@ -338,6 +342,7 @@ public:
         std::string id = "rule"; // default id
         std::string summary;
         Severity severity = Severity::style; // default severity
+        Regex regex;
     };
 
     /**
