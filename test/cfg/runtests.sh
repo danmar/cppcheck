b42714ad641bb16058eb34f5547326ebacc5997b
orbitcowboy <orbitcowboy@web.de>
2024-04-06 14:10:25 +0200

    Fixed some shellcheck warnings

--- test/cfg/runtests.sh
+++ test/cfg/runtests.sh
@@ -453,91 +453,91 @@ function check_file {
     case $f in
         boost.cpp)
             boost_fn
-            ${CPPCHECK} "${CPPCHECK_OPT[@]}" --library=$lib ${DIR}$f
+            "${CPPCHECK}"  "${CPPCHECK_OPT[@]}" --library="$lib" "${DIR}""$f"
             ;;
         bsd.c)
             bsd_fn
-            ${CPPCHECK} "${CPPCHECK_OPT[@]}" --library=$lib ${DIR}$f
+            "${CPPCHECK}"  "${CPPCHECK_OPT[@]}" --library="$lib" "${DIR}""$f"
             ;;
         cairo.c)
             cairo_fn
-            ${CPPCHECK} "${CPPCHECK_OPT[@]}" --library=$lib ${DIR}$f
+            "${CPPCHECK}"  "${CPPCHECK_OPT[@]}" --library="$lib" "${DIR}""$f"
             ;;
         cppunit.cpp)
             cppunit_fn
-            ${CPPCHECK} "${CPPCHECK_OPT[@]}" --library=$lib ${DIR}$f
+            "${CPPCHECK}"  "${CPPCHECK_OPT[@]}" --library="$lib" "${DIR}""$f"
             ;;
         gnu.c)
             gnu_fn
             # TODO: posix needs to specified first or it has a different mmap() config
             # TODO: get rid of posix dependency
-            ${CPPCHECK} "${CPPCHECK_OPT[@]}" --library=posix,$lib ${DIR}gnu.c
+            "${CPPCHECK}"  "${CPPCHECK_OPT[@]}" --library=posix,"$lib" "${DIR}"gnu.c
             ;;
         googletest.cpp)
             googletest_fn
-            ${CPPCHECK} "${CPPCHECK_OPT[@]}" --library=$lib ${DIR}$f
+            "${CPPCHECK}"  "${CPPCHECK_OPT[@]}" --library="$lib" "${DIR}""$f"
             ;;
         gtk.c)
             gtk_fn
-            ${CPPCHECK} "${CPPCHECK_OPT[@]}" --library=$lib ${DIR}$f
+            "${CPPCHECK}"  "${CPPCHECK_OPT[@]}" --library="$lib" "${DIR}""$f"
             ;;
         kde.cpp)
             # TODO: "kde-4config" is no longer commonly available in recent distros
             #kde_fn
-            ${CPPCHECK} "${CPPCHECK_OPT[@]}" --library=$lib ${DIR}$f
+            "${CPPCHECK}"  "${CPPCHECK_OPT[@]}" --library="$lib" "${DIR}""$f"
             ;;
         libcurl.c)
             libcurl_fn
-            ${CPPCHECK} "${CPPCHECK_OPT[@]}" --library=$lib ${DIR}$f
+            "${CPPCHECK}"  "${CPPCHECK_OPT[@]}" --library="$lib" "${DIR}""$f"
             ;;
         libsigc++.cpp)
             libsigcpp_fn
-            ${CPPCHECK} "${CPPCHECK_OPT[@]}" --library=$lib ${DIR}$f
+            "${CPPCHECK}"  "${CPPCHECK_OPT[@]}" --library="$lib" "${DIR}""$f"
             ;;
         lua.c)
             lua_fn
-            ${CPPCHECK} "${CPPCHECK_OPT[@]}" --library=$lib ${DIR}$f
+            "${CPPCHECK}"  "${CPPCHECK_OPT[@]}" --library="$lib" "${DIR}""$f"
             ;;
         mfc.cpp)
             mfc_fn
-            ${CPPCHECK} "${CPPCHECK_OPT[@]}" --platform=win64  --library=$lib ${DIR}$f
+            "${CPPCHECK}"  "${CPPCHECK_OPT[@]}" --platform=win64 --library="$lib" "${DIR}""$f"
             ;;
         opencv2.cpp)
             # TODO: "opencv.pc" is not commonly available in distros
             #opencv2_fn
-            ${CPPCHECK} "${CPPCHECK_OPT[@]}" --library=$lib ${DIR}$f
+            "${CPPCHECK}"  "${CPPCHECK_OPT[@]}" --library="$lib" "${DIR}""$f"
             ;;
         openmp.c)
             openmp_fn
-            ${CPPCHECK} "${CPPCHECK_OPT[@]}" --library=$lib ${DIR}$f
+            "${CPPCHECK}"  "${CPPCHECK_OPT[@]}" --library="$lib" "${DIR}""$f"
             ;;
         openssl.c)
             openssl_fn
-            ${CPPCHECK} "${CPPCHECK_OPT[@]}" --library=$lib ${DIR}$f
+            "${CPPCHECK}"  "${CPPCHECK_OPT[@]}" --library="$lib" "${DIR}""$f"
             ;;
         posix.c)
             posix_fn
-            ${CPPCHECK} "${CPPCHECK_OPT[@]}" --library=$lib ${DIR}$f
+            "${CPPCHECK}"  "${CPPCHECK_OPT[@]}" --library="$lib" "${DIR}""$f"
             ;;
         python.c)
             python_fn
-            ${CPPCHECK} "${CPPCHECK_OPT[@]}" --library=$lib ${DIR}$f
+            "${CPPCHECK}"  "${CPPCHECK_OPT[@]}" --library="$lib" "${DIR}""$f"
             ;;
         qt.cpp)
             qt_fn
-            ${CPPCHECK} "${CPPCHECK_OPT[@]}" --library=$lib ${DIR}$f
+            "${CPPCHECK}"  "${CPPCHECK_OPT[@]}" --library="$lib" "${DIR}""$f"
             ;;
         sqlite3.c)
             sqlite3_fn
-            ${CPPCHECK} "${CPPCHECK_OPT[@]}" --library=$lib ${DIR}$f
+            "${CPPCHECK}"  "${CPPCHECK_OPT[@]}" --library="$lib" "${DIR}""$f"
             ;;
         std.c)
             std_c_fn
-            ${CPPCHECK} "${CPPCHECK_OPT[@]}" ${DIR}$f
+            "${CPPCHECK}"  "${CPPCHECK_OPT[@]}" "${DIR}""$f"
             ;;
         std.cpp)
             std_cpp_fn
-            ${CPPCHECK} "${CPPCHECK_OPT[@]}" ${DIR}$f
+            "${CPPCHECK}"  "${CPPCHECK_OPT[@]}" "${DIR}""$f"
             ;;
         windows.cpp)
             windows_fn
