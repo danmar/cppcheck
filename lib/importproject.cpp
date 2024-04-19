77a7dcea7884b057c30e8ef78bc127ba9039f706
Felix Faber <felix.faber@areus.de>
2024-04-19 07:18:57 +0200

    skipping string constructor

--- lib/importproject.cpp
+++ lib/importproject.cpp
@@ -188,7 +188,7 @@ ImportProject::Type ImportProject::import(const std::string &filename, Settings 
     if (!mPath.empty() && !endsWith(mPath,'/'))
         mPath += '/';
     if (mPath.empty())
-        mPath = std::string("./");
+        mPath = "./";
 
     const std::vector<std::string> fileFilters =
         settings ? settings->fileFilters : std::vector<std::string>();
