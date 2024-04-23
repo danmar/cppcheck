2ef95159d3ba1f511e448f598ac4747d4cd52512
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2024-04-23 23:38:44 +0200

    htmlreport: cppcheck-htmlreport: Update setup.py to allow package installation

--- htmlreport/setup.py
+++ htmlreport/setup.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
-with open('README.txt') as f:
+with open("README.txt") as f:
     readme = f.read()
 
 setup(
-    name="cppcheck",
-    description='Python script to parse the XML (version 2) output of ' +
-                'cppcheck and generate an HTML report using Pygments for syntax ' +
-                'highlighting.',
+    name="cppcheck-htmlreport",
+    description=(
+        "Python script to parse the XML (version 2) output of "
+        "cppcheck and generate an HTML report using Pygments for syntax "
+        "highlighting."
+    ),
     long_description=readme,
-    author='Henrik Nilsson',
-    url='https://github.com/danmar/cppcheck',
-    license='GPL',
-    scripts=[
-        "cppcheck-htmlreport",
-    ],
-    install_requires=['Pygments']
+    author="Henrik Nilsson",
+    url="https://github.com/danmar/cppcheck",
+    license="GPL",
+    packages=find_packages(exclude=("tests", "docs")),
+    entry_points={
+        "console_scripts": [
+            "cppcheck-htmlreport = cppcheck_htmlreport:cppcheck_htmlreport.main",
+        ]
+    },
+    test_suite="tests",
+    install_requires=["Pygments"],
 )
