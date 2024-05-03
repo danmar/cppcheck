cdee2f4a91ba58406588ccdb0bfaa1584e4dc47b
firewave <firewave@users.noreply.github.com>
2024-05-03 17:47:34 +0200

    removed unused stream-related functions

--- lib/cppcheck.cpp
+++ lib/cppcheck.cpp
@@ -622,18 +622,6 @@ unsigned int CppCheck::checkBuffer(const FileWithDetails &file, const std::strin
     });
 }
 
-// cppcheck-suppress unusedFunction
-unsigned int CppCheck::checkStream(const FileWithDetails &file, const std::string &cfgname, std::istream& fileStream)
-{
-    return checkInternal(file, cfgname,
-                         [&file, &fileStream](TokenList& list) {
-        list.createTokens(fileStream, file.spath());
-    },
-                         [&file, &fileStream](std::vector<std::string>& files, simplecpp::OutputList* outputList) {
-        return simplecpp::TokenList{fileStream, files, file.spath(), outputList};
-    });
-}
-
 unsigned int CppCheck::checkFile(const FileWithDetails& file, const std::string &cfgname)
 {
     return checkInternal(file, cfgname,
