3af12cabb0093610787e5bd37c5d9f0bcfa3ec69
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2024-05-08 13:03:07 +0200

    Update token.cpp

--- lib/token.cpp
+++ lib/token.cpp
@@ -100,38 +100,6 @@ static const std::unordered_set<std::string> controlFlowKeywords = {
     "return"
 };
 
-// TODO: replace with Keywords::getX()?
-// Another list of keywords
-static const std::unordered_set<std::string> baseKeywords = {
-    "asm",
-    "auto",
-    "break",
-    "case",
-    "const",
-    "continue",
-    "default",
-    "do",
-    "else",
-    "enum",
-    "extern",
-    "for",
-    "goto",
-    "if",
-    "inline",
-    "register",
-    "restrict",
-    "return",
-    "sizeof",
-    "static",
-    "struct",
-    "switch",
-    "typedef",
-    "union",
-    "volatile",
-    "while",
-    "void"
-};
-
 void Token::update_property_info()
 {
     setFlag(fIsControlFlowKeyword, controlFlowKeywords.find(mStr) != controlFlowKeywords.end());
@@ -147,8 +115,6 @@ void Token::update_property_info()
             if (mImpl->mVarId)
                 tokType(eVariable);
             else if (mTokensFrontBack.list.isKeyword(mStr))
-                tokType(eKeyword);
-            else if (baseKeywords.count(mStr) > 0)
                 tokType(eKeyword);
             else if (mTokType != eVariable && mTokType != eFunction && mTokType != eType && mTokType != eKeyword)
                 tokType(eName);
