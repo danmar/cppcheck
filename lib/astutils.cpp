77de5bde81be8abf9cd601b3b541df9ed314dcf0
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2024-05-23 16:10:04 +0200

    Update astutils.cpp

--- lib/astutils.cpp
+++ lib/astutils.cpp
@@ -2297,7 +2297,7 @@ static T* getTokenArgumentFunctionImpl(T* tok, int& argn)
             parent = parent->astParent();
 
         // passing variable to subfunction?
-        if (Token::Match(parent, "[*[(,{.]") || Token::Match(parent, "%oror%|&&"))
+        if (Token::Match(parent, "[[(,{.]") || Token::Match(parent, "%oror%|&&") || (parent && parent->isUnaryOp("*")))
             ;
         else if (Token::simpleMatch(parent, ":")) {
             while (Token::Match(parent, "[?:]"))
