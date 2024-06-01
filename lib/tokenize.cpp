fe38733137cd23419dc33e2e34a19daeef3d79ee
chrchr-github <christian.riesch@live.com>
2024-06-01 17:50:27 +0200

    Fix #12801 fuzzing timeout/out-of-memory in Tokenizer::simplifyTypedefCpp()

--- lib/tokenize.cpp
+++ lib/tokenize.cpp
@@ -8745,6 +8745,13 @@ void Tokenizer::findGarbageCode() const
                 }
                 if (!tok2->next() || tok2->isControlFlowKeyword() || Token::Match(tok2, "typedef|static|."))
                     syntaxError(tok);
+                if (Token::Match(tok2, "%name% %name%") && tok2->str() == tok2->strAt(1)) {
+                    if (tok2->isStandardType() && tok2->str() == "long")
+                        continue;
+                    if (Token::Match(tok2->tokAt(-1), "enum|struct|union") || (isCPP() && Token::Match(tok2->tokAt(-1), "class|::")))
+                        continue;
+                    syntaxError(tok2);
+                }
             }
         }
         if (isCPP() && tok->str() == "namespace" && tok->tokAt(-1)) {
