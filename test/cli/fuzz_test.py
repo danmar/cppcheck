d9e14f44626ef504210b400797fc6a1477e1e437
Oliver Stöneberg <firewave@users.noreply.github.com>
2024-06-01 12:42:55 +0200

    test/cli/fuzz_test.py: added test for fuzzing crashes with C code (#6466)

--- test/cli/fuzz_test.py
+++ test/cli/fuzz_test.py
@@ -18,13 +18,25 @@ def test_fuzz_crash():
     assert failures == {}
 
 
+def test_fuzz_crash_c():
+    failures = {}
+
+    fuzz_crash_dir = os.path.join(__script_dir, 'fuzz-crash_c')
+    # TODO: remove check if we have test data
+    if not os.path.exists(fuzz_crash_dir):
+        return
+    for f in os.listdir(fuzz_crash_dir):
+        ret, stdout, _ = cppcheck(['-q', '--language=c', '--enable=all', '--inconclusive', f], cwd=fuzz_crash_dir)
+        if ret != 0:
+            failures[f] = stdout
+
+    assert failures == {}
+
+
 def test_fuzz_timeout():
     failures = []
 
     fuzz_timeout_dir = os.path.join(__script_dir, 'fuzz-timeout')
-    # TODO: remove check if we have test data
-    if not os.path.exists(fuzz_timeout_dir):
-        return
     for f in os.listdir(fuzz_timeout_dir):
         try:
             ret, stdout, _ = cppcheck(['-q', '--language=c++', '--enable=all', '--inconclusive', f], cwd=fuzz_timeout_dir, timeout=5)
