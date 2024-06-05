44a82d9e55d53603d73b83fc49ffa62350dad262
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2024-06-05 16:00:34 +0200

    Update testtokenize.cpp

--- test/testtokenize.cpp
+++ test/testtokenize.cpp
@@ -6633,6 +6633,7 @@ private:
         ASSERT_EQUALS("double&(4[", testAst("void f(double(&)[4]) {}"));
         ASSERT_EQUALS("voidu*", testAst("int* g ( void* (f) (void*), void* u);")); // #12475
         ASSERT_EQUALS("f::(", testAst("::f();")); // #12544
+        ASSERT_EQUALS("(( (, f ({ (= (. c x) 0))))", testAst("f(c, { .x = 0 });", AstStyle::Z3)); // #12806
     }
 
     void asttemplate() { // uninstantiated templates will have <,>,etc..
