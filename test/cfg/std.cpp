4e86debc5f2428cca792ba2b4bf924d91b350990
chrchr-github <christian.riesch@live.com>
2024-06-10 22:30:34 +0200

    Fix FP constParameterReference, bad heuristic

--- test/cfg/std.cpp
+++ test/cfg/std.cpp
@@ -553,7 +553,6 @@ void bufferAccessOutOfBounds_std_ofstream_write(std::ofstream &os, const char* s
     (void)os.write(s,n);
 }
 
-// cppcheck-suppress constParameterReference // TODO: FP
 void bufferAccessOutOfBounds_std_ifstream_get(std::ifstream& in, std::streambuf& sb)
 {
     char cBuf[10];
