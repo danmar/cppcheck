1a7cdb1284d0ef02bf2a5ac4be4dbf1f996ddf7a
firewave <firewave@users.noreply.github.com>
2024-07-13 11:22:24 +0200

    utils.cpp: simplified `splitString()`

--- lib/utils.cpp
+++ lib/utils.cpp
@@ -188,18 +188,16 @@ std::string replaceEscapeSequences(const std::string &source) {
 
 std::list<std::string> splitString(const std::string& str, char sep)
 {
-    if (std::strchr(str.c_str(), sep) == nullptr)
-        return {str};
-
     std::list<std::string> l;
-    std::string p(str);
-    for (;;) {
-        const std::string::size_type pos = p.find(sep);
-        if (pos == std::string::npos)
+
+    std::string::size_type pos1 = 0;
+    std::string::size_type pos2;
+    while (true) {
+        pos2 = str.find(sep, pos1);
+        l.push_back(str.substr(pos1, pos2 - pos1));
+        if (pos2 == std::string::npos)
             break;
-        l.push_back(p.substr(0,pos));
-        p = p.substr(pos+1);
+        pos1 = pos2 + 1;
     }
-    l.push_back(std::move(p));
     return l;
 }
