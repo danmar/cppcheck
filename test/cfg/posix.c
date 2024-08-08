0b1bff2e43c5e1224eb5bda6142a92c5ea45bbbe
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2024-08-08 19:45:45 +0200

    Update posix.c

--- test/cfg/posix.c
+++ test/cfg/posix.c
@@ -1057,6 +1057,15 @@ void memleak_getaddrinfo_if() // #12506
     }
 }
 
+void memleak_getaddrinfo_if2() // #12996
+{
+    struct addrinfo *addrs = NULL;
+    if (getaddrinfo("a", "b", NULL, &addrs)) {
+        return -1;
+    }
+    freeaddrinfo(addrs);
+}
+
 void memleak_mmap(int fd)
 {
     // cppcheck-suppress [unusedAllocatedMemory, unreadVariable, constVariablePointer]
