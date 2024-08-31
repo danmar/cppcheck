698e981f8618f0d8a0833781512c76a51534ca7e
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2024-08-31 18:55:38 +0200

    donate-cpu-server.py: Update OLD_VERSION to 2.15.0 [ci skip] (#6757)

--- tools/donate-cpu-server.py
+++ tools/donate-cpu-server.py
@@ -26,9 +26,9 @@ from urllib.parse import urlparse
 # Version scheme (MAJOR.MINOR.PATCH) should orientate on "Semantic Versioning" https://semver.org/
 # Every change in this script should result in increasing the version number accordingly (exceptions may be cosmetic
 # changes)
-SERVER_VERSION = "1.3.56"
+SERVER_VERSION = "1.3.57"
 
-OLD_VERSION = '2.14.0'
+OLD_VERSION = '2.15.0'
 
 HEAD_MARKER = 'head results:'
 INFO_MARKER = 'info messages:'
