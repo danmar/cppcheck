2ec2d2d76897947ae57bffedc7dce17a5b025099
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2024-09-04 10:53:56 +0200

    Update checkother.cpp

--- lib/checkother.cpp
+++ lib/checkother.cpp
@@ -1657,6 +1657,7 @@ void CheckOther::checkConstPointer()
         else if (astIsRangeBasedForDecl(tok))
             continue;
         if (deref != NONE) {
+            const Token* gparent = parent->astParent();
             while (Token::simpleMatch(gparent, "[") && parent->str() == gparent->str())
                 gparent = gparent->astParent();
             if (deref == MEMBER) {
