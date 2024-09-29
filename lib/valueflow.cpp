4226a8a5aec5393860316210f3a08f1b11933045
firewave <firewave@users.noreply.github.com>
2024-09-30 01:42:45 +0200

    valueflow.cpp: made some `SymbolicInferModel` members private

--- lib/valueflow.cpp
+++ lib/valueflow.cpp
@@ -3659,10 +3659,10 @@ static void valueFlowSymbolicOperators(const SymbolDatabase& symboldatabase, con
 }
 
 struct SymbolicInferModel : InferModel {
-    const Token* expr;
     explicit SymbolicInferModel(const Token* tok) : expr(tok) {
         assert(expr->exprId() != 0);
     }
+private:
     bool match(const ValueFlow::Value& value) const override
     {
         return value.isSymbolicValue() && value.tokvalue && value.tokvalue->exprId() == expr->exprId();
@@ -3675,6 +3675,7 @@ struct SymbolicInferModel : InferModel {
         result.setKnown();
         return result;
     }
+    const Token* expr;
 };
 
 static void valueFlowSymbolicInfer(const SymbolDatabase& symboldatabase, const Settings& settings)
