420e230201bb68cb825fe7ba61bd0d97dfac785b
firewave <firewave@users.noreply.github.com>
2024-10-17 01:19:22 +0200

    ValueFlow: extracted `valueFlowSubFunction()` into separate file

--- lib/vf_analyze.h
+++ lib/vf_analyze.h
@@ -38,6 +38,7 @@
 #include "vf_rightshift.h" // IWYU pragma: export
 #include "vf_sameexpressions.h" // IWYU pragma: export
 #include "vf_string.h" // IWYU pragma: export
+#include "vf_subfunction.h" // IWYU pragma: export
 #include "vf_switchvariable.h" // IWYU pragma: export
 #include "vf_symbolicinfer.h" // IWYU pragma: export
 #include "vf_symbolicoperators.h" // IWYU pragma: export
