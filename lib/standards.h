6ed2148f675f5f525e0edd28f20da57cdabf8ef8
firewave <firewave@users.noreply.github.com>
2024-10-16 13:29:20 +0200

    Standards: moved implementations into source file

--- lib/standards.h
+++ lib/standards.h
@@ -21,7 +21,7 @@
 #define standardsH
 //---------------------------------------------------------------------------
 
-#include "utils.h"
+#include "config.h"
 
 #include <cstdint>
 #include <string>
@@ -34,7 +34,7 @@
  * @brief This is just a container for standards settings.
  * This struct contains all possible standards that cppcheck recognize.
  */
-struct Standards {
+struct CPPCHECKLIB Standards {
     enum Language : std::uint8_t { None, C, CPP };
 
     /** C code standard */
@@ -49,113 +49,15 @@ struct Standards {
     /** --std value given on command line */
     std::string stdValueCPP;
 
-    bool setC(std::string str) {
-        if (str.empty())
-            return false;
-        const cstd_t c_new = getC(str);
-        const bool b = (str == getC(c_new));
-        if (b) {
-            c = c_new;
-            stdValueC = std::move(str);
-        }
-        return b;
-    }
-    std::string getC() const {
-        return getC(c);
-    }
-    static std::string getC(cstd_t c_std) {
-        switch (c_std) {
-        case C89:
-            return "c89";
-        case C99:
-            return "c99";
-        case C11:
-            return "c11";
-        case C17:
-            return "c17";
-        case C23:
-            return "c23";
-        }
-        return "";
-    }
-    static cstd_t getC(const std::string &std) {
-        if (std == "c89") {
-            return Standards::C89;
-        }
-        if (std == "c99") {
-            return Standards::C99;
-        }
-        if (std == "c11") {
-            return Standards::C11;
-        }
-        if (std == "c17") {
-            return Standards::C17;
-        }
-        if (std == "c23") {
-            return Standards::C23;
-        }
-        return Standards::CLatest;
-    }
-    bool setCPP(std::string str) {
-        if (str.empty())
-            return false;
-        const cppstd_t cpp_new = getCPP(str);
-        const bool b = (str == getCPP(cpp_new));
-        if (b) {
-            cpp = cpp_new;
-            stdValueCPP = std::move(str);
-        }
-        return b;
-    }
-    std::string getCPP() const {
-        return getCPP(cpp);
-    }
-    static std::string getCPP(cppstd_t std) {
-        switch (std) {
-        case CPP03:
-            return "c++03";
-        case CPP11:
-            return "c++11";
-        case CPP14:
-            return "c++14";
-        case CPP17:
-            return "c++17";
-        case CPP20:
-            return "c++20";
-        case CPP23:
-            return "c++23";
-        case CPP26:
-            return "c++26";
-        }
-        return "";
-    }
-    static cppstd_t getCPP(const std::string &std) {
-        if (std == "c++03") {
-            return Standards::CPP03;
-        }
-        if (std == "c++11") {
-            return Standards::CPP11;
-        }
-        if (std == "c++14") {
-            return Standards::CPP14;
-        }
-        if (std == "c++17") {
-            return Standards::CPP17;
-        }
-        if (std == "c++20") {
-            return Standards::CPP20;
-        }
-        if (std == "c++23") {
-            return Standards::CPP23;
-        }
-        if (std == "c++26") {
-            return Standards::CPP26;
-        }
-        return Standards::CPPLatest;
-    }
-    bool setStd(const std::string& str) {
-        return setC(str) || setCPP(str);
-    }
+    bool setC(std::string str);
+    std::string getC() const;
+    static std::string getC(cstd_t c_std);
+    static cstd_t getC(const std::string &std);
+    bool setCPP(std::string str);
+    std::string getCPP() const;
+    static std::string getCPP(cppstd_t std);
+    static cppstd_t getCPP(const std::string &std);
+    bool setStd(const std::string& str);
 };
 
 /// @}
