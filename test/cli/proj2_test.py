3b7ed5031aecaef2d76ba722c528022bec185b4b
Ludvig Gunne Lindström <ludviggunnelindstrom@gmail.com>
2024-11-18 13:44:29 +0100

    calculate percentage in test

--- test/cli/proj2_test.py
+++ test/cli/proj2_test.py
@@ -86,10 +86,12 @@ def test_absolute_path(tmp_path):
     assert stdout.find('Checking %s ...' % file2) >= 0
 
 def test_progress_threads():
+    size1 = os.path.getsize(os.path.join(__proj_dir, 'a', 'a.c'))
+    size2 = os.path.getsize(os.path.join(__proj_dir, 'b', 'b.c'))
     __create_compile_commands()
     ret, stdout, _ = cppcheck(['--project=' + os.path.join(__proj_dir, __COMPILE_COMMANDS_JSON), '-j2'], cwd=__script_dir)
     assert ret == 0, stdout
-    assert stdout.find('1/2 files checked 76% done') >= 0
+    assert stdout.find('1/2 files checked %d%% done' % (100 * size1 // (size1 + size2))) >= 0
     assert stdout.find('2/2 files checked 100% done') >= 0
 
 def test_gui_project_loads_compile_commands_1(tmp_path):
