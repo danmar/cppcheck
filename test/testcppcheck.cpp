1c0b5886c5ae13aea6fd242d8872d2d92d6e5763
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2024-12-08 20:21:36 +0000

    Fix #13402 (Clang import: Pass --include options to clang) (#7083)

--- test/testcppcheck.cpp
+++ test/testcppcheck.cpp
@@ -60,6 +60,7 @@ private:
         TEST_CASE(isPremiumCodingStandardId);
         TEST_CASE(getDumpFileContentsRawTokens);
         TEST_CASE(getDumpFileContentsLibrary);
+        TEST_CASE(getClangFlagsIncludeFile);
     }
 
     void getErrorMessages() const {
@@ -241,6 +242,13 @@ private:
         cppcheck.settings().libraries.emplace_back("posix.cfg");
         const std::string expected2 = "  <library lib=\"std.cfg\"/>\n  <library lib=\"posix.cfg\"/>\n";
         ASSERT_EQUALS(expected2, cppcheck.getLibraryDumpData());
+    }
+
+    void getClangFlagsIncludeFile() const {
+        ErrorLogger2 errorLogger;
+        CppCheck cppcheck(errorLogger, false, {});
+        cppcheck.settings().userIncludes.emplace_back("1.h");
+        ASSERT_EQUALS("-x c --include 1.h ", cppcheck.getClangFlags(Standards::Language::C));
     }
 
     // TODO: test suppressions
