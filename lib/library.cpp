8c2390e0196bee65257a2a7d88030496c1b878a7
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2024-12-11 16:33:29 +0100

    Fix #13409 Crash in valueFlowUnknownFunctionReturn() (#7095)

--- lib/library.cpp
+++ lib/library.cpp
@@ -1205,6 +1205,8 @@ const Library::AllocFunc* Library::getAllocFuncInfo(const Token *tok) const
 {
     while (Token::simpleMatch(tok, "::"))
         tok = tok->astOperand2() ? tok->astOperand2() : tok->astOperand1();
+    if (!tok)
+        return nullptr;
     const std::string funcname = getFunctionName(tok);
     return isNotLibraryFunction(tok) && mData->mFunctions.find(funcname) != mData->mFunctions.end() ? nullptr : getAllocDealloc(mData->mAlloc, funcname);
 }
@@ -1214,6 +1216,8 @@ const Library::AllocFunc* Library::getDeallocFuncInfo(const Token *tok) const
 {
     while (Token::simpleMatch(tok, "::"))
         tok = tok->astOperand2() ? tok->astOperand2() : tok->astOperand1();
+    if (!tok)
+        return nullptr;
     const std::string funcname = getFunctionName(tok);
     return isNotLibraryFunction(tok) && mData->mFunctions.find(funcname) != mData->mFunctions.end() ? nullptr : getAllocDealloc(mData->mDealloc, funcname);
 }
@@ -1223,6 +1227,8 @@ const Library::AllocFunc* Library::getReallocFuncInfo(const Token *tok) const
 {
     while (Token::simpleMatch(tok, "::"))
         tok = tok->astOperand2() ? tok->astOperand2() : tok->astOperand1();
+    if (!tok)
+        return nullptr;
     const std::string funcname = getFunctionName(tok);
     return isNotLibraryFunction(tok) && mData->mFunctions.find(funcname) != mData->mFunctions.end() ? nullptr : getAllocDealloc(mData->mRealloc, funcname);
 }
