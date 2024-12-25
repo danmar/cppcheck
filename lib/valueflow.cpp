0edd83e5f88c377fc7869ea5be1bef92cdb20191
firewave <firewave@users.noreply.github.com>
2024-12-25 16:28:19 +0100

    ValueFlow: avoid unnecessary copy in `getLifetimeObjValue()`

--- lib/valueflow.cpp
+++ lib/valueflow.cpp
@@ -1511,7 +1511,7 @@ ValueFlow::Value ValueFlow::getLifetimeObjValue(const Token *tok, bool inconclus
     // There should only be one lifetime
     if (values.size() != 1)
         return ValueFlow::Value{};
-    return values.front();
+    return std::move(values.front());
 }
 
 template<class Predicate>
