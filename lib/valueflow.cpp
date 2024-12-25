a8eb1a9f61edddc41bc8b504af3ebd3da8dc3895
firewave <firewave@users.noreply.github.com>
2024-12-25 16:49:23 +0100

    valueflow.cpp: avoid unnecessary copies in `valueFlowInferCondition()`

--- lib/valueflow.cpp
+++ lib/valueflow.cpp
@@ -4970,7 +4970,7 @@ static void valueFlowInferCondition(TokenList& tokenlist, const Settings& settin
                 for (const ValuePtr<InferModel>& model : iteratorModels) {
                     std::vector<ValueFlow::Value> result =
                         infer(model, tok->str(), tok->astOperand1()->values(), tok->astOperand2()->values());
-                    for (ValueFlow::Value value : result) {
+                    for (ValueFlow::Value& value : result) {
                         value.valueType = ValueFlow::Value::ValueType::INT;
                         setTokenValue(tok, std::move(value), settings);
                     }
@@ -4988,8 +4988,7 @@ static void valueFlowInferCondition(TokenList& tokenlist, const Settings& settin
             std::vector<ValueFlow::Value> result = infer(makeIntegralInferModel(), "!=", tok->values(), 0);
             if (result.size() != 1)
                 continue;
-            ValueFlow::Value value = result.front();
-            setTokenValue(tok, std::move(value), settings);
+            setTokenValue(tok, std::move(result.front()), settings);
         }
     }
 }
