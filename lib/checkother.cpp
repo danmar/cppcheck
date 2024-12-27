32a37f449115c4bda06af6e7278b6ceff947251f
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2024-12-27 14:38:35 +0100

    Follow-up to #13478: Fix FP, remove redundant statement (#7138)

--- lib/checkother.cpp
+++ lib/checkother.cpp
@@ -907,11 +907,9 @@ void CheckOther::checkUnreachableCode()
                 tok->str() == "break") {
                 while (Token::simpleMatch(secondBreak, "{") && secondBreak->scope()->type == Scope::ScopeType::eUnconditional)
                     secondBreak = secondBreak->next();
-                if (Token::Match(secondBreak, "%name% :"))
-                    continue;
             }
-            if (Token::simpleMatch(secondBreak, "; }"))
-                continue;
+            while (Token::simpleMatch(secondBreak, ";"))
+                secondBreak = secondBreak->next();
 
             // Statements follow directly, no line between them. (#3383)
             // TODO: Try to find a better way to avoid false positives due to preprocessor configurations.
