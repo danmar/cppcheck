620f20b62a1c95f5ac32093c0ce570c5bde16f03
Ludvig Gunne Lindström <ludviggunnelindstrom@gmail.com>
2024-12-27 14:44:36 +0100

    add errorlogger tests

--- test/testerrorlogger.cpp
+++ test/testerrorlogger.cpp
@@ -70,6 +70,9 @@ private:
         TEST_CASE(substituteTemplateLocationStatic);
 
         TEST_CASE(isCriticalErrorId);
+
+        TEST_CASE(ErrorMessageReportTypeMisraC);
+        TEST_CASE(ErrorMessageReportTypeCertC);
     }
 
     void TestPatternSearchReplace(const std::string& idPlaceholder, const std::string& id) const {
@@ -192,6 +195,32 @@ private:
             ASSERT_EQUALS("[file.cpp:0]: (error) msg: message", msg.toString(false));
             ASSERT_EQUALS("[file.cpp:0]: (error) msg: message: details", msg.toString(true));
         }
+    }
+
+    void ErrorMessageReportTypeMisraC() const {
+        std::list<ErrorMessage::FileLocation> locs = { fooCpp5 };
+        const auto reportType = checkers::ReportType::misraC;
+        const auto mapping = checkers::createGuidelineMapping(reportType);
+        const std::string format = "{severity} {id}";
+        ErrorMessage msg(std::move(locs), emptyString, Severity::error, "", "unusedVariable", Certainty::normal);
+        msg.guideline = checkers::getGuideline(msg.id, reportType, mapping, msg.severity);
+        msg.classification = checkers::getClassification(msg.guideline, reportType);
+        ASSERT_EQUALS("Advisory", msg.classification);
+        ASSERT_EQUALS("2.8", msg.guideline);
+        ASSERT_EQUALS("Advisory 2.8", msg.toString(true, format));
+    }
+
+    void ErrorMessageReportTypeCertC() const {
+        std::list<ErrorMessage::FileLocation> locs = { fooCpp5 };
+        const auto reportType = checkers::ReportType::certC;
+        const auto mapping = checkers::createGuidelineMapping(reportType);
+        const std::string format = "{severity} {id}";
+        ErrorMessage msg(std::move(locs), emptyString, Severity::error, "", "resourceLeak", Certainty::normal);
+        msg.guideline = checkers::getGuideline(msg.id, reportType, mapping, msg.severity);
+        msg.classification = checkers::getClassification(msg.guideline, reportType);
+        ASSERT_EQUALS("L3", msg.classification);
+        ASSERT_EQUALS("FIO42-C", msg.guideline);
+        ASSERT_EQUALS("L3 FIO42-C", msg.toString(true, format));
     }
 
     void CustomFormat() const {
