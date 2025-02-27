3caccd25436ddb10398ab591f7f0f296729d514e
firewave <firewave@users.noreply.github.com>
2025-02-27 14:48:36 +0100

    Regex: hide implementation details

--- lib/cppcheck.cpp
+++ lib/cppcheck.cpp
@@ -1490,7 +1490,7 @@ void CppCheck::executeRules(const std::string &tokenlist, const TokenList &list)
             mErrorLogger.reportErr(errmsg);
         };
 
-        const std::string err = rule.regex.match(str, f);
+        const std::string err = rule.regex->match(str, f);
         if (!err.empty()) {
             const ErrorMessage errmsg(std::list<ErrorMessage::FileLocation>(),
                                       emptyString,
