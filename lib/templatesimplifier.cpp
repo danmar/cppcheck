895425ce790a58e50104d388d297a48de21ca019
chrchr-github <christian.riesch@live.com>
2025-03-07 22:15:23 +0100

    Fix

--- lib/templatesimplifier.cpp
+++ lib/templatesimplifier.cpp
@@ -2611,7 +2611,7 @@ void TemplateSimplifier::simplifyTemplateArgs(Token *start, const Token *end, st
         }
 
         for (Token *tok = first->next(); tok && tok != end; tok = tok->next()) {
-            if (endsWith(tok->str(), "_cast")) {
+            if (tok->isKeyword() && endsWith(tok->str(), "_cast")) {
                 Token* tok2 = tok->next()->findClosingBracket();
                 if (!tok2 || !Token::simpleMatch(tok2, "> ("))
                     syntaxError(tok);
