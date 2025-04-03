f27e7a05e12427266a79e26d853b7e7efec0b8ef
wy <wykevin@163.com>
2025-04-03 14:24:37 +0800

    fix #11824 - Option --max-configs has no effect if -D is used

--- cli/cmdlineparser.cpp
+++ cli/cmdlineparser.cpp
@@ -380,8 +380,6 @@ CmdLineParser::Result CmdLineParser::parseFromArgs(int argc, const char* const a
         }
     }
 
-    bool def = false;
-    bool maxconfigs = false;
     bool debug = false;
 
     ImportProject::Type projectType = ImportProject::Type::NONE;
@@ -426,8 +424,6 @@ CmdLineParser::Result CmdLineParser::parseFromArgs(int argc, const char* const a
             if (!mSettings.userDefines.empty())
                 mSettings.userDefines += ";";
             mSettings.userDefines += define;
-
-            def = true;
         }
 
         // -E
@@ -959,8 +955,6 @@ CmdLineParser::Result CmdLineParser::parseFromArgs(int argc, const char* const a
             }
 
             mSettings.maxConfigs = tmp;
-            mSettings.force = false;
-            maxconfigs = true;
         }
 
         // max ctu depth
@@ -1128,7 +1122,6 @@ CmdLineParser::Result CmdLineParser::parseFromArgs(int argc, const char* const a
                 return Result::Fail;
             }
 
-            mSettings.checkAllConfigurations = false;     // Can be overridden with --max-configs or --force
             std::string projectFile = argv[i]+10;
             projectType = project.import(projectFile, &mSettings, &mSuppressions);
             if (projectType == ImportProject::Type::CPPCHECK_GUI) {
@@ -1534,14 +1527,6 @@ CmdLineParser::Result CmdLineParser::parseFromArgs(int argc, const char* const a
     // replace static parts of the templates
     substituteTemplateFormatStatic(mSettings.templateFormat);
     substituteTemplateLocationStatic(mSettings.templateLocation);
-
-    if (mSettings.force || maxconfigs)
-        mSettings.checkAllConfigurations = true;
-
-    if (mSettings.force)
-        mSettings.maxConfigs = INT_MAX;
-    else if ((def || mSettings.preprocessOnly) && !maxconfigs)
-        mSettings.maxConfigs = 1U;
 
     if (debug) {
         mSettings.debugnormal = true;
