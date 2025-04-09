472563a0f575058d699b58e780f6ac633a6f07c2
firewave <firewave@users.noreply.github.com>
2025-04-09 09:55:56 +0200

    id [skip ci]

--- test/testother.cpp
+++ test/testother.cpp
@@ -250,6 +250,7 @@ private:
 
         TEST_CASE(testUnsignedLessThanZero);
 
+        mNewTemplate = true;
         TEST_CASE(doubleMove1);
         TEST_CASE(doubleMoveMemberInitialization1);
         TEST_CASE(doubleMoveMemberInitialization2);
