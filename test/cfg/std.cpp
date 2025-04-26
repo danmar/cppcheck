bd97fdfe0827bdb968f16e9cfe0bbba1f1a01961
chrchr-github <noreply@github.com>
2025-04-26 12:51:46 +0200

    Partial fix for  #13801 FP constParameterPointer due to bad library configuration

--- test/cfg/std.cpp
+++ test/cfg/std.cpp
@@ -5163,6 +5163,11 @@ void constVariablePointer_push_back(std::vector<T*>& d, const std::vector<T*>& s
     }
 }
 
+
+std::streampos constParameterPointer_istream_tellg(std::istream* p) { // #13801
+    return p->tellg();
+}
+
 // cppcheck-suppress constParameterReference
 void constParameterReference_push_back(std::vector<std::string>& v, std::string& s) { // #12661
     v.push_back(s);
