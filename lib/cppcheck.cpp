d32ccce8e1b73e535e8e2facf04548f7f482cb08
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2025-05-07 16:52:59 +0200

    skip-ctu

--- lib/cppcheck.cpp
+++ lib/cppcheck.cpp
@@ -1999,6 +1999,8 @@ void CppCheck::analyseClangTidy(const FileSettings &fileSettings)
 
 bool CppCheck::analyseWholeProgram()
 {
+    return false;
+
     bool errors = false;
     // Analyse the tokens
     CTU::FileInfo ctu;
@@ -2025,6 +2027,8 @@ bool CppCheck::analyseWholeProgram()
 
 unsigned int CppCheck::analyseWholeProgram(const std::string &buildDir, const std::list<FileWithDetails> &files, const std::list<FileSettings>& fileSettings, const std::string& ctuInfo)
 {
+    return mLogger->exitcode();
+
     executeAddonsWholeProgram(files, fileSettings, ctuInfo);
     if (mSettings.checks.isEnabled(Checks::unusedFunction))
         CheckUnusedFunctions::analyseWholeProgram(mSettings, mErrorLogger, buildDir);
