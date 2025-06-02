9097f2c01f19b61ae40e870adba52646de26e37a
Ludvig Gunne Lindström <ludviggunnelindstrom@gmail.com>
2025-06-02 10:35:20 +0200

    generate test files

--- test/cli/metrics_test.py
+++ test/cli/metrics_test.py
@@ -4,8 +4,17 @@ import os
 from testutils import cppcheck
 
 __script_dir = os.path.dirname(os.path.abspath(__file__))
-__addon_path = os.path.join(__script_dir, 'metrics_test', 'dummy_addon.py')
-__source_path = os.path.join(__script_dir, 'metrics_test', 'dummy_file.c')
+__addon_output = """
+{"metric":{"fileName":"1.cpp","function":"write","id":"HISCall","lineNumber":6,"value":2}}
+{"metric":{"fileName":"1.cpp","function":"write","id":"HISGoto","lineNumber":6,"value":0}}
+{"metric":{"fileName":"1.cpp","function":"write","id":"HISLevel","lineNumber":6,"value":2}}
+{"metric":{"fileName":"1.cpp","function":"write","id":"HISParam","lineNumber":6,"value":2}}
+{"metric":{"fileName":"1.cpp","function":"write","id":"HISPath","lineNumber":6,"value":3}}
+{"metric":{"fileName":"1.cpp","function":"write","id":"HISReturn","lineNumber":6,"value":0}}
+{"metric":{"fileName":"1.cpp","function":"write","id":"HISStmt","lineNumber":6,"value":15}}
+{"metric":{"fileName":"1.cpp","function":"write","id":"cyclomaticComplexity","lineNumber":6,"value":3}}
+"""
+__addon_source = f'print("""{__addon_output}""")'
 __expected_xml = [
     '<metric fileName="1.cpp" function="write" id="HISCall" lineNumber="6" value="2"/>',
     '<metric fileName="1.cpp" function="write" id="HISGoto" lineNumber="6" value="0"/>',
@@ -17,19 +26,33 @@ __expected_xml = [
     '<metric fileName="1.cpp" function="write" id="cyclomaticComplexity" lineNumber="6" value="3"/>'
 ]
 
+def __create_addon(tmpdir):
+    path = os.path.join(tmpdir, 'addon.py')
+    with open(path, 'w') as file:
+        file.write(__addon_source)
+    return path
+
+def __create_source_file(tmpdir):
+    path = os.path.join(tmpdir, 'test.c')
+    with open(path, 'w') as _:
+        pass
+    return path
+
 def test_dummy_metrics_xml_report(tmpdir):
     output_file = os.path.join(tmpdir, "results.xml")
+    source_path = __create_source_file(tmpdir)
+    addon_path = __create_addon(tmpdir)
     args = [
         f'--output-file={output_file}',
-        f'--addon={__addon_path}',
+        f'--addon={addon_path}',
         '--xml-version=3',
-        __source_path
+        source_path
     ]
 
     ret, stdout, stderr = cppcheck(args)
     assert ret == 0
     assert stderr == ''
-    assert stdout == f'Checking {__source_path} ...\n'
+    assert stdout == f'Checking {source_path} ...\n'
 
     with open(output_file, 'r') as file:
         xml = file.read()
@@ -37,16 +60,18 @@ def test_dummy_metrics_xml_report(tmpdir):
     for expected in __expected_xml:
         assert xml.find(expected) >= 0
 
-def test_dummy_metrics_stdout():
+def test_dummy_metrics_stdout(tmpdir):
+    source_path = __create_source_file(tmpdir)
+    addon_path = __create_addon(tmpdir)
     args = [
-        f'--addon={__addon_path}',
+        f'--addon={addon_path}',
         '--xml-version=3',
-        __source_path
+        source_path
     ]
 
     ret, stdout, stderr = cppcheck(args)
     assert ret == 0
-    assert stdout == f'Checking {__source_path} ...\n'
+    assert stdout == f'Checking {source_path} ...\n'
 
     for expected in __expected_xml:
         assert stderr.find(expected) >= 0
