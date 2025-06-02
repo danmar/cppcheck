95a60bea93f409b68a2b26d060988c2870d59ffc
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2025-06-02 11:43:31 +0200

    Update qt.cpp

--- test/cfg/qt.cpp
+++ test/cfg/qt.cpp
@@ -799,6 +799,11 @@ void constVariablePointer_QVector(QVector<int*>& qv, int* p)
     qv.push_back(p); // #12661
 }
 
+void constParameterPointer_QHash(QHash<QString, int*>& qh, const QString& qs, int* p)
+{
+    qh.insert(qs, p); // #13902
+}
+
 const QString& unassignedVariable_static_QString() // #12935
 {
     static QString qs;
