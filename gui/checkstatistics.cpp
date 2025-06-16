2d6360598449f629e70ced5467b57a588d41655d
wy <wykevin@163.com>
2025-06-16 08:44:01 +0800

    fix #12642 - GUI: statistics is not correct

--- gui/checkstatistics.cpp
+++ gui/checkstatistics.cpp
@@ -33,7 +33,7 @@ static void addItem(QMap<QString,unsigned> &m, const QString &key)
     if (m.contains(key))
         m[key]++;
     else
-        m[key] = 0;
+        m[key] = 1;
 }
 
 void CheckStatistics::addItem(const QString &tool, ShowTypes::ShowType type)
