9b11b6b0ac2c7806f06c8b92112221d5ad89b794
Oliver Stöneberg <firewave@users.noreply.github.com>
2025-07-04 16:03:30 +0200

    fixed #13979 - added `macos-15` to CI / adjusted some tests (#6519)

--- test/cfg/runtests.sh
+++ test/cfg/runtests.sh
@@ -44,7 +44,7 @@ CPPCHECK_OPT=(
 CXX=g++
 CXX_OPT=("-fsyntax-only" "-w" "-std=c++2a")
 CC=gcc
-CC_OPT=("-fsyntax-only" "-w" "-Wno-implicit-function-declaration" "-std=c11") # TODO: remove -Wno-implicit-function-declaration when warnings are fixed on MacOS
+CC_OPT=("-fsyntax-only" "-w" "-std=c11")
 
 function get_pkg_config_cflags {
     # TODO: get rid of the error enabling/disabling?
