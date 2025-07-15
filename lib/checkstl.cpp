c17b7e26d3eda6b00107acf8b0b0be27cc872e7c
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2025-07-15 10:38:42 +0200

    Update checkstl.cpp

--- lib/checkstl.cpp
+++ lib/checkstl.cpp
@@ -1202,6 +1202,10 @@ void CheckStl::invalidContainer()
                         if (info.tok->variable()->isReference() && !isVariableDecl(info.tok) &&
                             reaches(info.tok->variable()->nameToken(), tok, nullptr)) {
 
+                            if (assignExpr && Token::Match(assignExpr->astOperand1(), "& %varid%", info.tok->varId())) { // TODO: fix AST
+                                return false;
+                            }
+
                             ErrorPath ep;
                             bool addressOf = false;
                             const Variable* var = ValueFlow::getLifetimeVariable(info.tok, ep, *mSettings, &addressOf);
