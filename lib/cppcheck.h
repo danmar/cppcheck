ac4a86dc96e4237a753ccbb0b7910db5f2219c58
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2025-08-19 16:31:46 +0200

    Fix #14064 Files are reanalyzed with --cppcheck-build-dir and inline suppressions (regression) (#7741)
    (cherry picked from commit 4617bc25d82ff8d46567e002995ad8f859d5e814)

--- lib/cppcheck.h
+++ lib/cppcheck.h
@@ -171,7 +171,7 @@ private:
      * @param tokens        Token list from preprocessed file.
      * @return hash
      */
-    std::size_t calculateHash(const Preprocessor &preprocessor, const simplecpp::TokenList &tokens) const;
+    std::size_t calculateHash(const Preprocessor &preprocessor, const simplecpp::TokenList &tokens, const std::string& filePath = {}) const;
 
     /**
      * @brief Check a file using stream
