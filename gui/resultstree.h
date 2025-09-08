1a5627ad2a1f1def06ddd27d8de7a7a6d6672c45
firewave <firewave@users.noreply.github.com>
2025-09-08 13:27:37 +0200

    fixed `misc-override-with-different-visibility` clang-tidy warnings

--- gui/resultstree.h
+++ gui/resultstree.h
@@ -204,8 +204,6 @@ public:
      */
     ShowTypes mShowSeverities;
 
-    void keyPressEvent(QKeyEvent *event) override;
-
     void setReportType(ReportType reportType);
 
     /**
@@ -559,6 +557,8 @@ protected:
     bool mVisibleErrors{};
 
 private:
+    void keyPressEvent(QKeyEvent *event) override;
+
     /** tag selected items */
     void tagSelectedItems(const QString &tag);
 
