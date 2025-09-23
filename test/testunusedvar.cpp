376d7f1896e1b9145a369055d5f15be2d87cea11
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2025-09-23 17:13:09 +0200

    Fix #14075 (False positive: unusedStructMember, member usage by alignas is ignored) (#7842)
    This should be reviewed extra carefully; I used ai to fix the ticket.

--- test/testunusedvar.cpp
+++ test/testunusedvar.cpp
@@ -71,6 +71,7 @@ private:
         TEST_CASE(structmember26); // #13345
         TEST_CASE(structmember27); // #13367
         TEST_CASE(structmember28);
+        TEST_CASE(structmember29); // #14075
         TEST_CASE(structmember_macro);
         TEST_CASE(structmember_template_argument); // #13887 - do not report that member used in template argument is unused
         TEST_CASE(classmember);
@@ -2011,6 +2012,15 @@ private:
                                "    unsigned int : 16;\n"
                                "};\n");
         ASSERT_EQUALS("[test.cpp:2:18]: (style) struct member 'S::a' is never used. [unusedStructMember]\n", errout_str());
+    }
+
+    void structmember29() { // #14075 - alignas false positive
+        checkStructMemberUsage("struct S {\n"
+                               "    static constexpr size_t cDataAlign = 8;\n"
+                               "    static constexpr size_t cDataSize = 128;\n"
+                               "    alignas(cDataAlign) std::array<uint8_t, cDataSize> storage{};\n"
+                               "};\n");
+        ASSERT_EQUALS("[test.cpp:4:56]: (style) struct member 'S::storage' is never used. [unusedStructMember]\n", errout_str());
     }
 
     void structmember_macro() {
