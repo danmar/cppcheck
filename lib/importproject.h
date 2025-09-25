2bb5cd5b5558a28203ce8909f53d3dc47cf813cd
firewave <firewave@users.noreply.github.com>
2025-09-25 12:29:35 +0200

    ImportProject: removed premium parameters

--- lib/importproject.h
+++ lib/importproject.h
@@ -98,10 +98,10 @@ public:
     void ignorePaths(const std::vector<std::string> &ipaths, bool debug = false);
     void ignoreOtherConfigs(const std::string &cfg);
 
-    Type import(const std::string &filename, Settings *settings=nullptr, Suppressions *supprs=nullptr, bool premium=false);
+    Type import(const std::string &filename, Settings *settings=nullptr, Suppressions *supprs=nullptr);
 protected:
     bool importCompileCommands(std::istream &istr);
-    bool importCppcheckGuiProject(std::istream &istr, Settings &settings, Suppressions &supprs, bool premium);
+    bool importCppcheckGuiProject(std::istream &istr, Settings &settings, Suppressions &supprs);
 
 private:
     struct SharedItemsProject {
