1ef66adf55d0108daa7f2417ef21e3b0d44e64b9
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2025-10-30 09:30:52 +0100

    Update astutils.cpp

--- lib/astutils.cpp
+++ lib/astutils.cpp
@@ -1615,10 +1615,10 @@ bool isSameExpression(bool macro, const Token *tok1, const Token *tok2, const Se
         return false;
 
     const Token *followTok1 = tok1, *followTok2 = tok2;
-    while (Token::simpleMatch(followTok1, "::") && followTok1->astOperand2())
-        followTok1 = followTok1->astOperand2();
+    while (Token::simpleMatch(followTok1, "::"))
+        followTok1 = followTok1->astOperand2() ? followTok1->astOperand2() : followTok1->astOperand1();
     while (Token::simpleMatch(followTok2, "::") && followTok2->astOperand2())
-        followTok2 = followTok2->astOperand2();
+        followTok2 = followTok2->astOperand2() ? followTok2->astOperand2() : followTok2->astOperand1();
     if (isSameConstantValue(macro, followTok1, followTok2))
         return true;
 
