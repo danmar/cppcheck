7c36faefc3fe0563e1bb78809b3e76960fdec05f
firewave <firewave@users.noreply.github.com>
2025-11-01 13:40:50 +0100

    other_test.py: added test for reporting unmatched suppressions with subpath

--- test/cli/other_test.py
+++ test/cli/other_test.py
@@ -3798,3 +3798,51 @@ def test_premium_disabled_unmatched(tmp_path):  #13663
     ]
     assert stdout == ''
     assert ret == 0, stdout
+
+
+def test_unmatched_file(tmp_path):
+    lib_path = tmp_path / 'lib'
+    os.makedirs(lib_path)
+
+    test_file = lib_path / 'test.c'
+    with open(test_file, "w"):
+        pass
+
+    suppr_txt = tmp_path / 'suppr.txt'
+    with open(suppr_txt, "w") as f:
+        f.write('''
+error:lib/test.c
+error2:lib\\test.c
+''')
+
+    suppr_xml = tmp_path / 'suppr.xml'
+    with open(suppr_xml, "w") as f:
+        f.write('''
+<suppressions>
+    <suppress>
+        <id>error3</id>
+        <fileName>lib/test.c</fileName>
+    </suppress>
+    <suppress>
+        <id>error4</id>
+        <fileName>lib\\test.c</fileName>
+    </suppress>
+</suppressions>
+''')
+
+    args = [
+        '-q',
+        '--template=simple',
+        '--enable=information',
+        f'--suppressions-list={suppr_txt}',
+        f'--suppress-xml={suppr_xml}',
+        '--suppress=error5:lib/test.c',
+        '--suppress=error6:lib\\test.c',
+        str(test_file)
+    ]
+
+    ret, stdout, stderr = cppcheck(args)
+    assert stdout == ''
+    assert stderr.splitlines() == [
+    ]
+    assert ret == 0, stdout
