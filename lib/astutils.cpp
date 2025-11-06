83c81365d1a34030862476a067b2be7f4bccd1e2
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2025-11-06 13:41:46 +0100

    Update astutils.cpp

--- lib/astutils.cpp
+++ lib/astutils.cpp
@@ -2617,6 +2617,11 @@ bool isVariableChangedByFunctionCall(const Token *tok, int indirect, const Setti
     return false;
 }
 
+static bool hasOverloadedMemberAccess(const Token* varTok)
+{
+    return !varTok || !varTok->variable() || !varTok->variable()->valueType() || varTok->variable()->valueType()->pointer == 0;
+}
+
 bool isVariableChanged(const Token *tok, int indirect, const Settings &settings, int depth)
 {
     if (!isMutableExpression(tok))
@@ -2631,7 +2636,7 @@ bool isVariableChanged(const Token *tok, int indirect, const Settings &settings,
            (Token::simpleMatch(tok2->astParent(), ".") && !Token::Match(tok2->astParent()->astParent(), "[(,]")) ||
            (tok2->astParent() && tok2->astParent()->isUnaryOp("&") && Token::simpleMatch(tok2->astParent()->astParent(), ".") && tok2->astParent()->astParent()->originalName()=="->") ||
            (Token::simpleMatch(tok2->astParent(), "[") && tok2 == tok2->astParent()->astOperand1())) {
-        if (tok2->astParent() && (tok2->astParent()->isUnaryOp("*") || (astIsLHS(tok2) && tok2->astParent()->originalName() == "->")))
+        if (tok2->astParent() && (tok2->astParent()->isUnaryOp("*") || (astIsLHS(tok2) && tok2->astParent()->originalName() == "->") && !hasOverloadedMemberAccess(tok2->astOperand2())))
             derefs++;
         if (derefs > indirect)
             break;
