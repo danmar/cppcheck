7100bdabf154b3a97397492261fbdb194073a7ff
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2025-11-07 19:03:37 +0100

    Fix #14251 FP constParameterReference for struct with optional struct member (#7948)

--- lib/astutils.cpp
+++ lib/astutils.cpp
@@ -2617,6 +2617,14 @@ bool isVariableChangedByFunctionCall(const Token *tok, int indirect, const Setti
     return false;
 }
 
+static bool hasOverloadedMemberAccess(const Token* tok)
+{
+    if (!Token::simpleMatch(tok, "."))
+        return false;
+    const Token* varTok = tok->astOperand2();
+    return !varTok || !varTok->variable() || !varTok->variable()->valueType() || varTok->variable()->valueType()->pointer == 0;
+}
+
 bool isVariableChanged(const Token *tok, int indirect, const Settings &settings, int depth)
 {
     if (!isMutableExpression(tok))
@@ -2631,7 +2639,7 @@ bool isVariableChanged(const Token *tok, int indirect, const Settings &settings,
            (Token::simpleMatch(tok2->astParent(), ".") && !Token::Match(tok2->astParent()->astParent(), "[(,]")) ||
            (tok2->astParent() && tok2->astParent()->isUnaryOp("&") && Token::simpleMatch(tok2->astParent()->astParent(), ".") && tok2->astParent()->astParent()->originalName()=="->") ||
            (Token::simpleMatch(tok2->astParent(), "[") && tok2 == tok2->astParent()->astOperand1())) {
-        if (tok2->astParent() && (tok2->astParent()->isUnaryOp("*") || (astIsLHS(tok2) && tok2->astParent()->originalName() == "->")))
+        if (tok2->astParent() && (tok2->astParent()->isUnaryOp("*") || (astIsLHS(tok2) && tok2->astParent()->originalName() == "->" && !hasOverloadedMemberAccess(tok2))))
             derefs++;
         if (derefs > indirect)
             break;
