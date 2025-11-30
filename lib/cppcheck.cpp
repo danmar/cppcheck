19663d69cbc00ed84f7bdc15958c09e4bacce2e5
firewave <firewave@users.noreply.github.com>
2025-11-30 20:53:37 +0100

    adjusted file lookups from `simplecpp::Location`

--- lib/cppcheck.cpp
+++ lib/cppcheck.cpp
@@ -1154,12 +1154,12 @@ unsigned int CppCheck::checkInternal(const FileWithDetails& file, const std::str
                         }
                         else {
                             // #error etc during preprocessing
-                            configurationError.push_back((currentConfig.empty() ? "\'\'" : currentConfig) + " : [" + o->location.file() + ':' + std::to_string(o->location.line) + "] " + o->msg);
+                            configurationError.push_back((currentConfig.empty() ? "\'\'" : currentConfig) + " : [" + tokensP.file(o->location) + ':' + std::to_string(o->location.line) + "] " + o->msg);
                             --checkCount; // don't count invalid configurations
 
                             if (!hasValidConfig && currCfg == *configurations.rbegin()) {
                                 // If there is no valid configuration then report error..
-                                preprocessor.error(o->location.file(), o->location.line, o->location.col, o->msg, o->type);
+                                preprocessor.error(tokensP.file(o->location), o->location.line, o->location.col, o->msg, o->type);
                             }
                             skipCfg = true;
                         }
