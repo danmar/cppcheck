e5118ea364b4ce4109de00529fc5650ec2788437
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2025-12-09 12:32:15 +0100

    Update astutils.cpp

--- lib/astutils.cpp
+++ lib/astutils.cpp
@@ -3678,7 +3678,7 @@ bool isGlobalData(const Token *expr)
             // TODO check if pointer points at local data
             const Variable *lhsvar = tok->astOperand1()->variable();
             const ValueType *lhstype = tok->astOperand1()->valueType();
-            if (lhsvar->isPointer()) {
+            if (lhsvar->isPointer() || !lhstype || lhstype == ValueType::Type::ITERATOR) {
                 globalData = true;
                 return ChildrenToVisit::none;
             }
