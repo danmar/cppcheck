804c4a4ab7547ed50deb8903d7be7781b980d04c
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2025-12-09 16:33:15 +0100

    Fix #13303 FN unreadVariable (iterator) (#8023)

--- lib/astutils.cpp
+++ lib/astutils.cpp
@@ -3678,7 +3678,7 @@ bool isGlobalData(const Token *expr)
             // TODO check if pointer points at local data
             const Variable *lhsvar = tok->astOperand1()->variable();
             const ValueType *lhstype = tok->astOperand1()->valueType();
-            if (lhsvar->isPointer()) {
+            if (lhsvar->isPointer() || !lhstype || lhstype->type == ValueType::Type::ITERATOR) {
                 globalData = true;
                 return ChildrenToVisit::none;
             }
@@ -3686,7 +3686,7 @@ bool isGlobalData(const Token *expr)
                 globalData = true;
                 return ChildrenToVisit::none;
             }
-            if (lhsvar->isArgument() && (!lhstype || (lhstype->type <= ValueType::Type::VOID && !lhstype->container))) {
+            if (lhsvar->isArgument() && lhstype->type <= ValueType::Type::VOID && !lhstype->container) {
                 globalData = true;
                 return ChildrenToVisit::none;
             }
