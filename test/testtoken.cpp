72883d3ba09c59748267ebc63a75e69945721169
firewave <firewave@users.noreply.github.com>
2025-12-31 05:14:58 +0100

    re-worded some messages and avoid using product name [skip ci]

--- test/testtoken.cpp
+++ test/testtoken.cpp
@@ -705,7 +705,7 @@ private:
         ASSERT(var.tokenize("int a ; int b ;"));
 
         // Varid == 0 should throw exception
-        ASSERT_THROW_INTERNAL_EQUALS((void)Token::Match(var.tokens(), "%type% %varid% ; %type% %name%", 0),INTERNAL,"Internal error. Token::Match called with varid 0. Please report this to Cppcheck developers");
+        ASSERT_THROW_INTERNAL_EQUALS((void)Token::Match(var.tokens(), "%type% %varid% ; %type% %name%", 0),INTERNAL,"Internal error. Token::Match called with varid 0. Please report this to the developers");
 
         ASSERT_EQUALS(true, Token::Match(var.tokens(), "%type% %varid% ; %type% %name%", 1));
         ASSERT_EQUALS(true, Token::Match(var.tokens(), "%type% %name% ; %type% %varid%", 2));
