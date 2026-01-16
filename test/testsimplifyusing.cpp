08307a9c9917908ea9729ac8e41673cda3d89056
chrchr-github <78114321+chrchr-github@users.noreply.github.com>
2026-01-16 13:34:50 +0100

    Update testsimplifyusing.cpp

--- test/testsimplifyusing.cpp
+++ test/testsimplifyusing.cpp
@@ -72,7 +72,8 @@ private:
         TEST_CASE(simplifyUsing33);
         TEST_CASE(simplifyUsing34);
         TEST_CASE(simplifyUsing35);
-        TEST_CASE(simplifyUsing36);
+        TEST_CASE(simplifyUsing36);        
+        TEST_CASE(simplifyUsing37);
 
         TEST_CASE(simplifyUsing8970);
         TEST_CASE(simplifyUsing8971);
@@ -880,6 +881,23 @@ private:
         const char expected[] = "int c ; c = B < int > :: a ;";
         ASSERT_EQUALS(expected, tok(code));
         ASSERT_EQUALS("", errout_str());
+    }
+
+    void simplifyUsing37() {
+        const char code1[] = "using fp1_t = int(*)(int);\n"
+                             "using fp2_t = int(* const)(int);\n"
+                             "fp1_t fp1;\n"
+                             "fp2_t fp2;\n";
+        const char expected1[] = "int ( * fp1 ) ( int ) ; int ( * const fp2 ) ( int ) ;";
+        ASSERT_EQUALS(expected1, tok(code1));
+        ASSERT_EQUALS("", errout_str());
+
+        const char code2[] = "using f_t = int(int);\n"
+                            "f_t* fp;\n";
+        const char expected2[] = "int ( * fp1 ) ( int ) ; int ( * const fp2 ) ( int ) ;";
+        const char actual2[] = "using f_t = int ( ) ; f_t * fp ;";
+        TODO_ASSERT_EQUALS(expected2, actual2, tok(code2));
+        TODO_ASSERT_EQUALS("", "[test.cpp:1:1]: (debug) Failed to parse 'using f_t = int ( ) ;'. The checking continues anyway. [simplifyUsing]\n", errout_str());
     }
 
     void simplifyUsing8970() {
