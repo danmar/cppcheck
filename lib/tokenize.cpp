7dbb1fadbb1624febd130839dd885ec9b0d486ed
ceJce <johan.crone@cikoriaembedded.se>
2026-02-03 16:42:15 +0100

    Fix #13735 AST:function name in parantheses (#8183)
    https://trac.cppcheck.net/ticket/13735
    
    If the start paranthese is preceeded by a keyword like "return" the
    parantheses removal is aborted.
    
    Added no abort removal of parantheses in
    simplifyParenthesizedLibraryFunctionsif the preceeding %name% is a
    keyword, like "return (modf) ...

--- lib/tokenize.cpp
+++ lib/tokenize.cpp
@@ -3790,7 +3790,7 @@ void Tokenizer::simplifyParenthesizedLibraryFunctions()
         if (!Token::simpleMatch(tok, ") ("))
             continue;
         Token *rpar = tok, *lpar = tok->link();
-        if (!lpar || Token::Match(lpar->previous(), "%name%"))
+        if (!lpar || (Token::Match(lpar->previous(), "%name%") && !lpar->previous()->isKeyword()))
             continue;
         const Token *ftok = rpar->previous();
         if (mSettings.library.isNotLibraryFunction(ftok))
