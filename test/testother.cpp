967b06c02681fb7d165afda2a8f9f0d681a48130
Ludvig Gunne Lindström <ludviggunnelindstrom@gmail.com>
2026-02-17 13:05:57 +0100

    Update old test

--- test/testother.cpp
+++ test/testother.cpp
@@ -10429,7 +10429,7 @@ private:
 
         // Member variable pointers
         check("void podMemPtrs() {\n"
-              "    int POD::*memptr;\n"
+              "    const int POD::*memptr;\n"
               "    memptr = &POD::a;\n"
               "    memptr = &POD::b;\n"
               "    if (memptr)\n"
