52b9648d08746e2e31dbf472297ef2b3364bdea1
Daniel Marjamäki <daniel.marjamaki@cppchecksolutions.com>
2026-02-25 11:43:55 +0100

    update

--- lib/checkclass.cpp
+++ lib/checkclass.cpp
@@ -2748,15 +2748,17 @@ namespace { // avoid one-definition-rule violation
 
 void CheckClass::initializerListOrder()
 {
-    if (!mSettings->severity.isEnabled(Severity::style) && !mSettings->isPremiumEnabled("initializerList"))
-        return;
+    if (!mSettings->isPremiumEnabled("initializerList")) {
+        if (!mSettings->severity.isEnabled(Severity::style))
+            return;
 
-    // This check is not inconclusive.  However it only determines if the initialization
-    // order is incorrect.  It does not determine if being out of order causes
-    // a real error.  Out of order is not necessarily an error but you can never
-    // have an error if the list is in order so this enforces defensive programming.
-    if (!mSettings->certainty.isEnabled(Certainty::inconclusive))
-        return;
+        // This check is not inconclusive.  However it only determines if the initialization
+        // order is incorrect.  It does not determine if being out of order causes
+        // a real error.  Out of order is not necessarily an error but you can never
+        // have an error if the list is in order so this enforces defensive programming.
+        if (!mSettings->certainty.isEnabled(Certainty::inconclusive))
+            return;
+    }
 
     logChecker("CheckClass::initializerListOrder"); // style,inconclusive
 
