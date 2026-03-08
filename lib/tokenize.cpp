7aa3306b3007acb6a4d8cc14c96401f850fc6ecf
Ludvig Gunne Lindström <ludviggunnelindstrom@gmail.com>
2026-03-08 12:38:51 +0100

    Fix #14539

--- lib/tokenize.cpp
+++ lib/tokenize.cpp
@@ -2998,6 +2998,9 @@ bool Tokenizer::simplifyUsing()
         if (!usingEnd)
             continue;
 
+        for (Token *typeTok = start; typeTok != usingEnd; typeTok = typeTok->next())
+            typeTok->isSimplifiedTypedef(true);
+
         // Move struct defined in using out of using.
         // using T = struct t { }; => struct t { }; using T = struct t;
         // fixme: this doesn't handle attributes
